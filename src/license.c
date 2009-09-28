a052d738c96174479a7e04064b390c227e14b373
Mark Burgess <mark@cfengine.com>
2009-09-28 07:37:57 +0000

    License file

---
+++ src/license.c
@@ -0,0 +1,16 @@
+
+/*
+
+ This file is (C) Cfengine AS. See COSL LICENSE for details.
+
+*/
+
+/*****************************************************************************/
+/*                                                                           */
+/* File: license.c                                                           */
+/*                                                                           */
+/*****************************************************************************/
+
+#include "cf3.defs.h"
+#include "cf3.extern.h"
+#include "cf.nova.h"
