22ad915ef84c2e16ddac9288689adb5e987b9917
Eystein Måløy Stenberg <eystein@cfengine.com>
2009-11-24 14:09:11 +0000

    Rewrote some process list functions to be more portable: limited command string to name of executable and use CreateToolhelp32Snapshot() to get pids. Only Debug level shows some of the errors. Use a more stable implementation to get process user name - works flawlessly when running as a service.

--- src/win_ps.c
+++ src/win_ps.c
@@ -17,62 +17,36 @@
 #include "cf3.defs.h"
 #include "cf3.extern.h"
 #include "cf.nova.h"
+#include <tlhelp32.h>
 
 #ifdef MINGW
 
 /* defines */
-#define MAXCMDSTR 512  // too big: we read across boundries, giving failure
-                       // too small: we truncate the command line string
+#define MAXCMDSTR 512  // max length of process commmand line (e.g. "c:\program.exe --param")
 #define TIMESTAMP_WAIT 500  // msecs between checking two process timestamps (for % CPU usage)
-
-
-/* structs TODO: Get from headers ? */
-typedef struct _PEB_LDR_DATA
-{
-   BYTE Reserved1[8];
-   PVOID Reserved2[3];
-   LIST_ENTRY InMemoryOrderModuleList;
-} PEB_LDR_DATA, *PPEB_LDR_DATA;
-
-typedef struct _PEB {  // Exists in Winternl.h, which is nonexistant on MinGW. Win64: use different structure.
-  BYTE                          Reserved1[2];
-  BYTE                          BeingDebugged;
-  BYTE                          Reserved2[1];
-  PVOID                         Reserved3[2];
-  PPEB_LDR_DATA                 Ldr;
-  PRTL_USER_PROCESS_PARAMETERS  ProcessParameters;
-  BYTE                          Reserved4[104];
-  PVOID                         Reserved5[52];
-  PVOID                         PostProcessInitRoutine;
-  BYTE                          Reserved6[128];
-  PVOID                         Reserved7[1];
-  ULONG                         SessionId;
-}PEB, *PPEB;
 
 // define pointer to NtQueryInformationProcess function
 typedef NTSTATUS (WINAPI *NTQIP)(HANDLE, PROCESSINFOCLASS, PVOID, ULONG, PULONG);
 
-
 /* static prototypes */
-static char *GetProcessInfo(DWORD pid, ULARGE_INTEGER lastTimeStamp, DWORDLONG totalPhysMemB);
+static char *GetProcessInfo(DWORD pid, char *execName, ULARGE_INTEGER lastTimeStamp, DWORDLONG totalPhysMemB);
 static int EnableDebugPriv(void);
-static BOOL GetProcessCmdLine(HANDLE procHandle, char *cmdLineStr, int cmdLineSz);
+//static int GetProcessCmdLine(HANDLE procHandle, char *cmdLineStr, int cmdLineSz);
 static void GetProcessTime(HANDLE procHandle, char *timeCreateStr, ULARGE_INTEGER *timeCpuInt, char *timeCpuStr);
 static void FormatCpuTime(FILETIME *timeKernel, FILETIME *timeUser, ULARGE_INTEGER *timeCpuInt, char *timeCpuStr);
 static void FormatCreationTime(FILETIME *timeCreate, char *timeCreateStr);
 static void GetMemoryInfo(HANDLE procHandle, char *memSzStr, DWORDLONG totalPhysMemB);
 static void GetProcessCpuTime(DWORD pid, ULARGE_INTEGER *timeCpuInt);
-static char *GetProcessUserNameOld(HANDLE procHandle, BOOL incDomain);
-static void GetProcessUserName(HANDLE hProcess, int incDomain, char *nameStr, int nameStrSz);
-static BOOL SidToUser(PSID pSid, LPTSTR szUser, DWORD dwUserLen, LPTSTR szDomain, DWORD dwDomainLen);
+static void GetProcessUserName(HANDLE procHandle, int incDomain, char *nameStr, int nameStrSz);
 
+/*****************************************************************************/
 
 int NovaWin_LoadProcessTable(struct Item **procdata,char *psopts)
 {
-  DWORD pids[CF_BUFSIZE];
+  HANDLE processSnap;
+  PROCESSENTRY32 pe32;
   ULARGE_INTEGER cpuTimeStamps[CF_BUFSIZE];
   MEMORYSTATUSEX memInfo;
-  DWORD pidLength, pidCount;
   struct Item *systemprocs = NULL;
   struct Item *otherprocs = NULL;
   char buf[CF_BUFSIZE];
@@ -82,19 +56,7 @@ int NovaWin_LoadProcessTable(struct Item **procdata,char *psopts)
   // need debug priviliges to open some special processes, like crss.exe, alg.exe, svchost.exe
   if(!EnableDebugPriv())
     {
-      CfOut(cf_verbose,"EnableDebugPriv","Current account need 'Debug programs' privilige to get info on all processes");
-    }
-
-
-  if (!EnumProcesses(pids, sizeof(pids), &pidLength))
-    {
-      CfOut(cf_error,"EnumProcesses","!! Could enumerate processes");
-      return false;
-    }
-
-  if(pidLength == sizeof(pids))
-    {
-      CfOut(cf_verbose,"","Array may be too small to fit all process IDs");
+      CfOut(cf_error,"EnableDebugPriv","!! Current account needs 'Debug programs' privilige to get info on all processes");
     }
 
   // get total memory (needed for process % mem usage)
@@ -105,31 +67,70 @@ int NovaWin_LoadProcessTable(struct Item **procdata,char *psopts)
       memInfo.ullTotalPhys = 1;
     }
 
-  pidCount = pidLength / sizeof(DWORD);
-  
-  Debug("Total number of processes found is %lu", pidCount);
+  // t snapshot of all processes in the system
+  processSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-  // need two timestamps to get CPU % usage
-
-  for (i = 0; i < pidCount; i++)
+  if(processSnap == INVALID_HANDLE_VALUE)
     {
-      GetProcessCpuTime(pids[i], &cpuTimeStamps[i]);
+      CfOut(cf_error, "CreateToolhelp32Snapshot", "!! Could not get process snapshot");
+      return false;
+    }
+
+  pe32.dwSize = sizeof(pe32);
+
+  // retrieve information about the first process
+  if(!Process32First(processSnap,&pe32))
+    {
+      CfOut(cf_error, "Process32First", "!! Could not get first entry in process snapshot");
+      CloseHandle(processSnap);
+      return false;
+    }
+
+  // get cpu timestamps for all processes
+  for(i = 0; i < CF_BUFSIZE; i++)
+    {
+      GetProcessCpuTime(pe32.th32ProcessID, &cpuTimeStamps[i]);
+      
+      if(!Process32Next(processSnap, &pe32))
+	{
+	  break;
+	}
+    }
+
+  if(i == CF_BUFSIZE)
+    {
+      CfOut(cf_error, "", "!! More than %d processes on this system - a larger buffer is needed", CF_BUFSIZE);
     }
 
   Sleep(TIMESTAMP_WAIT);
-  
-  for (i = 0; i < pidCount; i++)
+
+  if(!Process32First(processSnap,&pe32))
     {
-      psLine = GetProcessInfo(pids[i], cpuTimeStamps[i], memInfo.ullTotalPhys);
+      CfOut(cf_error, "Process32First", "!! Could not get first entry in process snapshot");
+      CloseHandle(processSnap);
+      return false;
+    }
+
+  // get rest of info for all processes
+  for(i = 0; i < CF_BUFSIZE; i++)
+    {
+      psLine = GetProcessInfo(pe32.th32ProcessID, pe32.szExeFile, cpuTimeStamps[i], memInfo.ullTotalPhys);
       
       if(psLine)
 	{
 	  AppendItem(procdata,psLine,"");
 	}
+
+      if(!Process32Next(processSnap, &pe32))
+	{
+	  break;
+	}
     }
 
-  // save ps output to files, include header
-  sprintf(buf, "%-20s %5s %s %s %8s %8s %-3s %s %5s %s", 
+  CloseHandle(processSnap);
+
+  // save process list output to files, include header
+  snprintf(buf, sizeof(buf), "%-20s %5s %s %s %8s %8s %-3s %s %5s %s", 
 	  "USER", "PID", "%CPU", "%MEM", "VSZ", "RSS", "TTY", "START", "TIME", "COMMAND"); 
   
   PrependItem(procdata, buf, NULL);
@@ -165,8 +166,9 @@ int NovaWin_LoadProcessTable(struct Item **procdata,char *psopts)
   return true;
 }
 
+/*****************************************************************************/
 
-static char *GetProcessInfo(DWORD pid, ULARGE_INTEGER lastTimeStamp, DWORDLONG totalPhysMemB)
+static char *GetProcessInfo(DWORD pid, char *execName, ULARGE_INTEGER lastTimeStamp, DWORDLONG totalPhysMemB)
 {
   static char psLine[CF_BUFSIZE];
   HANDLE procHandle;
@@ -184,9 +186,9 @@ static char *GetProcessInfo(DWORD pid, ULARGE_INTEGER lastTimeStamp, DWORDLONG t
       return NULL;
     }
   
-  procHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | STANDARD_RIGHTS_READ, FALSE, pid);
+  procHandle = OpenProcess(PROCESS_QUERY_INFORMATION | STANDARD_RIGHTS_READ, FALSE, pid);
   
-  if(procHandle == NULL) 
+  if(procHandle == NULL)
     {
       CfOut(cf_error,"OpenProcess","!! Error opening process handle for pid=%lu", pid);
       return NULL;
@@ -207,7 +209,8 @@ static char *GetProcessInfo(DWORD pid, ULARGE_INTEGER lastTimeStamp, DWORDLONG t
       break;
 
     default:
-      GetProcessCmdLine(procHandle, cmdLineStr, sizeof(cmdLineStr));
+      //GetProcessCmdLine(procHandle, cmdLineStr, sizeof(cmdLineStr));
+      snprintf(cmdLineStr, sizeof(cmdLineStr), "%s", execName);  // use only execuable image name for now
     }
   
   CloseHandle(procHandle);
@@ -226,11 +229,12 @@ static char *GetProcessInfo(DWORD pid, ULARGE_INTEGER lastTimeStamp, DWORDLONG t
       cpuUsagePercent = 0;
     }
   
-  memset(psLine, 0, sizeof(psLine));
-  sprintf(psLine, "%-20.20s %5lu %4.1f %s %s %s %s %s", userName, pid, cpuUsagePercent, memSzStr, "?  ", timeCreateStr, timeCpuStr, cmdLineStr);
+  snprintf(psLine, sizeof(psLine), "%-20.20s %5lu %4.1f %s %s %s %s %s", userName, pid, cpuUsagePercent, memSzStr, "?  ", timeCreateStr, timeCpuStr, cmdLineStr);
+
   return psLine;
 }
 
+/*****************************************************************************/
 
 static int EnableDebugPriv(void) 
 {
@@ -261,65 +265,25 @@ static int EnableDebugPriv(void)
 	return true;
 }
 
+/*****************************************************************************/
 
-static BOOL GetProcessCmdLine(HANDLE procHandle, char *cmdLineStr, int cmdLineSz)
-{
-  PROCESS_BASIC_INFORMATION procInfo = {0};
-  RTL_USER_PROCESS_PARAMETERS procParams = {0};
-  WCHAR cmdLineWstr[cmdLineSz];
-  PEB envBlock = {{0}};
-  SIZE_T read;
-
-  // load ntdll.dll and get function pointer
-  HMODULE ntdll = LoadLibrary("ntdll.dll");
-
-  if(ntdll == NULL)
-    {
-      CfOut(cf_error,"LoadLibrary","!! Error loading ntdll library");
-      return false;
-    }
-
-  NTQIP ntqip = (NTQIP) GetProcAddress(ntdll, "NtQueryInformationProcess");
-
-  if(ntqip == NULL)
-    {
-      CfOut(cf_error,"GetProcAddress","!! Could not get pointer to NtQueryInformationProcess()");
-      return false;
-    }
+// NOTE: Only works for Vista/Server2008 +
+/* static int GetProcessCmdLine(HANDLE procHandle, char *cmdLineStr, int cmdLineSz) */
+/* { */
+/*   DWORD cmdLineSzWritten = (DWORD)cmdLineSz; */
   
-  // call function to get PROCESS_BASIC_INFORMATION
-  ntqip(procHandle, ProcessBasicInformation, (LPVOID) &procInfo, sizeof(procInfo), NULL);
+/*   if(!QueryFullProcessImageName(procHandle, 0, cmdLineStr, &cmdLineSzWritten)) */
+/*     { */
+/*       CfOut(cf_error, "QueryFullProcessImageName", "Could not get process image name"); */
+/*       snprintf(cmdLineStr, cmdLineSz, "???"); */
+/*       return false; */
+/*     } */
+  
+/*   return true; */
+/* } */
 
-  FreeLibrary(ntdll);
 
-  // read PEB structure
-  if(!ReadProcessMemory(procHandle, (LPVOID) procInfo.PebBaseAddress, (LPVOID) &envBlock, sizeof(PEB), NULL))
-    {
-      CfOut(cf_error,"ReadProcessMemory","!! Could not read process environment block");
-      return false;
-    }
-
-  // read PROCESS_PARAMETERS
-  if(!ReadProcessMemory(procHandle, (LPVOID) envBlock.ProcessParameters, (LPVOID) &procParams, sizeof(RTL_USER_PROCESS_PARAMETERS), NULL))
-    {
-      CfOut(cf_error,"ReadProcessMemory","!! Could not read process parameters");
-      return false;
-    }
-
-  // read process commandline + terminating null (2 bytes in unicode)
-  wmemset(cmdLineWstr, '\0', cmdLineSz);
-  if(!ReadProcessMemory(procHandle, (LPVOID) procParams.CommandLine.Buffer, (LPVOID) cmdLineWstr, sizeof(cmdLineWstr), &read))
-    {
-      CfOut(cf_error,"ReadProcessMemory","!! Could not read process command line");
-      return false;
-    }
-
-  // format the command-line as a multibyte string instead of wide string
-  wcstombs(cmdLineStr, cmdLineWstr, cmdLineSz);
-
-  return true;
-}
-
+/*****************************************************************************/
 
 static void GetProcessTime(HANDLE procHandle, char *timeCreateStr, ULARGE_INTEGER *timeCpuInt, char *timeCpuStr)
 {
@@ -336,6 +300,7 @@ static void GetProcessTime(HANDLE procHandle, char *timeCreateStr, ULARGE_INTEGE
   FormatCpuTime(&timeKernel, &timeUser, timeCpuInt, timeCpuStr);
 }
 
+/*****************************************************************************/
 
 // creation time shold be hh:mm if started the same date,
 // the date as MmmDD if started the same year, and YYYY otherwise
@@ -371,6 +336,7 @@ static void FormatCreationTime(FILETIME *timeCreate, char *timeCreateStr)
     }
 }
 
+/*****************************************************************************/
 
 /* adds user and kernel time, puts it in the int argument,
  *  formats this sum as hh:mm and puts it into the string argument */
@@ -398,6 +364,7 @@ static void FormatCpuTime(FILETIME *timeKernel, FILETIME *timeUser, ULARGE_INTEG
   sprintf(timeCpuStr, "%02d:%02d", timeCpuFormat.wHour, timeCpuFormat.wMinute);
 }
 
+/*****************************************************************************/
 
 static void GetMemoryInfo(HANDLE procHandle, char *memSzStr, DWORDLONG totalPhysMemB)
 {
@@ -438,6 +405,7 @@ static void GetMemoryInfo(HANDLE procHandle, char *memSzStr, DWORDLONG totalPhys
   sprintf(memSzStr, "%4.1f %8lu %8lu", (memInfo.WorkingSetSize * 100.0) / totalPhysMemB, vmemInfo.VirtualSize / 1024, memInfo.WorkingSetSize / 1024);
 }
 
+/*****************************************************************************/
 
 static void GetProcessCpuTime(DWORD pid, ULARGE_INTEGER *timeCpuInt)
 {
@@ -480,121 +448,59 @@ static void GetProcessCpuTime(DWORD pid, ULARGE_INTEGER *timeCpuInt)
   timeCpuInt->QuadPart = tmp1.QuadPart;
 }
 
+/*****************************************************************************/
 
-// FIXME: Cannot get username of "svchost -k rpcss" and "svchost.exe -k LocalService":
-// uses logon SID (we now print string SID instead)
-static void GetProcessUserName(HANDLE hProcess, int incDomain, char *nameStr, int nameStrSz)
-{
-  SECURITY_DESCRIPTOR *pSecDescr = NULL;
-  DWORD dwSecInfoSize;
-  BOOL bDefaulted;
-  PSID psidUser;
-  char userName[CF_BUFSIZE];
-  char domainName[CF_BUFSIZE];
-  char *stringSID;
+// NOTE: OpenProcessToken fails on some processes if we're not running as a service
+void GetProcessUserName(HANDLE procHandle, int incDomain, char *nameStr, int nameStrSz)
+{ 
+  char userName[CF_BUFSIZE], domainName[CF_BUFSIZE], tokenBuf[CF_BUFSIZE]; 
+  HANDLE token; 
+  SID_NAME_USE accType; 
+  DWORD minLen; 
+  DWORD unLen = sizeof(userName); 
+  DWORD dnLen = sizeof(domainName); 
+  TOKEN_USER *userToken = (TOKEN_USER *)tokenBuf;
 
-  memset(nameStr, 0, sizeof(nameStrSz));
+  memset(nameStr, 0, nameStrSz);
   strcpy(nameStr, "???");  // in case of error
 
-  // Get Security Descriptor size
-  GetKernelObjectSecurity(hProcess, OWNER_SECURITY_INFORMATION, pSecDescr, 0, &dwSecInfoSize);
+  if(!OpenProcessToken(procHandle, TOKEN_QUERY, &token))
+    { 
+      // NOTE: we often get "access denied" when we're not running as a service
+      Debug("Could not get process user information (often happens when we're not a service) - OpenProcessToken() failed with code %lu\n", GetLastError());
+      return;
+    } 
 
-  // Call should have failed due to zero-length buffer.
-  if(GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+  // get token SID 
+  if(!GetTokenInformation(token, TokenUser, userToken, sizeof(tokenBuf), &minLen)) 
+    { 
+      CfOut(cf_error, "GetTokenInformation", "!! Could not get process token information");
+      CloseHandle(token); 
+      return; 
+    } 
+
+  // get SID account and domain name 
+  if(!LookupAccountSid(0, userToken->User.Sid, userName, &unLen, domainName, &dnLen, &accType)) 
+    { 
+      CfOut(cf_error, "LookupAccountSid", "!! Could not look up account name");
+      CloseHandle(token); 
+      return;
+    }  
+
+  // copy names to argument buffer
+
+  if(incDomain && dnLen) 
     {
-
-      // Security Descriptor allocation
-      pSecDescr = (SECURITY_DESCRIPTOR *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(BYTE) * dwSecInfoSize);
-
-      if (pSecDescr == NULL)
-	{
-	  CfOut(cf_error,"HeapAlloc","!! Could not allocate memory for security descriptor");
-	  return;
-	}
-
-      // Get Security Descriptor (real call)
-      if (GetKernelObjectSecurity(hProcess, OWNER_SECURITY_INFORMATION, pSecDescr, dwSecInfoSize, &dwSecInfoSize))
-	{
-	  // Get descriptor owner SID
-	  if (GetSecurityDescriptorOwner(pSecDescr, &psidUser, &bDefaulted))
-	    {   
-	      // Converts SID to Domain + Username
-	      if (SidToUser(psidUser, userName, sizeof(userName), domainName, sizeof(domainName)))
-		{
-		  if(incDomain)
-		    {
-		      strcpy(nameStr, domainName);
-		      strcat(nameStr, "\\");
-		    }
-		  else
-		    {
-		      *nameStr = '\0';
-		    }
-
-		  strcat(nameStr, userName);
-		  
-		  HeapFree (GetProcessHeap(), 0, pSecDescr);
-		  return;
-		}
-	      else
-		{
-		  Debug("Failed to convert SID to user name, using SID instead");
-
-		  // This is usually Session Logon SID, S-1-5-5-#-# http://blog.delphi-jedi.net/2008/03/10/what-is-a-sid/
-		  
-		  if(ConvertSidToStringSid(psidUser, &stringSID))
-		    {
-		      strncpy(nameStr, stringSID, nameStrSz);
-		      LocalFree(stringSID);
-		    }
-		}
-	    }
-	  else
-	    {
-	      CfOut(cf_error,"GetSecurityDescriptorOwner","!! Could not get owner security descriptor");
-	    }
-	}
-      else
-	{
-	  CfOut(cf_error,"GetKernelObjectSecurity","!! Could not get kernel security information for process");
-	}
-    }
+      snprintf(nameStr, nameStrSz, "%s\\\\%s", domainName, userName);
+    } 
   else
     {
-      CfOut(cf_error,"GetKernelObjectSecurity","!! Could not get kernel security information for process");
+      snprintf(nameStr, nameStrSz, "%s", userName);
     }
 
-
-  if (pSecDescr != NULL)
-    HeapFree (GetProcessHeap(), 0, pSecDescr);
-
-  return;
+  CloseHandle(token);
 }
 
 
-static BOOL SidToUser(PSID pSid, LPTSTR szUser, DWORD dwUserLen, LPTSTR szDomain, DWORD dwDomainLen)
-{
-  SID_NAME_USE snuSIDNameUse;
-  DWORD dwULen;
-  DWORD dwDLen;
-
-  dwULen = dwUserLen;
-  dwDLen = dwDomainLen;
-
-  // Get user and domain name based on SID
-  if (LookupAccountSid(NULL, pSid, szUser, &dwULen, szDomain, &dwDLen, &snuSIDNameUse))
-    {
-      // LocalSystem processes are incorrectly reported as owned by BUILTIN\Administrators
-      // We modify that behavior to conform to standard taskmanager
-      if (lstrcmpi(szDomain, TEXT("builtin")) == 0 && lstrcmpi(szUser, TEXT("administrators")) == 0)
-	{
-	  strncpy(szUser, TEXT("SYSTEM"), dwUserLen);
-	  strncpy(szDomain, TEXT("NT AUTHORITY"), dwDomainLen);
-	}
-			
-      return true;
-    }
-  return false;
-}
 
 #endif  /* MINGW */
