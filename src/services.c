8aede5b936c209235ee5d00e3a341495597461e4
Mark Burgess <mark@cfengine.com>
2009-12-04 12:39:08 +0000

    services promise stubs

---
+++ src/services.c
@@ -0,0 +1,24 @@
+
+/*
+
+ This file is (C) Cfengine AS. See COSL LICENSE for details.
+
+*/
+
+/*****************************************************************************/
+/*                                                                           */
+/* File: services.c                                                          */
+/*                                                                           */
+/*****************************************************************************/
+
+#include "cf3.defs.h"
+#include "cf3.extern.h"
+#include "cf.nova.h"
+
+
+void Nova_VerifyServices(struct Attributes a,struct Promise *pp)
+
+{
+
+// if (a.service.service_type ....)
+}
