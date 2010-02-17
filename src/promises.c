4592efc8ef53aee8d2121643517840395bed218a
Eystein Måløy Stenberg <eystein@cfengine.com>
2010-02-17 13:14:20 +0000

    semicolon too much

--- src/promises.c
+++ src/promises.c
@@ -395,7 +395,7 @@ fprintf(fout,
 "body common control\n"
 "{\n"
 "bundlesequence => { \"update\" };\n"
-"host_licenses_paid => \"1\";\n";
+"host_licenses_paid => \"1\";\n"
 "}\n"
 "bundle agent update\n"
 "{\n"
