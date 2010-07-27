a082fd60aa54d9a47c282d091ad576f2cde86a97
Mark Burgess <mark@cfengine.com>
2010-07-27 18:26:39 +0000

    Module integration

--- src/web_api.c
+++ src/web_api.c
@@ -19,3 +19,53 @@
 
 /*****************************************************************************/
 
+int Nova2PHP_software_report(char *name,char *value, char *arch,int regex,char *returnval,int bufsize)
+
+{ char *report,buffer[CF_BUFSIZE];
+  struct HubSoftware *hs;
+  struct HubQuery *hq;
+  struct Rlist *rp,*result;
+  int count = 0, tmpsize;
+  mongo_connection dbconn;
+  bson query,b;
+
+if (!CFDB_Open(&dbconn, "127.0.0.1", 27017))
+   {
+   CfOut(cf_error, "", "!! Could not open connection to report database");
+   return false;
+   }
+
+hq = CFDB_QuerySoftware(&dbconn,bson_empty(&b),name,value,arch,false);
+
+returnval[0] = '\0';
+
+strcat(returnval,"<table>\n");
+count += strlen(returnval);
+
+for (rp = hq->records; rp != NULL; rp=rp->next)
+   {
+   hs = (struct HubSoftware *)rp->item;
+   snprintf(buffer,CF_BUFSIZE,"<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n",hs->name,hs->version,Nova_LongArch(hs->arch));
+
+   tmpsize = strlen(buffer);
+   
+   if (count + tmpsize > bufsize - strlen("</table>\n") - 1)
+      {
+      break;
+      }
+   
+   strcat(returnval,buffer);
+   count += tmpsize;
+   }
+
+strcat(returnval,"</table>\n");
+
+DeleteHubQuery(hq,DeleteHubSoftware);
+
+if (!CFDB_Close(&dbconn))
+   {
+   CfOut(cf_error, "", "!! Could not close connection to report database");
+   }
+
+return true;
+}
