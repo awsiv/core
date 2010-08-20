c3c52d1b604869dbe832331be7c99aeba7a68c1f
Eystein Måløy Stenberg <eystein@cfengine.com>
2010-08-20 14:41:22 +0000

    fixed regexp (again)

--- GUI/promise.php
+++ GUI/promise.php
@@ -5,6 +5,7 @@ $handle = $_POST['handle'];
 if ($hostkey == "")
    {
    $handle = $_GET['handle'];
+   $expand = $_GET['expand'];
    }
 
 $title = "promise $handle";
@@ -20,31 +21,37 @@ $pid = cfpr_get_pid_for_topic("promises","$handle");
 <?php 
 $mybundle = cfpr_get_promise_bundle($handle);
 echo "<h4>Other promises in bundle <a href=\"bundle.php?bundle=$mybundle\"><span id=\"bundle\">$mybundle</span></a></h4>";
-$allhandles = cfpr_list_handles_for_bundle($mybundle,"agent"); 
+$allhandles = cfpr_list_handles_for_bundle($mybundle,"agent",false); 
 echo "$allhandles"; 
 
 echo "<h4>Other promises by promiser</h4>";
 
 $promiser = cfpr_get_promiser($handle);
-$allhandles = cfpr_list_handles($promiser,""); 
+$allhandles = cfpr_list_handles($promiser,"",false);
 echo "$allhandles";
 
 $type = cfpr_get_promise_type($handle);
 echo "<h4>Other promises of type $type</h4>";
-$allhandles = cfpr_list_handles("",$type); 
+$allhandles = cfpr_list_handles("",$type,false);
 echo "$allhandles"; 
-?>
 
-</td>
-<td valign="top">
-  <h2>Promise definition: </h2>
+echo "</td>";
 
-<p>
+echo "<td valign=\"top\">
+  <h2>Promise definition (<a href=\"promise.php?handle=$handle&expand=1\">expand</a>): </h2>
 
-<?php 
+<p>";
+
 $promise = cfpr_summarize_promise($handle);
 
 echo "$promise<p>";
+
+if($expand == "1")
+{
+  echo "<h3>Expanded promise: </h3>";
+  //$expandedPromise = cfpr_summarize_promise_exp($handle);
+  //echo "$expandedPromise<p>";
+}
 
 if ($pid > 0) 
    {
