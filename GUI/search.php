84ae9b9622fd7e04df99cc214af89e37a3082a8d
Eystein Måløy Stenberg <eystein@cfengine.com>
2010-10-12 08:44:42 +0000

    knowledge link

--- GUI/search.php
+++ GUI/search.php
@@ -24,7 +24,7 @@ include 'header.php';
               <li><a href="index.php">SUMMARY</a></li>
               <li><a href="helm.php">PLANNING</a></li>
               <li><a href="status.php" class="current">STATUS</a></li>
-              <li><a href="knowledgw.php">LIBRARY</a></li>
+              <li><a href="knowledge.php">LIBRARY</a></li>
              </ul>
              <span id="status" class="grid_2 alignright">
              Login:Max Manus
