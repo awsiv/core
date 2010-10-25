3e1c4ef3cef3db14c88ed30192c9c088c870b1ec
Sudhir Pandey <sudhir.pandey@cfengine.com>
2010-10-25 14:44:03 +0000

    session support to support multi users

--- GUI/policy/checksyntax.php
+++ GUI/policy/checksyntax.php
@@ -1,6 +1,7 @@
 <?php
 //$filename=$_POST['file']||$_GET['file'];
-$path=realpath('../policies/promises.cf');
+session_start();
+$path=realpath('../policies/'.session_id().'/promises.cf');
 $result=cfpr_validate_policy($path);
 $details=array(
 		'result'=>$result
