fdf87e98a0f3500d2c400e4eb2df5e12919da0f5
Sudhir Pandey <sudhir.pandey@cfengine.com>
2010-10-28 08:37:16 +0000

    adjusted to catch images from images folder

--- GUI/helm.php
+++ GUI/helm.php
@@ -13,14 +13,14 @@ cfpr_menu("Planning : menu");
                 <div class="panelcontent">
      
     <ul class="grid_6">
-        <li><a href="services.php"><img src="/img/goal.png" class="align" /><span class="imglabel">Enterprise goals and Service Catalogue</span></a></li>
-        <li><a href="Cfeditor.php"><img src="/img/tool.png" class="align" /><span class="imglabel">Develop and work on policy</span></a></li>
-        <li><a href="knowledge.php?topic=security view"><img src="/img/security.png" class="align" /><span class="imglabel">Security</span></a></li>
+        <li><a href="services.php"><img src="images/goal.png" class="align" /><span class="imglabel">Enterprise goals and Service Catalogue</span></a></li>
+        <li><a href="Cfeditor.php"><img src="images/tool.png" class="align" /><span class="imglabel">Develop and work on policy</span></a></li>
+        <li><a href="knowledge.php?topic=security view"><img src="images/security.png" class="align" /><span class="imglabel">Security</span></a></li>
     </ul>
     <ul class="grid_6">
-        <li><a href="search.php?report=Business%20value%20report&manyhosts=1"><img src="/img/info.png" class="align" /><span class="imglabel">Latest enterprise cost and value</span></a></li>
-        <li><a href="knowledge.php?topic=best.*practice"><img src="/img/library.png" class="align" /><span class="imglabel">Standards and practices</span></a></li>
-        <li><a href="knowledge.php?topic=policy.*"><img src="/img/library.png" class="align" /><span class="imglabel">Policy guidance</span></a></li>
+        <li><a href="search.php?report=Business%20value%20report&manyhosts=1"><img src="images/info_services.png" class="align" /><span class="imglabel">Latest enterprise cost and value</span></a></li>
+        <li><a href="knowledge.php?topic=best.*practice"><img src="images/library.png" class="align" /><span class="imglabel">Standards and practices</span></a></li>
+        <li><a href="knowledge.php?topic=policy.*"><img src="images/library.png" class="align" /><span class="imglabel">Policy guidance</span></a></li>
     </ul>
     <div class="clearleft"></div>
     </div> 
