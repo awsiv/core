c6c008b4c7bce7bec83d5fb4e786f99aafc7df9a
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2011-01-28 12:50:22 +0000

    temporary file to show and add comments (must be removed later)

---
+++ GUI/comments.php
@@ -0,0 +1,17 @@
+<?php
+$comment_id = $_GET['comment_id'];
+$hostkey = $_GET['hostkey'];
+$report_type = $_GET['report_type'];
+$report_data = $_GET['report_data'];
+
+if($comment_id == "NO_COMMENT")
+{
+/* show form */
+}
+else
+{
+/* show comment*/
+  $comment	
+}
+
+?>
