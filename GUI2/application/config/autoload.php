e19e67f702390095f11db74686af68671ffd800e
Sudhir Pandey <sudhir.pandey@cfengine.com>
2011-02-01 13:31:09 +0000

    new logo added

--- GUI2/application/config/autoload.php
+++ GUI2/application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('template');
+$autoload['libraries'] = array('template','session');
 
 
 /*
