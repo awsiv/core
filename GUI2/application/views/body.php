23560f52a85e0033124ec98d1f509f42c1fc1916
Sudhir Pandey <sudhir.pandey@cfengine.com>
2011-02-03 20:02:44 +0000

    new view for body.php in old old gui

---
+++ GUI2/application/views/body.php
@@ -0,0 +1,17 @@
+<div id="tabpane">
+         <div class="grid_5">
+         <div class="panel"><div class="panelhead">Other bodies of type <?php echo $type?></div>
+             <div class="panelcontent">
+             <?php echo $allbodies?>
+             </div></div>
+
+         </div>
+         <div class="grid_7">
+
+             <div class="panel"><div class="panelhead">Body definition:</div>
+             <div class="panelcontent"><div class="tables">
+             <?php echo $def?>
+             </div></div></div>
+            
+         </div>
+     </div>
