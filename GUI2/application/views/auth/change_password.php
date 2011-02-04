6c6c9938fb154271412cb8e5d9a19b87645f884d
Sudhir Pandey <sudhir.pandey@cfengine.com>
2011-02-04 11:04:35 +0000

    change password workable version might change in future

--- GUI2/application/views/auth/change_password.php
+++ GUI2/application/views/auth/change_password.php
@@ -1,8 +1,8 @@
-<h1>Change Password</h1>
+<div class="panelhead">Change password</div>
 
 <div id="infoMessage"><?php echo $message;?></div>
 
-<?php echo form_open("auth/change_password");?>
+<?php echo form_open("auth/change_password/".$this->uri->segment(3), array('id'=>'change_password'));?>
 
       <p>Old Password:<br />
       <?php echo form_input($old_password);?>
@@ -15,8 +15,7 @@
       <p>Confirm New Password:<br />
       <?php echo form_input($new_password_confirm);?>
       </p>
-      
-      <?php echo form_input($user_id);?>
+
       <p><?php echo form_submit('submit', 'Change');?></p>
-      
+
 <?php echo form_close();?>
