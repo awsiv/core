5bde74270c3ccdd194a956653bbd5a5a8fe8be9d
Sudhir Pandey <sudhir.pandey@cfengine.com>
2011-02-08 12:16:53 +0000

    codeigniter2.0 needs this core if extended to beplaced in a seperate directory core

---
+++ GUI2/application/core/Cf_Controller.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+// Code here is run before ALL controllers
+class Cf_Controller extends CI_Controller
+{	
+	function __construct()
+	{
+		parent::__construct();		
+		
+		$this->load->library('ion_auth');
+		$this->load->library('session');
+		$this->load->helper(array('url','html'));
+		///cfpr_set_siteurl(site_url());
+		// Login: If not logged in, redirect to  authentication index
+		if(!$this->ion_auth->logged_in())
+		{
+			redirect('auth/index','refresh');
+		}
+                
+	}
+}
+
+?>
