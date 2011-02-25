bf8900c300c27a4330582489074feaefa6e68caa
Sudhir Pandey <sudhir.pandey@cfengine.com>
2011-02-25 13:27:33 +0000

    Text changed

--- GUI2/application/views/includes/menu.php
+++ GUI2/application/views/includes/menu.php
@@ -1,7 +1,7 @@
  <div id="nav">
              <ul class="grid_10">
-             <li><a href="<?php echo site_url()?>/welcome/index" class="<?php $text=isset($summary)?$summary:""; echo $text ?>">SUMMARY</a></li>
-			 <li><a href="<?php echo site_url()?>/welcome/status" class="<?php $text=isset($status)?$status:""; echo $text ?>">STATUS</a></li>
+             <li><a href="<?php echo site_url()?>/welcome/index" class="<?php $text=isset($summary)?$summary:""; echo $text ?>">Dash board</a></li>
+	     <li><a href="<?php echo site_url()?>/welcome/status" class="<?php $text=isset($status)?$status:""; echo $text ?>">STATUS</a></li>
              <li><a href="<?php echo site_url()?>/welcome/helm" class="<?php $text=isset($planning)?$planning:""; echo $text ?>">PLANNING</a></li>
              <li><a href="<?php echo site_url()?>/welcome/knowledge" class="<?php $text=isset($library)?$library:""; echo $text ?>">KNOWLEDGE</a></li>
              <?php if(in_array('admin', $this->session->userdata('group') )) {?><li><a href="<?php echo site_url()?>/auth/index" class="<?php $text=isset($admin)?$admin:""; echo $text ?>">ADMIN</a></li><?php }?>
