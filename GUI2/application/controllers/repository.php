a92f4172ec25b3bb7c9b1578d8bc17aceac311a2
Nishes Joshi <nishes.joshi@cfengine.com>
2011-05-20 13:27:18 +0000

    update repository model and classes. Encryption included with mcrypt module.

--- GUI2/application/controllers/repository.php
+++ GUI2/application/controllers/repository.php
@@ -9,37 +9,43 @@ class Repository extends CI_Controller {
 
     function insertRepository() {
         $info = array('userId' => 'sudhir', 'repoPath' => 'http://test/test2', 'username' => 'something', 'password' => 'password');
-        $return = $this->repository_model->insertRepository($info);
+        $return = $this->repository_model->insert_repository($info);
         if (!$return) {
-            var_dump($this->repository_model->getErrors());
+            var_dump($this->repository_model->get_errors());
+        } else {
+            var_dump($return);
         }
     }
 
     function getRepository() {
-        $repo = $this->repository_model->getAllRepository();
-        print_r($repo);
+        $repo = $this->repository_model->get_all_repository();
+        foreach ($repo as $obj) {
+            $info = array('userId' => $obj['userId'], 'password' => $obj['password']);
+            $pass = $this->repository_model->decrypt_password($info);
+            //var_dump($obj['password']);
+            echo "Enc pass :: " . $obj['password'] . " original pass :: " . $pass;
+            echo "<br>";
+        }
     }
 
     function updateRepository() {
         $info = array('userId' => 'sudhir', 'repoPath' => 'http://test/test2', 'username' => 'something', 'password' => 'password');
         $newInfo = array('userId' => 'sudhir', 'repoPath' => 'http://test/test2/new', 'username' => 'newsomething', 'password' => 'newpassword');
-        $result = $this->repository_model->updateRepository($info, $newInfo);
+        $result = $this->repository_model->update_repository($info, $newInfo);
         if (!$result) {
-            var_dump($this->repository_model->getErrors());
+            var_dump($this->repository_model->get_errors());
         }
     }
 
     function deleteRepository() {
 
         $info = array('userId' => 'sudhir', 'repoPath' => 'http://test/test2', 'username' => 'something', 'password' => 'password');
-        $result = $this->repository_model->deleteRepository($info);
+
+        $result = $this->repository_model->delete_repository();
         if (!$result) {
-            var_dump($this->repository_model->getErrors());
+            var_dump($this->repository_model->get_errors());
         }
     }
-
-
-    
 
 }
 
--- GUI2/application/models/repository_model.php
+++ GUI2/application/models/repository_model.php
@@ -15,7 +15,9 @@ class Repository_model extends CI_Model {
      * Get all added repository for the user
      * @param <type> $userId 
      */
-    function getAllRepository($userId = '') {
+    function get_all_repository($userId = '') {
+
+        if (trim($userId)) $this->mongo_db->where(array('username'=>$userId));
         return $this->mongo_db->get($this->collectionName);
     }
 
@@ -24,17 +26,21 @@ class Repository_model extends CI_Model {
      * @param <type> $repoInfo
      * @return bool
      */
-    function insertRepository($repoInfo = array()) {
+    function insert_repository($repoInfo = array()) {
+
 
         // validate data
         if (!is_array($repoInfo) && count($repoInfo) == 0) {
             $this->setError('Invalid Parameter passed.');
             return FALSE;
         }
-        if ($this->checkDuplicateEntries($repoInfo)) {
-            $this->setError('Repository already exists.');
+        if ($this->check_duplicate_entries($repoInfo)) {
+            $this->set_error('Repository already exists.');
             return FALSE;
         }
+
+        $repoInfo['password'] = $this->encrypt_password($repoInfo);
+
         $id = $this->mongo_db->insert($this->collectionName, $repoInfo);
     }
 
@@ -44,14 +50,14 @@ class Repository_model extends CI_Model {
      * @param array $newRepoInfo new info for update
      * @return bool 
      */
-    function updateRepository($repoInfo=array(), $newRepoInfo=array()) {
+    function update_repository($repoInfo=array(), $newRepoInfo=array()) {
         if (!is_array($repoInfo) && count($repoInfo) == 0) {
-            $this->setError('Invalid Parameter passed.');
+            $this->set_error('Invalid Parameter passed.');
             return FALSE;
         }
 
-        if (!$this->checkDuplicateEntries($repoInfo)) {
-            $this->setError('Cannot find existing repository information to update');
+        if (!$this->check_duplicate_entries($repoInfo)) {
+            $this->set_error('Cannot find existing repository information to update');
             return FALSE;
         }
 
@@ -66,7 +72,7 @@ class Repository_model extends CI_Model {
      * @param <array> $repoInfo repository to delete
      * @return bool
      */
-    function deleteRepository($repoInfo) {
+    function delete_repository($repoInfo = array()) {
         return $this->mongo_db->where($repoInfo)->delete_all($this->collectionName);
     }
 
@@ -75,28 +81,53 @@ class Repository_model extends CI_Model {
      * @param <array> $repoInfo
      * @return bool
      */
-    function checkDuplicateEntries($repoInfo) {
+    function check_duplicate_entries($repoInfo) {
 
         return $this->mongo_db->where(array('userId' => $repoInfo['userId'], 'repoPath' => $repoInfo['repoPath']))->count($this->collectionName) > 0;
     }
 
-    function hashPassword($userInfo) {
+    /**
+     * get key for Encryption
+     * @param <type> $userInfo 
+     */
+    function get_key($userInfo) {
+        $obj = $this->mongo_db->select(array('username', 'id', 'password', 'group'))
+                        ->where(array('username' => $userInfo['userId'], 'active' => 1))
+                        ->limit(1)
+                        ->get_object('users');
 
-        
-        
+        return hash('sha256', $obj->_id, TRUE);
     }
 
+    /**
+     * Hash the passowrd
+     * @param <type> $userInfo 
+     */
+    function encrypt_password($userInfo) {
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $key = $this->get_key($userInfo);
+        $text = $userInfo['password'];
+        $cryptpass = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_ECB, $iv);
+        return base64_encode($cryptpass);
+    }
 
-    function setError($msg) {
+    function decrypt_password($userInfo) {
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        return trim(
+                mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->get_key($userInfo), base64_decode($userInfo['password']),
+                        MCRYPT_MODE_ECB,
+                        $iv));
+    }
+
+    function set_error($msg) {
         $this->errors[] = $msg;
     }
 
-    function getErrors() {
+    function get_errors() {
         return $this->errors;
     }
-
-
-
 
 }
 
