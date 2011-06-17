da26c65e0073ef3689f8cd16855e43aec2b9bb36
Sudhir Pandey <sudhir.pandey@cfengine.com>
2011-06-17 14:52:41 +0000

    for packaging

---
+++ GUI2/application/views/searchpages/paging_footer.php
@@ -0,0 +1,32 @@
+
+             <div class="Paging">
+                    <div class="pages">
+                        <div class="inside">
+                            <a href="<?= site_url('search/index/' . $params . 'rows/' . $number_of_rows . '/page/' . $pg['first']) ?>" title="Go to First Page" class="first"><span>First</span></a>
+                            <a href="<?= site_url('search/index/' . $params . 'rows/' . $number_of_rows . '/page/' . $pg['prev']) ?>" title="Go to Previous Page" class="prev"><span><</span></a>
+
+            <?php
+                for ($i = $pg['start']; $i <= $pg['end']; $i++) {
+                    if ($i == $pg['page'])
+                        $current = 'current'; else
+                        $current="";
+            ?>
+
+                        <a href="<?= site_url("search/index/" . $params . "page/$i") ?>" title="Go to Page <?= $i ?>" class="page <?= $current ?>"><span><?= $i ?></span></a>
+
+                    <? } ?>
+
+                        <a href="<?= site_url('search/index/' . $params . 'rows/' . $number_of_rows . '/page/' . $pg['next']) ?>" title="Go to Next Page" class="next"><span>></span></a>
+                        <a href="<?= site_url('search/index/' . $params . 'rows/' . $number_of_rows . '/page/' . $pg['last']) ?>" title="Go to Last Page" class="last"><span>Last</span></a>
+                    </div>
+                </div>
+
+            </div>
+           <div>
+            <?
+                echo form_open('search/index/' . $params);
+                echo form_input('rows', $number_of_rows);
+                echo "Rows/Page";
+                echo form_close();
+            ?>
+       </div>
