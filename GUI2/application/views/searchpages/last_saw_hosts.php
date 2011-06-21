b1bf9ebaff4b699cd26861f94b7717b31bf7b1ff
Eystein Måløy Stenberg <eystein@cfengine.com>
2011-06-21 23:41:29 +0000

    improved names for Lastseen report query

--- GUI2/application/views/searchpages/last_saw_hosts.php
+++ GUI2/application/views/searchpages/last_saw_hosts.php
@@ -1,10 +1,10 @@
  <div id="searchform" class="panel"><div class="panelhead"><?php echo $report_type?> query</div>
  <div class="panelcontent">
           <form method="post" action="<?php echo site_url('search')?>">
-          <p>Host name: (.*+[])<input class="searchfield" type="text" name="name"></p>
-          <p>Host address: (.*+[])<input class="searchfield" type="text" name="address"></p>
-          <p>Host's unique key:<input class="searchfield" type="text" name="key"></p>
-          <p>Hours ago:<p><input class="searchfield" type="text" name="ago"></p>
+          <p>Remote host name: (.*+[])<input class="searchfield" type="text" name="name"></p>
+          <p>Remote IP address: (.*+[])<input class="searchfield" type="text" name="address"></p>
+          <p>Remote host key:<input class="searchfield" type="text" name="key"></p>
+          <p>Minimum hours ago:<p><input class="searchfield" type="text" name="ago"></p>
           <p>Host group: (.*+[])
             <a href="<?php echo site_url('widget/allclasses')?>" id="bphghelp" class="help"
              title="<?php echo $this->lang->line('report_hostgp_help');?>">Help ?</a>
