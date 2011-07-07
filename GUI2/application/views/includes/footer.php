1ad9116fa34cb4b6195a86713c6b7ca18a35d072
Sudhir Pandey <sudhir.pandey@cfengine.com>
2011-07-07 11:57:27 +0000

    make the footer text unbreakable in small screens

--- GUI2/application/views/includes/footer.php
+++ GUI2/application/views/includes/footer.php
@@ -1,6 +1,6 @@
 
        <div id="footer">
-              <p class="grid_7 push_1"><?php echo cfpr_enterprise_version();?> running on <?php echo cfpr_community_version();?> Copyright &copy; 2011 CFEngine AS - All rights reserved</p>
+              <p class="grid_10 push_1"><?php echo cfpr_enterprise_version();?> running on <?php echo cfpr_community_version();?> Copyright &copy; 2011 CFEngine AS - All rights reserved</p>
           </div>
         </div>
 </body>
