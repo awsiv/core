1b195d3429e5424414fa5a0bc3c9c85d3573db14
Mark Burgess <mark@cfengine.com>
2011-07-15 08:55:49 +0000

    Added some help text

--- src/web_api.c
+++ src/web_api.c
@@ -439,7 +439,8 @@ char *Nova2PHP_get_help_text(char *key)
     {"class finder","Find hosts by the class contexts they occupy in the local cluster."},
     {"policy finder","Find promises that make up the policy in the local cluster."},
     {"reports finder","Search and browse the available reports in the local cluster."},
-
+    {"summary finder","Show the current state of knowledge about promise compliance. If a single promise handle is specified, this returns the number of hosts matching a class regex that is compliant. If a regex is given for the promise the result is an expectation value of number of hosts wrt all the matching promises."},
+    
     {"license report","Displays a summary of the utilization of CFEngine licenses for this cluster."},
     {"vital signs","Performance measurements that characterize the observed runtime state of a host."},
     {"problems","An incarnation of the promises not kept summary for a host, showing the most frequent repeated incidents in relation to the approved policy."},
