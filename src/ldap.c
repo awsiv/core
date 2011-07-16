23dbd6fa8bb27ee1319ae8f7cf4b906e45697ee9
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-07-16 10:42:59 +0000

    Add missing semicolon to prototype

--- src/ldap.c
+++ src/ldap.c
@@ -19,7 +19,7 @@
 
 /* Prototypes */
 
-int CfLDAPAuthenticate(char *uri,char *basedn,char *passwd)
+int CfLDAPAuthenticate(char *uri,char *basedn,char *passwd);
 LDAP *NovaQueryLDAP(char *uri,char *basedn,char *sec,char *pwd);
 void *CfLDAPValue(char *uri,char *basedn,char *filter,char *name,char *scopes,char *sec);
 void *CfLDAPList(char *uri,char *basedn,char *filter,char *name,char *scopes,char *sec);
