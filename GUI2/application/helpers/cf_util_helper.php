98d416295db472ffa4e9091048c1d498c7d7f0a2
Nishes Joshi <nishes.joshi@cfengine.com>
2011-08-04 11:59:28 +0000

    tooltip doesnt shows any text as well when you turn off the tooltip option in preferences.

--- GUI2/application/helpers/cf_util_helper.php
+++ GUI2/application/helpers/cf_util_helper.php
@@ -115,7 +115,7 @@ function host_only_table($data_array) {
             //$keys = array_keys($cols); if key valuesupplied in format{"key":"","id":""}
             array_push($cells, anchor('welcome/host/' . $cols['hostkey'], $cols['hostname'], 'class="imglabel"'));
             //use first key as param and second key as text as keys tend to change like key or hostkey, hostname or id etc
-           // array_push($cells, anchor('welcome/host/' . $cols[1], $cols[0], 'class="imglabel"') . anchor('visual/vital/' . $cols[1], "View vitals", 'class="viewvitalslnk"'));
+            // array_push($cells, anchor('welcome/host/' . $cols[1], $cols[0], 'class="imglabel"') . anchor('visual/vital/' . $cols[1], "View vitals", 'class="viewvitalslnk"'));
         }
     }
     if (count($cells) > 0) {
@@ -133,21 +133,20 @@ function host_only_table($data_array) {
     return $table;
 }
 
-function getonlineusernames()
-    {
-       $onlineuser=array();
-        $CI = get_instance();
-        $CI->load->library(array('mongo_db','ion_auth'));
-        $result=$CI->mongo_db->get('onlineusers');
-         foreach ($result as  $docs) {
-            $obj=(object)$docs;
-            //$user=$CI->ion_auth->get_user($obj->user_id);
-           // array_push($onlineuser, $user->username);
-               array_push($onlineuser, $obj->user_id);
-           //var_dump($user);
-         }
-       return $onlineuser;
+function getonlineusernames() {
+    $onlineuser = array();
+    $CI = get_instance();
+    $CI->load->library(array('mongo_db', 'ion_auth'));
+    $result = $CI->mongo_db->get('onlineusers');
+    foreach ($result as $docs) {
+        $obj = (object) $docs;
+        //$user=$CI->ion_auth->get_user($obj->user_id);
+        // array_push($onlineuser, $user->username);
+        array_push($onlineuser, $obj->user_id);
+        //var_dump($user);
     }
+    return $onlineuser;
+}
 
 /**
  * Formats a timestamp and then gives it a color code
@@ -156,23 +155,23 @@ function getonlineusernames()
  * @param <type> $timestamp
  * @return <string> datetime with wrapped in span with colorclass
  */
-function getDateStatus($timestamp,$noColor=false) {
-    
-    $timestamp = intval($timestamp,10);
+function getDateStatus($timestamp, $noColor=false) {
+
+    $timestamp = intval($timestamp, 10);
     $colorClass = '';
     $now = time();
     // 6 hours
-    if ($now - $timestamp > 6*3600) {
+    if ($now - $timestamp > 6 * 3600) {
         $colorClass = 'amber';
     }
     // 7 days
-    if ($now - $timestamp > 7*24*3600) {
+    if ($now - $timestamp > 7 * 24 * 3600) {
         $colorClass = 'red';
     }
-    if ($noColor) $colorClass = '';
-    $formattedDate = date('c',$timestamp);
-    return '<span class="localtime '.$colorClass.'">'.$formattedDate.'</span>';
-    
+    if ($noColor)
+        $colorClass = '';
+    $formattedDate = date('c', $timestamp);
+    return '<span class="localtime ' . $colorClass . '">' . $formattedDate . '</span>';
 }
 
 /**
@@ -181,102 +180,123 @@ function getDateStatus($timestamp,$noColor=false) {
  * @param <type> $_data
  *
  */
-function notifier($url,$_data)
-{
+function notifier($url, $_data) {
     //$_data=array("name"=>"sudhir","login"=>true);
-        $data = array();
-       while(list($n,$v) = each($_data)){
-        $data[] = urlencode($n)."=".urlencode($v);
-       }
-      $data = implode('&', $data);
-       $ch = curl_init();
-       
-        // set URL and other appropriate options
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt ($ch, CURLOPT_POST, true);
-        curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
-        // grab URL and pass it to the browser
-        curl_exec($ch);
+    $data = array();
+    while (list($n, $v) = each($_data)) {
+        $data[] = urlencode($n) . "=" . urlencode($v);
+    }
+    $data = implode('&', $data);
+    $ch = curl_init();
 
-        // close cURL resource, and free up system resources
-        curl_close($ch);
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    // grab URL and pass it to the browser
+    curl_exec($ch);
+
+    // close cURL resource, and free up system resources
+    curl_close($ch);
 }
 
 //checking the format of json data returned from the core before echoing back to user
-    function sanitycheckjson($data,$noecho=false) {
-        //php 5.1.6 only have json_encode and json_decode and red hat only have this 5.1.6 ats stable package so need to support it as well
+function sanitycheckjson($data, $noecho=false) {
+    //php 5.1.6 only have json_encode and json_decode and red hat only have this 5.1.6 ats stable package so need to support it as well
     $data_check = json_decode(utf8_encode($data), true);
-      $CI=&get_instance();
-        if (function_exists("json_last_error")) {
-            if (json_last_error() == 0) {
-                if (!empty($data_check )) {
-                    if($noecho){
-                        return $data_check;
-                    }
-                    echo $data;
-                } else {
-                    $CI->output->set_status_header('400', 'No data Found');
+    $CI = &get_instance();
+    if (function_exists("json_last_error")) {
+        if (json_last_error() == 0) {
+            if (!empty($data_check)) {
+                if ($noecho) {
+                    return $data_check;
                 }
+                echo $data;
             } else {
-                   $CI->output->set_status_header('500', 'Valid Json Data cannot be generated from Promises');
+                $CI->output->set_status_header('400', 'No data Found');
             }
         } else {
-            if ($data_check  == NULL) {
-                $CI->output->set_status_header('500', 'Valid Json Data cannot be generated from Promises');
-            } else {
-                if (is_array($data_check ) && !empty($data_check)) {
-                      if($noecho){
-                        return $data_check;
-                      }
-                    echo $data;
-                } else {
-                    $CI->output->set_status_header('400', 'No data Found');
+            $CI->output->set_status_header('500', 'Valid Json Data cannot be generated from Promises');
+        }
+    } else {
+        if ($data_check == NULL) {
+            $CI->output->set_status_header('500', 'Valid Json Data cannot be generated from Promises');
+        } else {
+            if (is_array($data_check) && !empty($data_check)) {
+                if ($noecho) {
+                    return $data_check;
                 }
+                echo $data;
+            } else {
+                $CI->output->set_status_header('400', 'No data Found');
             }
         }
     }
+}
 
-    function is_execptional_body($body){
-       $list=array('usebundle','edit_line');
-       if(array_search($body, $list) !==FALSE){
-           return True;
-       }
-       return false;
+function is_execptional_body($body) {
+    $list = array('usebundle', 'edit_line');
+    if (array_search($body, $list) !== FALSE) {
+        return True;
     }
+    return false;
+}
 
-    function array_msort($array, $cols,$natsort=false)
-{
+function array_msort($array, $cols, $natsort=false) {
     $colarr = array();
     foreach ($cols as $col => $order) {
         $colarr[$col] = array();
-        foreach ($array as $k => $row) { 
-            $colarr[$col]['_'.$k] = strtolower($row[$col]);
-          }
+        foreach ($array as $k => $row) {
+            $colarr[$col]['_' . $k] = strtolower($row[$col]);
+        }
     }
     $eval = 'array_multisort(';
     foreach ($cols as $col => $order) {
-        $eval .= '$colarr[\''.$col.'\'],'.$order.',';
+        $eval .= '$colarr[\'' . $col . '\'],' . $order . ',';
     }
-    $eval = substr($eval,0,-1).');';
+    $eval = substr($eval, 0, -1) . ');';
     eval($eval);
-   if($natsort)
-    {
-        foreach($cols as $col => $order){
-         natsort($colarr[$col]);
+    if ($natsort) {
+        foreach ($cols as $col => $order) {
+            natsort($colarr[$col]);
         }
     }
     $ret = array();
     foreach ($colarr as $col => $arr) {
         foreach ($arr as $k => $v) {
-            $k = substr($k,1);
-            if (!isset($ret[$k])) $ret[$k] = $array[$k];
+            $k = substr($k, 1);
+            if (!isset($ret[$k]))
+                $ret[$k] = $array[$k];
             $ret[$k][$col] = $array[$k][$col];
         }
     }
-    return  $ret;
-    
+    return $ret;
+}
 
+/**
+ * Lang
+ *
+ * Fetches a language variable and optionally outputs a form label for tooltips
+ *
+ * @access	public
+ * @param	string	the language line
+ * @param	string	the id of the form element
+ * @return	string
+ */
+function tooltip($line, $id = '') {
+    $CI = & get_instance();
+    // if tooltip is disabled in preferences return blank
+    if (!$CI->setting_lib->get_tooltips_status())
+        return '';
+
+    $line = $CI->lang->line($line);
+
+    if ($id != '') {
+        $line = '<label for="' . $id . '">' . $line . "</label>";
+    }
+
+    return $line;
 }
 
 ?>
--- GUI2/application/views/engineering.php
+++ GUI2/application/views/engineering.php
@@ -3,10 +3,10 @@
         <div id="hoststatus" class="minwidth30 innerdiv">
          <p class="title">Host Status (Last hour) </p>
         <ul>
-            <li> <a href="<?php echo site_url('welcome/hosts/red')?>" title="<?php echo lang('tooltip_redhost'); ?>" class="redhost showqtip hoststatuslist"><?php echo $r ?> hosts 20% failure</a> </li>
-            <li> <a href="<?php echo site_url('welcome/hosts/yellow')?>" title="<?php echo lang('tooltip_yellowhost'); ?>"  class="yellowhost showqtip hoststatuslist"><?php echo $y ?> hosts at 20% repaired </a> </li>
-            <li> <a href="<?php echo site_url('welcome/hosts/green')?>" title="<?php echo lang('tooltip_greenhost'); ?>" class="greenhost showqtip hoststatuslist"><?php echo $g ?> hosts greater than 80% compliant </a> </li>
-            <li> <a href="<?php echo site_url('welcome/hosts/blue')?>" title="<?php echo lang('tooltip_bluehost'); ?>" class="bluehost showqtip hoststatuslist"><?php echo $b ?> hosts unreachable</a> </li>
+            <li> <a href="<?php echo site_url('welcome/hosts/red')?>" title="<?php echo tooltip('tooltip_redhost'); ?>" class="redhost showqtip hoststatuslist"><?php echo $r ?> hosts 20% failure</a> </li>
+            <li> <a href="<?php echo site_url('welcome/hosts/yellow')?>" title="<?php echo tooltip('tooltip_yellowhost'); ?>"  class="yellowhost showqtip hoststatuslist"><?php echo $y ?> hosts at 20% repaired </a> </li>
+            <li> <a href="<?php echo site_url('welcome/hosts/green')?>" title="<?php echo tooltip('tooltip_greenhost'); ?>" class="greenhost showqtip hoststatuslist"><?php echo $g ?> hosts greater than 80% compliant </a> </li>
+            <li> <a href="<?php echo site_url('welcome/hosts/blue')?>" title="<?php echo tooltip('tooltip_bluehost'); ?>" class="bluehost showqtip hoststatuslist"><?php echo $b ?> hosts unreachable</a> </li>
         </ul>
 
             <ul>
@@ -26,12 +26,12 @@
         <div id="finder" class="dash innerdiv">
             <p class="title">Finders</p>
             <ul>
-                <li><?php echo anchor('#', 'host', array('class' => 'host showqtip','title'=>lang('tooltip_host_finder'), 'id' => 'findhost')); ?></li>
-                <li><?php echo anchor('widget/allclasses', 'class', array('class' => 'classes showqtip','title'=>lang('tooltip_class_finder'), 'id' => 'findclass')); ?></li>
-                <li><?php echo anchor('widget/allpolicies', 'promises', array('class' => 'policies showqtip','title'=>lang('tooltip_promise_finder'), 'id' => 'findpolicy')); ?></li>
-                <li><?php echo anchor('widget/allreports', 'reports', array('class' => 'report showqtip', 'id' => 'findreport','title'=>lang('tooltip_reports_finder'))); ?></li>
-                <li><?php echo anchor('summaryreports/search', 'summary reports', array('class' => 'summary_report showqtip','id' => 'summaryreport','title'=>lang('tooltip_summary_finder'))); ?></li>
-                 <li><?php echo anchor('widget/cdpreports', 'Cdp reports', array('class' => 'report showqtip', 'id' => 'findcdpreport','title'=>lang('tooltip_reports_finder'))); ?></li>
+                <li><?php echo anchor('#', 'host', array('class' => 'host showqtip','title'=>tooltip('tooltip_host_finder'), 'id' => 'findhost')); ?></li>
+                <li><?php echo anchor('widget/allclasses', 'class', array('class' => 'classes showqtip','title'=>tooltip('tooltip_class_finder'), 'id' => 'findclass')); ?></li>
+                <li><?php echo anchor('widget/allpolicies', 'promises', array('class' => 'policies showqtip','title'=>tooltip('tooltip_promise_finder'), 'id' => 'findpolicy')); ?></li>
+                <li><?php echo anchor('widget/allreports', 'reports', array('class' => 'report showqtip', 'id' => 'findreport','title'=>tooltip('tooltip_reports_finder'))); ?></li>
+                <li><?php echo anchor('summaryreports/search', 'summary reports', array('class' => 'summary_report showqtip','id' => 'summaryreport','title'=>tooltip('tooltip_summary_finder'))); ?></li>
+                 <li><?php echo anchor('widget/cdpreports', 'Cdp reports', array('class' => 'report showqtip', 'id' => 'findcdpreport','title'=>tooltip('tooltip_reports_finder'))); ?></li>
                 
                 <p class="clearleft"></p>
             </ul>
