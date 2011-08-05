358bc93a70d621af39ff3bcc3fbc1c1acf3ba38f
Nishes Joshi <nishes.joshi@cfengine.com>
2011-08-05 12:50:52 +0000

    added a check to redirect a page if there is no javascript enabled.

--- GUI2/application/views/includes/header.php
+++ GUI2/application/views/includes/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-
+<noscript>
+  <META HTTP-EQUIV="Refresh" CONTENT="0;URL=/disabled.html">
+</noscript>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=9" />
 <title><?php echo $title ;?></title>
