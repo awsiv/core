96375c1f6c8363e7e72bd6ba383ded2259997dcf
Nishes Joshi <nishes.joshi@cfengine.com>
2011-08-09 14:50:35 +0000

    fixed the tooltip flickering issue.

--- GUI2/application/views/status.php
+++ GUI2/application/views/status.php
@@ -1,64 +1,64 @@
-        <div class="outerdiv">
-            <div class="grid_9">
-                <div id="overall" class="innerdiv">
-                    <div id="pie-charts">
+<div class="outerdiv">
+    <div class="grid_9">
+        <div id="overall" class="innerdiv">
+            <div id="pie-charts">
 
-                        <div id="business-value-pie-chart-container" class="grid_4">
-                            <p class="title">Business Value</p>
-                            <div id="business-value-pie-chart" style="height: 120px;width:300px;">
-                            </div>
-                        </div>
-                        <div id="compliance-value-pie-chart-container" class="grid_4 push_1" style="margin-bottom: 15px">
-                            <p class="title">Host Status</p>
-                            <div id="compliance-now-pie-chart" style="height: 120px; width:300px;">
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div id="compliance" class="innerdiv">
-                    <p class="title">Compliance Summary</p>
-                    <div id="environment-list">
-                        <ul>
-                            <li><!-- chrome fix --></li>
-                            <li><span class="front"></span><a class ="updateComplianceGraph" href="<?php echo site_url(); ?>/welcome/getJsonComplianceSummary/" title="">Default</a><span class="rear"></span></li>
-                            <?php foreach ($envList as $key => $env) {
-                                ?>
-                                <li><span class="front"></span><a class ="updateComplianceGraph" href="<?php echo site_url(); ?>/welcome/getJsonComplianceSummary/<?php echo $env; ?>" title="environment_<?php echo $env; ?>"><?php echo $env; ?></a><span class="rear"></span></li>
-                            <?php } ?>
-                        </ul>
-                    </div>
-                    <div id="compliancemeter" style="padding:5px;">
-                        <div id="compliance_summary_graph" style="height: 200px; width:98%;"></div>
-                        <div class="clear"></div>
+                <div id="business-value-pie-chart-container" class="grid_4">
+                    <p class="title">Business Value</p>
+                    <div id="business-value-pie-chart" style="height: 120px;width:300px;">
                     </div>
                 </div>
-        </div>
-        <div class="grid_3">
-            <div class="innerdiv" style="margin:15px 8px 22px 8px;">
-                <p><a href="<?php echo site_url() ?>/hubstatus/status" title="See the status of the hub replication" class="showqtip">Hub replication status</a></p>
-             </div>   
-            
-                <div id="goals" class="innerdiv statusgoal">
-                    <p class="title">Services/goals</p>
-                    <ul>
-                        <?php
-                        $limit=0;
-                        $goalsorted=array_msort($goals,array('name' => SORT_ASC),true);
-                        foreach ((array) $goalsorted as $goal) {
-                            if($limit==10)
-                                break;
-                            $words = explode("_", $goal['name']);
-                            echo "<li><span class=\"goal\">$words[0] $words[1]</span> - <span>".$goal['desc']."</span><span class=\"check\"></span></li>";
-                            $limit++;
-                        }
-                        ?>
-                    </ul>
-                      <p class="morebtnpane"><span class="morebtn"><?php echo anchor('welcome/goals', 'More...') ?></span></p>
+                <div id="compliance-value-pie-chart-container" class="grid_4 push_1" style="margin-bottom: 15px">
+                    <p class="title">Host Status</p>
+                    <div id="compliance-now-pie-chart" style="height: 120px; width:300px;">
+                    </div>
                 </div>
             </div>
-            <div class="clear"></div>
+
         </div>
+        <div id="compliance" class="innerdiv">
+            <p class="title">Compliance Summary</p>
+            <div id="environment-list">
+                <ul>
+                    <li><!-- chrome fix --></li>
+                    <li><span class="front"></span><a class ="updateComplianceGraph" href="<?php echo site_url(); ?>/welcome/getJsonComplianceSummary/" title="">Default</a><span class="rear"></span></li>
+                    <?php foreach ($envList as $key => $env) {
+                        ?>
+                        <li><span class="front"></span><a class ="updateComplianceGraph" href="<?php echo site_url(); ?>/welcome/getJsonComplianceSummary/<?php echo $env; ?>" title="environment_<?php echo $env; ?>"><?php echo $env; ?></a><span class="rear"></span></li>
+                    <?php } ?>
+                </ul>
+            </div>
+            <div id="compliancemeter" style="padding:5px;">
+                <div id="compliance_summary_graph" style="height: 200px; width:98%;"></div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grid_3">
+        <div class="innerdiv" style="margin:15px 8px 22px 8px;">
+            <p><a href="<?php echo site_url() ?>/hubstatus/status" title="See the status of the hub replication" class="showqtip">Hub replication status</a></p>
+        </div>   
+
+        <div id="goals" class="innerdiv statusgoal">
+            <p class="title">Services/goals</p>
+            <ul>
+                <?php
+                $limit = 0;
+                $goalsorted = array_msort($goals, array('name' => SORT_ASC), true);
+                foreach ((array) $goalsorted as $goal) {
+                    if ($limit == 10)
+                        break;
+                    $words = explode("_", $goal['name']);
+                    echo "<li><span class=\"goal\">$words[0] $words[1]</span> - <span>" . $goal['desc'] . "</span><span class=\"check\"></span></li>";
+                    $limit++;
+                }
+                ?>
+            </ul>
+            <p class="morebtnpane"><span class="morebtn"><?php echo anchor('welcome/goals', 'More...') ?></span></p>
+        </div>
+    </div>
+    <div class="clear"></div>
+</div>
 
 <script type="text/javascript">
     // for summary compliance graph
@@ -130,41 +130,42 @@
        
  
     
-    
+    var previousPoint = {point:null,sIndex:null};
     $("#compliance_summary_graph").bind("plothover", function (event, pos, item) {
         $("#x").text(pos.x.toFixed(2));
         $("#y").text(pos.y.toFixed(2));
  
         
         if (item) {
-            previousPoint = item.dataIndex;
-                    
-            $("#tooltip").remove();
-            var x = item.datapoint[0].toFixed(2),
-            y = item.datapoint[1].toFixed(2);
-            var actualvalue = item.series.data[item.dataIndex][1];
-            var startTime = (item.datapoint[0]);
-            
-            var startDate = new Date(startTime );
-            var start = startDate.getHours() + ':00';
-            var stopDate = new Date(startDate);
-            stopDate.setHours(stopDate.getHours() + 6);
-            var stop =stopDate.getHours() + ':00';
-            var key =  startTime /1000;
-                   
-            var c = countTrack[key];
-                     
-            var tooltip = item.series.label + ": "+ actualvalue + '<br /> Local time : ' + start + '-' + stop
-                + ' <br />Count :: ' + c;
-                   
-            ;
-            showTooltip(item.pageX, item.pageY,
-            tooltip);
+            if (previousPoint == null || previousPoint.point != item.dataIndex || previousPoint.sIndex != item.seriesIndex ) { 
+                previousPoint.point = item.dataIndex;
+                previousPoint.sIndex = item.seriesIndex;
                 
+                $("#tooltip").remove();
+                var x = item.datapoint[0].toFixed(2),
+                y = item.datapoint[1].toFixed(2);
+                var actualvalue = item.series.data[item.dataIndex][1];
+                var startTime = (item.datapoint[0]);
+            
+                var startDate = new Date(startTime );
+                var start = startDate.getHours() + ':00';
+                var stopDate = new Date(startDate);
+                stopDate.setHours(stopDate.getHours() + 6);
+                var stop =stopDate.getHours() + ':00';
+                var key =  startTime /1000;
+                   
+                var c = countTrack[key];
+                     
+                var tooltip = item.series.label + ": "+ actualvalue + '<br /> Local time : ' + start + '-' + stop
+                    + ' <br />Count :: ' + c;
+                   
+                showTooltip(item.pageX, item.pageY,tooltip);
+            }   
         }
         else {
             $("#tooltip").remove();
-            previousPoint = null;            
+            previousPoint.point = null;
+            previousPoint.sIndex = null;
         }
         
     });
@@ -218,7 +219,7 @@
                 tilt: 0.3,
                 label: {
                     show: false
-                   },
+                },
                 highlight: {
                     opacity: .5
                 }
@@ -249,7 +250,7 @@
                 tilt: 0.3,
                 label: {
                     show: false
-                    },
+                },
                 highlight: {
                     opacity: .5
                 }
@@ -264,22 +265,61 @@
 
     });
 
+    var previousPointBusinessValue = {'val':null};
+    var previousPointComplianceNow = {'val':null};
+    $("#business-value-pie-chart").bind("plothover",{'unit':'mu','previousPoint':previousPointBusinessValue},pieHover);
+    $("#compliance-now-pie-chart").bind("plothover",{'unit':'host','previousPoint':previousPointComplianceNow}, pieHover);
     
-    $("#business-value-pie-chart").bind("plothover",{'unit':'mu'},pieHover);
-    $("#compliance-now-pie-chart").bind("plothover",{'unit':'host'}, pieHover);
-    function pieHover(event, pos, obj)
+    $($("#compliance-now-pie-chart")).bind('mouseout', function() {
+        plot.unhighlight();
+        $("#tooltip").remove();
+        $(this).data('previous-post', -1);
+    });
+    var previousPost = null;
+    function pieHover(event, pos, item)
     {
-        if (!obj) {
+        
+        if (item) {
+            if ($(this).data('previous-post') != item.seriesIndex) {
+                plot.unhighlight();
+                plot.highlight(item.series, item.datapoint);
+                $(this).data('previous-post', item.seriesIndex);
+            } else return ;
             $("#tooltip").remove();
+            var  percent = parseFloat(item.series.percent).toFixed(2);
+            var index = item.dataIndex;
+            var value = item.series.data[index][1];
+            var content = '<span>'+item.series.label+' ('+percent+'%) <br /> Value:: '+value+' '+event.data.unit+' '+ '</span>';
+            showTooltip(pos.pageX, pos.pageY, content);
+        } else {
+            plot.unhighlight();
+            $("#tooltip").remove();
+            previousPost = $(this).data('previous-post', -1);
+        }
+        
+        
+        /*
+        
+        if (obj) {
+          
+            console.log(obj);
+            if (event.data.previousPoint.val != obj.dataIndex) {
+                event.data.previousPoint.val = obj.dataIndex;
+                   
+                percent = parseFloat(obj.series.percent).toFixed(2);
+        
+                var index = obj.dataIndex;
+                var value = obj.series.data[index][1];
+                var content = '<span>'+obj.series.label+' ('+percent+'%) <br /> Value:: '+value+' '+event.data.unit+' '+ '</span>';
+                $("#tooltip").remove();
+                showTooltip(pos.pageX, pos.pageY, content);
+            }
+        } else {
+            $("#tooltip").remove();
+            event.data.previousPoint.val = null;
             return;
         }
-        percent = parseFloat(obj.series.percent).toFixed(2);
-        
-        var index = obj.dataIndex;
-        var value = obj.series.data[index][1];
-        var content = '<span>'+obj.series.label+' ('+percent+'%) <br /> Value:: '+value+' '+event.data.unit+' '+ '</span>';
-        $("#tooltip").remove();
-        showTooltip(pos.pageX, pos.pageY, content);
+         */
     }
 
 
@@ -298,7 +338,7 @@
             color: '#000',
             'background-color': '#cccccc',
             opacity: 1
-        }).appendTo("body").fadeIn(200);
+        }).appendTo("body").fadeIn(100);
     }
 
 
