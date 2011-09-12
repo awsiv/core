4ee54364e2415a54cc644a9da140f71db796f8c6
Eystein Måløy Stenberg <eystein@cfengine.com>
2011-09-12 02:37:15 +0000

    minor tooltip adjustments

--- GUI2/application/language/english/tooltip_lang.php
+++ GUI2/application/language/english/tooltip_lang.php
@@ -4,14 +4,14 @@
  * and open the template in the editor.
  */
 
-$lang['tooltip_host_finder'] = 'Find hosts by name or address within the local cluster.';
-$lang['tooltip_class_finder'] = 'Find hosts by the class contexts they occupy in the local cluster.';
-$lang['tooltip_promise_finder'] = 'Find promises that make up the policy in the local cluster.';
+$lang['tooltip_host_finder'] = 'Find hosts by name or address.';
+$lang['tooltip_class_finder'] = 'Find hosts by the classes they have defined.';
+$lang['tooltip_promise_finder'] = 'Find promises that make up the policy.';
 
 
-$lang['tooltip_reports_finder'] = 'Search and browse the available reports in the local cluster.';
+$lang['tooltip_reports_finder'] = 'Search and browse the available reports.';
 
-$lang['tooltip_summary_finder']  = 'Show the current state of knowledge about promise compliance. If a single promise handle is specified, this returns the number of hosts matching a class regex that is compliant. If a regex is given for the promise the result is an expectation value of number of hosts wrt all the matching promises.';
+$lang['tooltip_summary_finder']  = 'Show the current promise compliance.';
 
 $lang['tooltip_license_report'] = 'Displays a summary of the utilization of CFEngine licenses for this cluster.';
 
@@ -23,7 +23,7 @@ $lang['tooltip_greenhost'] = 'More than 80% of promises are stable and kept.';
 $lang['tooltip_bluehost'] = 'Hosts that have not been reachable for more than the time horizon.';
 
 $lang['tooltip_statusroom'] = 'The mission status room shows how close you are an ideal state. The average compliance of hosts is graphed for the present and over the past week. If business values have been configured, the monetary value of the promises is also represented. A reminder of the documented goals is shown in the panel.';
-$lang['tooltip_engineeringroom'] = 'Engineering shows technical status of hosts in the local cluster.';
+$lang['tooltip_engineeringroom'] = 'Engineering shows technical status of hosts.';
 $lang['tooltip_planningroom'] = 'The mission planning room allows you to see how the current goals and policies are performing, approve a change of course and communicate with co-workers.';
 $lang['tooltip_libraryroom'] = 'A place where users can learn about the software and the system.';
 
