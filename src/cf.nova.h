0df638f7edd397c8144243d9d54fba292a8590aa
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2011-10-10 15:34:08 +0000

    Move cfl_view and related functions to Nova

--- src/cf.nova.h
+++ src/cf.nova.h
@@ -99,6 +99,28 @@
 
 /*****************************************************************************/
 
+enum cfl_view
+   {
+   cfl_view_comp,
+   cfl_view_virtbundlecomp,
+   cfl_view_aggr_repaired,
+   cfl_view_aggr_notkept,
+   cfl_view_aggr_repairedreason,
+   cfl_view_aggr_notkeptreason,
+   cfl_view_aggr_filechange,
+   cfl_view_aggr_software,
+   cfl_view_aggr_classes,
+   cfl_view_software,
+   cfl_view_log_repair,
+   cfl_view_log_notkept,
+   cfl_view_hub_status,
+   cfl_view_hub_meter,
+   cfl_view_error
+   };
+
+
+/*****************************************************************************/
+
 enum cf_rank_method
    {
    cfrank_compliance,
@@ -290,6 +312,11 @@ void Nova_RecordNetwork(time_t now, double datarate,struct cfagent_connection *c
 /* cmd_api.c */
 
 #include "cf.nova.cmd_api.h"
+
+/* conversion.c */
+
+enum cfl_view Str2View(const char *s);
+const char *View2Str(enum cfl_view view);
 
 /* copernicus.c */
 
---
+++ src/conversion.c
@@ -0,0 +1,73 @@
+/*
+   Copyright (C) Cfengine AS
+
+   This file is part of Cfengine 3 - written and maintained by Cfengine AS.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; version 3.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+
+  To the extent this program is licensed as part of the Enterprise
+  versions of Cfengine, the applicable Commerical Open Source License
+  (COSL) may apply to this file if you as a licensee so wish it. See
+  included file COSL.txt.
+*/
+
+#include "cf3.defs.h"
+#include "cf3.extern.h"
+
+/*****************************************************************************/
+
+static const char *CFCON_VIEWS[] = { "Comp",  // NOTE: must match cfl_view enum
+                        "VirtBundleComp",
+                        "AggrRepaired",
+                        "AggrNotKept",
+                        "AggrRepairedReason",
+                        "AggrNotKeptReason",
+                        "AggrFileChange",
+                        "AggrSoftware",
+                        "AggrClasses",
+                        "Software",
+                        "RepairLog",
+                        "NotKeptLog",
+                        "HubStatus",
+                        "HubMeter",
+                        NULL };
+
+/*****************************************************************************/
+
+enum cfl_view Str2View(const char *s)
+{
+int i;
+
+for (i = 0; CFCON_VIEWS[i] != NULL; i++)
+   {
+   if (strcmp(s,CFCON_VIEWS[i]) == 0)
+      {
+      return i;
+      }
+   }
+
+return cfl_view_error;
+}
+
+/*********************************************************************/
+
+const char *View2Str(enum cfl_view view)
+{
+if(view < 0 || view >= cfl_view_error)
+   {
+   return "VIEWERROR";
+   }
+
+return CFCON_VIEWS[view];
+}
