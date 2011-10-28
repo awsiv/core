3471070a499f258853ecf9d09f7d7fa2752f6725
Eystein Måløy Stenberg <eystein@cfengine.com>
2011-10-28 15:27:33 +0000

    ifdefs around bson to avoid compile error on non-mongolib systems

--- src/bson_lib.h
+++ src/bson_lib.h
@@ -28,8 +28,11 @@
 #include "cf3.defs.h"
 #include "cf3.extern.h"
 
+#ifdef HAVE_LIBMONGOC
+
 void BsonAppendRecentQuery(bson_buffer *querybuf, int maxAgeInSeconds);
 void BsonAppendAgedQuery(bson_buffer *querybuf, int maxAgeInSeconds);
 
+#endif  /* HAVE_LIBMONGOC */
 
 #endif  /* NOT CFENGINE_BSON_LIB_H */
