92ba154964756e302ce1353cd4ea25252a46be8e
Nishes Joshi <nishes.joshi@cfengine.com>
2011-11-01 16:48:56 +0000

    some cleanup for errors in creating virtual bundles.

--- GUI2/application/controllers/virtualbundle.php
+++ GUI2/application/controllers/virtualbundle.php
@@ -1,19 +1,19 @@
 <?php
-class Virtualbundle extends Cf_controller{
-    
+
+class Virtualbundle extends Cf_controller {
+
     function __construct() {
         parent::__construct();
-        $this->load->library(array('table', 'cf_table', 'pagination','form_validation'));
+        $this->load->library(array('table', 'cf_table', 'pagination', 'form_validation'));
         $this->load->helper('form');
         $this->load->model('virtual_bundle_model');
         $this->carabiner->js('jquery.tablesorter.min.js');
         $this->carabiner->js('picnet.jquery.tablefilter.js');
         $this->carabiner->js('jquery.tablesorter.pager.js');
-       $this->carabiner->js('widgets/policyfinder.js');
-       $this->carabiner->js('widgets/notes.js');
+        $this->carabiner->js('widgets/policyfinder.js');
+        $this->carabiner->js('widgets/notes.js');
     }
-    
-    
+
     function details($handle=NULL) {
         $this->carabiner->css('tabs-custom.css');
 
@@ -23,8 +23,8 @@ class Virtualbundle extends Cf_controller{
             $handle = urldecode($handle);
         }
 
-      $handleData = $this->virtual_bundle_model->getVirtualBundleDetails($handle);
-      $handleStatus = $this->virtual_bundle_model->getVirtualBundleStatus($handle);
+        $handleData = $this->virtual_bundle_model->getVirtualBundleDetails($handle);
+        $handleStatus = $this->virtual_bundle_model->getVirtualBundleStatus($handle);
 
         $bc = array(
             'title' => $this->lang->line('breadcrumb_promise'),
@@ -43,90 +43,90 @@ class Virtualbundle extends Cf_controller{
         );
         $this->template->load('template', 'virtualbundle/detail', $data);
     }
-    
-    
+
     function search() {
         $data['report_type'] = 'virtual bundle';
         $this->load->view('searchpages/virtualbundle_search', $data);
     }
-    
-    function searchresult(){
+
+    function searchresult() {
         
     }
-    
-    function manage(){
+
+    function manage() {
         $this->carabiner->css('tabs-custom.css');
-          $bc = array(
+        $bc = array(
             'title' => 'virtual bundles',
             'url' => 'virtualbundle/manage',
             'isRoot' => false,
             'replace_existing' => true
         );
         $this->breadcrumb->setBreadCrumb($bc);
-        
+
         $data = array(
             'title' => $this->lang->line('mission_portal_title'),
             'breadcrumbs' => $this->breadcrumblist->display(),
         );
         $this->template->load('template', 'virtualbundle/managebundles', $data);
-        
     }
-    
-    function create(){
-    
-         $this->form_validation->set_rules('name', 'Virtual Bundlename', 'xss_clean|trim|required');
-         $this->form_validation->set_rules('promises', 'Promises', 'xss_clean|trim|required');
-         $this->form_validation->set_rules('description', 'Description', 'xss_clean|trim|required');
-         //$this->form_validation->set_rules('hostclass', 'Host class', 'xss_clean|trim|required');
-         //$this->form_validation->set_rules('description', 'Description', 'xss_clean|trim|required');
-         $this->form_validation->set_error_delimiters('<span>', '</span><br/>');
-         if($this->form_validation->run() == FALSE){
-             echo validation_errors();     
-         }else{
-         $inputs=array(
-                 'username'=>$this->session->userdata('username'),
-                 'name'=>$this->input->post('name'),
-                 'hostclass'=>$this->input->post('hostclass')?$this->input->post('hostclass'):"",
-                 'description'=>$this->input->post('description')?$this->input->post('description'):"",
-                 'promises'=>$this->input->post('promises')
-                  );
-         try{
-            $result=$this->virtual_bundle_model->createVirtualBundle($inputs);
-             if($result){ 
-                echo "<p class=\"success\">".$inputs['name']." is created as virtual bundle</p>";
-             }else{
-                echo "<p class=\"error\">".$inputs['name']."  was not created</p>";   
-             }
-           }catch(Exception $e){
-            echo "<p class=\"error\">Exception occured while carrying out the operation. Please check the log file</p>"; 
-          }
-         }
+
+    function create() {
+
+        $this->form_validation->set_rules('name', 'Virtual Bundlename', 'xss_clean|trim|required');
+        $this->form_validation->set_rules('promises', 'Promises', 'xss_clean|trim|required');
+        $this->form_validation->set_rules('description', 'Description', 'xss_clean|trim|required');
+        $this->form_validation->set_error_delimiters('<span>', '</span><br/>');
+        if ($this->form_validation->run() == FALSE) {
+            $this->output->set_status_header('404', "Cannot create virtual bundle");                  
+            echo validation_errors();
+        } else {
+            $inputs = array(
+                'username' => $this->session->userdata('username'),
+                'name' => $this->input->post('name'),
+                'hostclass' => $this->input->post('hostclass') ? $this->input->post('hostclass') : "",
+                'description' => $this->input->post('description') ? $this->input->post('description') : "",
+                'promises' => $this->input->post('promises')
+            );
+            try {
+                $result = $this->virtual_bundle_model->createVirtualBundle($inputs);
+                if ($result) {
+                    echo sprintf('Virtual bundle ( %s ) sucessfully created.',$inputs['name']);;
+                } else {
+                    $this->output->set_status_header('500', "Cannot create virtual bundle". $inputs['name']);
+                    echo $result;                  
+                }
+            } catch (Exception $e) {
+                $this->output->set_status_header('500', "Cannot create virtual bundle. exception occured". $inputs['name']);
+                echo $e->getMessage(); 
+            }
+        }
     }
-    
-    function myBundles(){
-        try{
-            $page_number=1;
+
+    function myBundles() {
+        try {
+            $page_number = 1;
             $getparams = $this->uri->uri_to_assoc(3);
             $rows = isset($getparams['rows']) ? $getparams['rows'] : ($this->input->post('rows') ? $this->input->post('rows') : $this->setting_lib->get_no_of_rows());
-             if (is_numeric($rows)) {
+            if (is_numeric($rows)) {
                 $rows = (int) $rows;
-             } else {
+            } else {
                 $rows = 20;
-             }
+            }
             $page_number = isset($getparams['page']) ? $getparams['page'] : 1;
-            $data=array(
-                 'report_title'=>'virtual bundle',
-                 'current'=>$page_number,
-                 'number_of_rows' => $rows,
-                 'params'=>'',
-                 'url'=>'virtualbundle/myBundles/'
+            $data = array(
+                'report_title' => 'virtual bundle',
+                'current' => $page_number,
+                'number_of_rows' => $rows,
+                'params' => '',
+                'url' => 'virtualbundle/myBundles/'
             );
-            $data['report_result'] = $this->virtual_bundle_model->getVirtualBundleData(null,$this->session->userdata('username'),$rows, $page_number);
+            $data['report_result'] = $this->virtual_bundle_model->getVirtualBundleData(null, $this->session->userdata('username'), $rows, $page_number);
             $this->load->view('searchpages/default_result_view', $data);
-        }catch(Exception $e){
+        } catch (Exception $e) {
             echo "<p class=\"error\">Exception occured while trying to fetch virtual bundles</p>";
-       }
-        
+        }
     }
+
 }
+
 ?>
--- GUI2/application/views/virtualbundle/managebundles.php
+++ GUI2/application/views/virtualbundle/managebundles.php
@@ -7,138 +7,137 @@
             </ul>
             <div id="tabs-1" class="ui-corner-all">
 
-                           <div id="infoMessage"><?php echo isset($message)?$message:"";?></div>
-                           <div class="form">
-                               <?php
-                               $attributes = array('class' => '', 'id' => 'virtualbundleform');
-                               echo form_open('virtualbundle/create', $attributes);
-                               ?>
-                               <p>
-                                   <label for="vbname">Virtual Bundle Name:<span class="required">*</span></label>
-                                   <input type="text" name="vbname" id="vbname"></input>
-                               </p>
-                               <p>
-                                   <label for="hostclass">Host Class:<span class="required"></span></label>
-                                   <input type="text" name="hostclass" id="hostclass"></input>
-                               </p>
-                               <p>
-                                   <label for="description" class="aligntop">Description:<span class="required">*</span></label>
-                                   <textarea id="description" name="description" rows="3" column="30" style="width:436px;height:155px"></textarea>
-                               </p>
-                               <p>
-                                   <label for="promises">Promises:<span class="required">*</span></label>
-                                   <?php echo anchor('widget/allpolicies', 'Add promises', array('id' => 'addpromises')) ?>
-                               </p>
-                               <ul id="promisesContainer">
-                          
-                               </ul>
-                               <p  id="btnholder">
-                               <?php echo form_submit(array('name' => 'submit', 'class' => 'btn', 'value' => 'Submit')); ?>
-                               </p>
-                           
-                             <?php echo form_close();?>
-                          </div>
+                <div id="infoMessage"><?php echo isset($message) ? $message : ""; ?></div>
+                <div class="form">
+                    <?php
+                    $attributes = array('class' => '', 'id' => 'virtualbundleform');
+                    echo form_open('virtualbundle/create', $attributes);
+                    ?>
+                    <p>
+                        <label for="vbname">Virtual Bundle Name:<span class="required">*</span></label>
+                        <input type="text" name="vbname" id="vbname"></input>
+                    </p>
+                    <p>
+                        <label for="hostclass">Host Class:<span class="required"></span></label>
+                        <input type="text" name="hostclass" id="hostclass"></input>
+                    </p>
+                    <p>
+                        <label for="description" class="aligntop">Description:<span class="required">*</span></label>
+                        <textarea id="description" name="description" rows="3" column="30" style="width:436px;height:155px"></textarea>
+                    </p>
+                    <p>
+                        <label for="promises">Promises:<span class="required">*</span></label>
+                        <?php echo anchor('widget/allpolicies', 'Add promises', array('id' => 'addpromises')) ?>
+                    </p>
+                    <ul id="promisesContainer">
+
+                    </ul>
+                    <p  id="btnholder">
+                        <?php echo form_submit(array('name' => 'submit', 'class' => 'btn', 'value' => 'Submit')); ?>
+                    </p>
+
+                    <?php echo form_close(); ?>
+                </div>
             </div>
             <div id="tabs-2" class="ui-corner-all">
-                
+
             </div>
         </div>
-                
+
     </div>
 </div>
 
 <script type="text/javascript">
     $(document).ready(function(){
-         var selectedpromisesContainer=$('#promisesContainer'),
-             messageContainer=$('#infoMessage'),
-             width=$(window).width(),
-             height=$(window).height()-10,
-             selPromises=new Array();
-         var promise_url= '<?php echo site_url('promise/details/')?>';
+        var selectedpromisesContainer=$('#promisesContainer'),
+        messageContainer=$('#infoMessage'),
+        width=$(window).width(),
+        height=$(window).height()-10,
+        selPromises=new Array();
+        var promise_url= '<?php echo site_url('promise/details/') ?>';
          
         var $tabs=$( "#custom-tabs" ).tabs();
         
         $('#addpromises').policyfinder({
-                        baseUrl: '<?php echo site_url() ?>',
-                        defaultbehaviour:false,
-                        showAddButton:true,
-                        position:[width,0],
-                        width:415,
-                        height:height,
-                        title:'Promises',
-                        handleClicked:function(event,data){
-                          if($.inArray(data.selectedHandleName,selPromises) == -1){
-                           var li=$('<li>');
-                           li.data('val',data.selectedHandleName);
-                           var rmbtn=$('<span class="removePromise">');
-                           var selectedPromise=$('<a class="promisetext" href="'+promise_url+'/'+data.selectedHandleName+'" target="_blank">'+data.selectedHandleName+'</a>');
-                           li.append(selectedPromise).appendTo(selectedpromisesContainer);
-                           //$(selectedPromise).ajaxyDialog({'title':'Promise Detail',dontOverrideTitle:true});
-                           rmbtn.text('x').appendTo(li);
-                           selPromises.push(data.selectedHandleName);
-                          }
-                        }
-                    });
+            baseUrl: '<?php echo site_url() ?>',
+            defaultbehaviour:false,
+            showAddButton:true,
+            position:[width,0],
+            width:415,
+            height:height,
+            title:'Promises',
+            handleClicked:function(event,data){
+                if($.inArray(data.selectedHandleName,selPromises) == -1){
+                    var li=$('<li>');
+                    li.data('val',data.selectedHandleName);
+                    var rmbtn=$('<span class="removePromise">');
+                    var selectedPromise=$('<a class="promisetext" href="'+promise_url+'/'+data.selectedHandleName+'" target="_blank">'+data.selectedHandleName+'</a>');
+                    li.append(selectedPromise).appendTo(selectedpromisesContainer);
+                    //$(selectedPromise).ajaxyDialog({'title':'Promise Detail',dontOverrideTitle:true});
+                    rmbtn.text('x').appendTo(li);
+                    selPromises.push(data.selectedHandleName);
+                }
+            }
+        });
          
          
         $('span.removePromise').live('click',function(){
-             var valremoved=$(this).parent().data('val');
-             selPromises.splice( selPromises.indexOf( valremoved),1);
-             $(this).parent().remove();
+            var valremoved=$(this).parent().data('val');
+            selPromises.splice( selPromises.indexOf( valremoved),1);
+            $(this).parent().remove();
         });
         
-    $('#virtualbundleform').submit(function(event){
-         event.preventDefault();
-         var promises=new Array(),
-             form=this,
-             inputs;
+        $('#virtualbundleform').submit(function(event){
+            event.preventDefault();
+            var promises=new Array(),
+            form=this,
+            inputs;
          
-         selectedpromisesContainer.find("li").each(function() { 
-            promises.push($(this).data('val'));
-          });
+            selectedpromisesContainer.find("li").each(function() { 
+                promises.push($(this).data('val'));
+            });
           
-         inputs={
-             'promises':promises.join(','),
-             'name':$('input:text[name=vbname]').val(),
-             'description':$('textarea#description').val(),
-             'hostclass':$('input:text[name=hostclass]').val()
-                };
-        $.ajax({
+            inputs={
+                'promises':promises.join(','),
+                'name':$('input:text[name=vbname]').val(),
+                'description':$('textarea#description').val(),
+                'hostclass':$('input:text[name=hostclass]').val()
+            };
+            $.ajax({
                 url:$(form).attr('action'),
                 data: inputs,
                 type: 'POST',
                 dataType:'html',
                 success: function(resp, status, xhr) {
-                    if (xhr.status != 0) {
-                      messageContainer.html(resp);  
-                    } else {
-                        //stupid jquery calling this 'success', it's
-                        //network unavailable.
-                      messageContainer.html("<div class='ui-state-error' style='padding: 1em;'><p><span style='float: left; margin-right: 0.3em;' class='ui-icon ui-icon-alert'></span>Sorry, a software error has occured.</p><p>" + ": " + xhr.status + " "  + status+"</p><div>");
-                    }
+                    messageContainer.html('<p class="success">'+xhr.responseText+'</p>');
                 },
                 error: function(xhr, msg) {
-                      messageContainer.html("<div class='ui-state-error' style='padding: 1em;'><p><span style='float: left; margin-right: 0.3em;' class='ui-icon ui-icon-alert'></span>Sorry, a software error has occured.</p><p>" + ": " + xhr.status + " "  + status+"</p><div>");
+                    if (xhr.status == '404') {
+                        // validation error
+                        messageContainer.html( xhr.responseText);
+                    }else {                      
+                        messageContainer.html('<p class="error">' + xhr.responseText +"</p>");
+                    }
                 }
             });
-    });
+        });
     
     
-    $('.inside').find('a').live('click',function(event){
-        event.preventDefault();
-        loadContent( this.href );
-    });
+        $('.inside').find('a').live('click',function(event){
+            event.preventDefault();
+            loadContent( this.href );
+        });
     
-    //when the no of rows displayed per page is change.
-    $('.tables').find('form').live('submit',function(event){
-        event.preventDefault();
-        var url=$(this).attr('action')+'/rows/'+$('input:text[name=rows]').val();
-        loadContent( url);
-    });
+        //when the no of rows displayed per page is change.
+        $('.tables').find('form').live('submit',function(event){
+            event.preventDefault();
+            var url=$(this).attr('action')+'/rows/'+$('input:text[name=rows]').val();
+            loadContent( url);
+        });
     
-    function loadContent( url ) {
-        var selected = $tabs.tabs('option', 'selected');
-        $tabs.tabs("url", selected , url ).tabs("load", selected);
+        function loadContent( url ) {
+            var selected = $tabs.tabs('option', 'selected');
+            $tabs.tabs("url", selected , url ).tabs("load", selected);
         }   
     });
 </script>
