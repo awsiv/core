f06cda03e621a410b7ab05816a7a88dfc165f6fb
Nishes Joshi <nishes.joshi@cfengine.com>
2011-11-01 16:02:35 +0000

    re addition of detail functions.

--- GUI2/application/controllers/virtualbundle.php
+++ GUI2/application/controllers/virtualbundle.php
@@ -13,6 +13,38 @@ class Virtualbundle extends Cf_controller{
        $this->carabiner->js('widgets/notes.js');
     }
     
+    
+    function details($handle=NULL) {
+        $this->carabiner->css('tabs-custom.css');
+
+        if (is_null($handle)) {
+            $handle = isset($_POST['handle']) ? $_POST['handle'] : NULL;
+        } else {
+            $handle = urldecode($handle);
+        }
+
+      $handleData = $this->virtual_bundle_model->getVirtualBundleDetails($handle);
+      $handleStatus = $this->virtual_bundle_model->getVirtualBundleStatus($handle);
+
+        $bc = array(
+            'title' => $this->lang->line('breadcrumb_promise'),
+            'url' => 'promise/details/' . $handle,
+            'isRoot' => false,
+            'replace_existing' => true
+        );
+        $this->breadcrumb->setBreadCrumb($bc);
+
+        $data = array(
+            'handle' => $handle,
+            'title' => $this->lang->line('mission_portal_title') . " - " . $this->lang->line('breadcrumb_promise') . " " . $handle,
+            'bundle_data' => $handleData,
+            'bundle_status' => $handleStatus,
+            'breadcrumbs' => $this->breadcrumblist->display()
+        );
+        $this->template->load('template', 'virtualbundle/detail', $data);
+    }
+    
+    
     function search() {
         $data['report_type'] = 'virtual bundle';
         $this->load->view('searchpages/virtualbundle_search', $data);
