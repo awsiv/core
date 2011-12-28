bcadbb5533a0c25862b45354499a0da851d72ff3
Nishes Joshi <nishes.joshi@cfengine.com>
2011-12-28 12:07:44 +0000

    added time marker on the weekly graph.

--- GUI2/application/views/graph/weekly.php
+++ GUI2/application/views/graph/weekly.php
@@ -3,14 +3,38 @@
 <div>
     <h3>Analysis</h3>
     <ul>
-    <?php foreach ((array)$graphDetails as $details) { ?>
-        <li><?php echo $details; ?></li>   
-    <?php } ?>
+        <?php foreach ((array) $graphDetails as $details) { ?>
+            <li><?php echo $details; ?></li>   
+        <?php } ?>
     </ul>
 </div>
 <script language="javascript" type="text/javascript">
     //<![CDATA[
     (function() {
+        
+        // setup background areas
+        var actualDate =parseInt("<?php echo $graphLastUpdated; ?>",10);
+        var d = new Date();
+        d.setTime( actualDate  * 1000 );
+        // getday sunday is 0 .. so we need to align this with the current data
+        var weekday=new Array(7);
+        weekday[0]=144;
+        weekday[1]=0;
+        weekday[2]=24;
+        weekday[3]=48;
+        weekday[4]=72;
+        weekday[5]=96;
+        weekday[6]=120;
+        
+        var alignX = weekday[d.getDay()] + d.getHours();
+        
+        
+        
+        var markings = [
+          
+            { color: '#000', lineWidth: 1, xaxis: { from: alignX, to: alignX } }
+        ];
+    
         var magnifiedViewdata = <?php echo ($graphdata); ?>;
         var lineSeries1 = <?php echo ($graphdatalineseries1); ?>;
         var lineSeries2 = <?php echo ($graphdatalineseries2); ?>;
@@ -20,7 +44,7 @@
                     lastUpdated : "<?php echo $graphLastUpdated; ?>"
                 }
             }
-            ,grid:   { hoverable: true, clickable: true,axisMargin: 15,labelMargin: 15},
+            ,grid:   { hoverable: true, clickable: true,axisMargin: 15,labelMargin: 15,markings: markings},
             yaxis: {
                 min: <?php echo $graphdatamin; ?>,
                 max: <?php echo $graphdatamax; ?>
@@ -29,20 +53,45 @@
             xaxis: {
                 max:168,
                 tickSize:24,
-                ticks: [[0, "mon"], [24, "tue"], [48, "wed"],[72, "thurs"],[96, "fri"],[120,"sat"],[148,"sun"]]
+                ticks: [[0, "mon"], [24, "tue"], [48, "wed"],[72, "thurs"],[96, "fri"],[120,"sat"],[144,"sun"]]
 
             }
         };
 
    
-
-        var plot = $.plot($("#placeholder-weekly_<?php echo $observable; ?>"),
+   
+   
+        var placeholder = $("#placeholder-weekly_<?php echo $observable; ?>");
+        var plot = $.plot(placeholder,
         [
             { data:magnifiedViewdata, magnifiedView: {show: true, lineWidth: 1}},
             { data:lineSeries1,lines: {show:true},color:"rgb(255, 153, 0)"},
             { data:lineSeries2,lines: {show: true, lineWidth: 1},color:"rgb(0, 204, 0)"}
         ]
         ,options);
+
+
+        // add labels
+        var o;
+
+        o = plot.pointOffset({ x: alignX, y: <?php echo $graphdatamax; ?>});
+        // we just append it to the placeholder which Flot already uses
+        // for positioning
+        var labelDate = d.toGMTString();
+        placeholder.append('<div style="position:absolute;left:' + (o.left + 14) + 'px;top:' + (o.top+5) + 'px;color:#666;font-size:smaller">'+labelDate+'</div>');
+        // draw a little arrow on top of the last label to demonstrate
+        // canvas drawing
+        var ctx = plot.getCanvas().getContext("2d");
+        ctx.beginPath();
+        o.left += 1;
+        var ttop = o.top+20;
+        ctx.moveTo(o.left, ttop-5);
+        ctx.lineTo(o.left, ttop - 10);
+        ctx.lineTo(o.left + 10, ttop-5);
+        ctx.lineTo(o.left, ttop);
+        ctx.fillStyle = "#000";
+        ctx.fill();
+
 
 
 
