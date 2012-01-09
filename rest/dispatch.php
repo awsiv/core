638c7533ab8da15ce38b3e56dde8fc8fd84c462e
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-01-09 14:10:50 +0000

    Catch missing auth header in rest

--- rest/dispatch.php
+++ rest/dispatch.php
@@ -1,29 +1,46 @@
 <?php
 
-// load Tonic library
 require_once 'lib/tonic.php';
 
 // load resources
-require_once 'resource/host.php';
 require_once 'resource/report.php';
+
+function addAuthenticateHeader($response)
+{
+    $response->addHeader('WWW-Authenticate', 'Basic realm="CFEngine Nova"');
+}
 
 // handle request
 $request = new Request();
+
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']))
+{
+    $response = new Response($request);
+    $response->code = Response::UNAUTHORIZED;
+    addAuthenticateHeader($response);
+    $response->output();
+    return;
+}
+
 try
 {
     $resource = $request->loadResource();
     $response = $resource->exec($request);
+    $response->output();
+    return;
 } 
 catch (ResponseException $e) 
 {
-    switch ($e->getCode()) {
+    switch ($e->getCode())
+    {
     case Response::UNAUTHORIZED:
         $response = $e->response($request);
-        $response->addHeader('WWW-Authenticate', 'Basic realm="CFEngine Nova"');
+        addAuthenticateHeader($response);
         break;
     default:
         $response = $e->response($request);
     }
-}
-$response->output();
 
+    $response->output();
+    return;
+}
