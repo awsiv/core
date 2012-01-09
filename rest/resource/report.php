4c256f087fab939c2236d9ce6e629ea5b5149f7f
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-01-09 14:10:57 +0000

    Add support for software-installed and contexts reports in rest

--- rest/resource/report.php
+++ rest/resource/report.php
@@ -2,17 +2,52 @@
 
 /**
  * @uri /report
+ * @uri /report/:id
  */
 class ReportResource extends Resource
 {
-    function get($request) 
+    function get($request, $id)
     {
         $response = new Response($request);
-        
+
         $response->code = Response::OK;
         $response->addHeader('Content-type', 'application/json');
-        $response->body = cfmod_resource_report_list();
         
-        return $response;
+        if ($id == '')
+        {
+            $response->body = cfmod_resource_report_list();
+            return $response;
+        }
+        else
+        {
+            switch ($id)
+            {
+                case 'software-installed':
+                    $response->body = cfpr_report_software_in($_GET['hostkey'],
+                            $_GET['name'],
+                            $_GET['version'],
+                            $_GET['arch'],
+                            1,
+                            $_GET['context'],
+                            $_GET['count'],
+                            $_GET['startPage']);
+                    break;
+
+                case 'contexts':
+                    $response->body = cfpr_report_classes($_GET['hostkey'],
+                            NULL,
+                            1,
+                            $_GET['context'],
+                            $_GET['count'],
+                            $_GET['startPage']);
+                    break;
+
+                default:
+                    $response->code = Response::NOTFOUND;
+                    return $response;
+            }
+
+            return $response;
+        }
     }
 }
