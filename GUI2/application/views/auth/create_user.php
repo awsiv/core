a85715aff3d47a3bf9e0f80bc0c51a4398dc6103
Dmitry Shevchenko <dmitry.shevchenko@cfengine.com>
2012-01-19 15:08:18 +0000

    Create user - added new version of role selection
    Known bug (GUI): if user don't have any roles, and we add new role => role item will nave a border at the top.

--- GUI2/application/views/auth/create_user.php
+++ GUI2/application/views/auth/create_user.php
@@ -24,6 +24,63 @@
       echo form_input($password_confirm);?>
       </p>
       
+            <div class="roles_wrapper">
+          <h2>Roles</h3>
+            <div class="role_list">
+                <h3>Assigned</h3>
+                <ul id="user_roles">
+                <?php 
+                 $i=1;
+                 $first = true;
+                 $fist_item_class = ' class="first"';
+                 
+                 foreach($roles as $role)
+                    {
+                      if ($role['checked'] != '') { 
+                          //$role['checked'] = false; // unc
+                          echo '<li id="li_item'.$i.'" itemid="'.$i.'" '.($first === true ?  $fist_item_class : "").' >';
+                              echo form_checkbox($role, '','','class="selectit"');
+                              echo form_label($role['id'], $role['id'],array('class'=>'role'));
+                          echo "</li>";     
+                          $i++;
+                          $first = false;
+                      }
+                    }
+                ?>
+                </ul>
+                <div class="clearboth">&nbsp;</div>
+            </div>  
+            <div class="switchers">
+                <a href="javascript:void(0);" id="move_left">&laquo; Left</a><br /><br />
+                <a href="javascript:void(0);" id="move_right">Right &raquo;</a><br /><br />
+                <div class="clearboth">&nbsp;</div>
+            </div>
+            <div class="role_list">
+                <h3>Available</h3>                
+                <ul id="all_roles">
+                <?php 
+                 $first = true;
+                  foreach($roles as $role)
+                    {
+                      if ($role['checked'] == '') {
+                        echo '<li id="li_item'.$i.'" itemid="'.$i.'" '.($first === true ? $fist_item_class : "").' >';
+                            echo form_checkbox($role, '','','class="selectit" disabled="disabled"');
+                            echo form_label($role['id'], $role['id'],array('class'=>'role'));
+                        echo "</li>";     
+                        $i++;
+                        $first = false;
+                      }
+                    }
+                ?>
+                 </ul>
+                 <div class="clearboth">&nbsp;</div>
+            </div>
+          <div class="clearboth">&nbsp;</div>
+          </div>
+      
+      
+      
+      <?php /*
       <p>
       <?php echo form_label('role');
       //echo form_dropdown('role', $roles,set_value('role', '2'));
@@ -35,7 +92,8 @@
       }
       ?>
       </p>
-      
+      */
+      ?>
       <p id="btnholder"><?php echo form_submit(array('name'=>'submit','class'=>'btn','value'=>'Create User'));?></p>
     <?php echo form_close();?>
  </div>
