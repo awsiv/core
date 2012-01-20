8173e8b1093d078e2d2527d0bec4c2dcfcac5e4e
Nishes Joshi <nishes.joshi@cfengine.com>
2012-01-20 10:08:22 +0000

    Reformatted the no host found page with the same message as other reports. Fixes #258

--- GUI2/application/views/searchpages/search_result_group.php
+++ GUI2/application/views/searchpages/search_result_group.php
@@ -1,21 +1,25 @@
 <div class="outerdiv grid_12">
     <div class="innerdiv">
-	 <p class="title">
-                <strong><?php echo $report_type?> Report</strong>  
-         </p>
-         <div><?php
-               $link="";
-               if($classregex) { echo "Host list according to  <strong>Class :</strong>  $classregex"; }
-              if(isset($name)) {$link.="  <strong>Name:</strong> ".$name; }
-               echo $link;
-              ?>
-         </div>
-         <!--<h2>Matched Hosts</h2>-->
-		   <div class="tables tablesfixed">
-			<?php 
-                        $data=json_decode($report_result,true);
-                        echo  host_only_table($data);
-                        ?>
-                   </div>
+        <p class="title">
+            <strong><?php echo $report_title ?> Report</strong>  
+        </p>
+        <div style="padding-left: 10px;">
+            <?php if ($classregex) { ?>
+                <p>Host list according to  <strong>Class :</strong>  <?php echo $classregex; ?></p>
+            <?php } ?>
+            <?php if (isset($name)) { ?>
+                <p> <strong>Name:</strong> <?php echo  $name; ?> </p>
+            <?php } ?>
+        </div>
+        <div class="tables tablesfixed">
+            <?php
+            $data = json_decode($report_result, true);
+            if (is_array($data) && !empty($data)) {
+                echo host_only_table($data);
+            } else {
+                echo"<div class='info'>" . $this->lang->line("no_data") . "</div>";
+            }
+            ?>
+        </div>
     </div>
 </div>
