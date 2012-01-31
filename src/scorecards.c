b8450b0ba8f72aef3de1a0f186360c89e0c40679
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-01-31 15:29:14 +0000

    Factor out scorecards.h, pending cleanup

--- src/scorecards.c
+++ src/scorecards.c
@@ -6,7 +6,7 @@ This file is (C) Cfengine AS. See LICENSE for details.
 
 #include "cf3.defs.h"
 #include "cf3.extern.h"
-#include "cf.nova.h"
+#include "scorecards.h"
 
 #ifdef HAVE_LIBMONGOC
 /*****************************************************************************/
