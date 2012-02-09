57e6e87ab82f8a35e987e371960c378a78384998
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-02-09 15:56:09 +0000

    Add convenience variables for dataset hashkeys in rest test

--- rest/tests/RestBaseTest.php
+++ rest/tests/RestBaseTest.php
@@ -6,7 +6,9 @@ require_once __DIR__ . "/../lib/PestJSON.php";
 
 class RestBaseTest extends PHPUnit_Framework_TestCase
 {
-
+    public $hostA = "SHA=305658693b94e003e765956f1609731419cbc0e5c9caa09e230df5e005f1f283";
+    public $hostB = "SHA=33736d45041e2a9407be8cf449aeffa95114bef661c20deaca1bbcfbc2922856";
+    
     function assertValidJson($data)
     {
         if ($data === null && json_last_error() !== JSON_ERROR_NONE)
