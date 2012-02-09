4dbaf104e763ca3d69453b7bf730fb77a3334d3e
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-02-09 15:56:06 +0000

    Update rest host test to reflect dataset

--- rest/tests/HostTest.php
+++ rest/tests/HostTest.php
@@ -11,6 +11,7 @@ class HostTest extends RestBaseTest
         {
             $jsonArray = $this->pest->get('/host');
             $this->assertValidJson($jsonArray);
+            $this->assertEquals(2, sizeof($jsonArray));
         }
         catch (Pest_NotFound $e)
         {
@@ -22,10 +23,10 @@ class HostTest extends RestBaseTest
     {
         try
         {
-            $ip = '10.0.0.29';
+            $ip = '10.0.0.150';
             $jsonArray = $this->pest->get('/host?ip=' . $ip);
             $this->assertValidJson($jsonArray);
-            $expectedKey = "SHA=bd6dfcc28b1a7be234a68e3fe77e3c199e68fc28f400de0f94eadf697ca213df";
+            $expectedKey = "SHA=305658693b94e003e765956f1609731419cbc0e5c9caa09e230df5e005f1f283";
             $this->assertEquals($expectedKey, $jsonArray[0], "got key :: $jsonArray[0]");
         }
         catch (Pest_NotFound $e)
