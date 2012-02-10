a46750ed365a43f82af8e502c6d0093b6eaa7937
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-02-10 19:12:42 +0000

    Drop seen/seenby remote context tests for rest, not supported

--- rest/tests/HostIdSeenTest.php
+++ rest/tests/HostIdSeenTest.php
@@ -40,35 +40,4 @@ class HostIdSeenTest extends RestBaseTest
             $this->fail('Resource not found');
         }
     }
-
-    /**
-     * Checks the remote context parameter
-     * Matches the data format (array keys) that are returned and expected, doesnot check for the values 
-     */
-    public function testhostIdSeenWithRemoteContext()
-    {
-        try
-        {
-            $context = "10_0_0_153";
-            $id = "SHA=bd6dfcc28b1a7be234a68e3fe77e3c199e68fc28f400de0f94eadf697ca213df";
-            $jsonArray = $this->pest->get("/host/$id/seen?context=$context");
-            $this->assertValidJson($jsonArray);
-            $expectedArray = array(
-                "hostkey" => "SHA=db6f6be353bb61e3e02dcae40c1d9c9d89a5e60edd42fabd8ee1da44552efda6",
-                "last-seen" => 288,
-                "average" => 288,
-                "stdv" => 180
-            );
-            $this->assertFalse(empty($jsonArray), "Result should not be empty");
-            if (isset($jsonArray[0]))
-            {
-                $this->assertTrue($this->array_keys_exists($jsonArray[0], array_keys($expectedArray)), "Returned data not equal to expected data.");
-            }
-        }
-        catch (Pest_NotFound $e)
-        {
-            $this->fail('Resource not found');
-        }
-    }
-
 }
