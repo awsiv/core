b76a40d450daa7cedac8f46e69ab7ada87f022fd
Sudhir Pandey <sudhir.pandey@cfengine.com>
2012-02-15 17:29:32 +0000

    making links in promise finder work when MP is deployed in sub directoty

--- GUI2/application/views/widgets/allpolicies.php
+++ GUI2/application/views/widgets/allpolicies.php
@@ -1,6 +1,6 @@
 <?php
-foreach ((array) $viewdata as $index => $data) {  ?><li><span class="type"><?php echo $data[3] ?></span><p><?php echo sprintf('<a href="/promise/details/%s" class="promiselnk" title="promise"><span class="promiser">%s</span></a>', urlencode($data[0]), $data[4]);
-    ?><?php if (!$showOnlyHandle) echo sprintf('<a class="handleClick" href="/promise/details/%s" rel="%s" title="handle"><span class="handle">%s</span></a>', $data[0], $data[0], $data[0]);
-    ?><?php if (!$showOnlyHandle) echo sprintf('<a href="/bundle/details/bundle/%s/type/%s" title="bundle"><span class="bundle">%s</span></a>', $data[2], $data[3], $data[2]);
+foreach ((array) $viewdata as $index => $data) {  ?><li><span class="type"><?php echo $data[3] ?></span><p><?php echo sprintf('<a href="%s/promise/details/%s" class="promiselnk" title="promise"><span class="promiser">%s</span></a>', site_url(),urlencode($data[0]), $data[4]);
+    ?><?php if (!$showOnlyHandle) echo sprintf('<a class="handleClick" href="%s/promise/details/%s" rel="%s" title="handle"><span class="handle">%s</span></a>', site_url (),$data[0], $data[0], $data[0]);
+    ?><?php if (!$showOnlyHandle) echo sprintf('<a href="%s/bundle/details/bundle/%s/type/%s" title="bundle"><span class="bundle">%s</span></a>', site_url (),$data[2], $data[3], $data[2]);
     ?></p></li>
 <?php } ?>
