e7d476fdc98429a633fa7fba409484b444673981
Maciej Mrowiec <maciej.mrowiec@cfengine.com>
2012-02-15 22:47:50 +0000

    update blue-state tooltips and add description for setting bluehost threshold

--- GUI2/application/language/english/tooltip_lang.php
+++ GUI2/application/language/english/tooltip_lang.php
@@ -20,7 +20,7 @@ $lang['tooltip_vital_signs'] = 'Performance measurements that characterize the o
 $lang['tooltip_redhost'] = 'More than 20% of promises could not be kept at last measurement.';
 $lang['tooltip_yellowhost'] = 'More than 20% of promises needed repair at last measurement.';
 $lang['tooltip_greenhost'] = 'More than 80% of promises are stable and kept.';
-$lang['tooltip_bluehost'] = 'Hosts that have not been reachable for more than the time horizon.';
+$lang['tooltip_bluehost'] = 'Hosts that have not been reachable (their state is unknown) for more than: ';
 
 $lang['tooltip_statusroom'] = 'The status room shows how close you are to an ideal state.';
 $lang['tooltip_engineeringroom'] = 'The engineering room shows the technical status of the managed hosts.';
@@ -47,7 +47,7 @@ $lang['tooltip_ad_domain_name']="The domain named used by the active directory e
 
 $lang['tooltip_fall_back']="Members of the selected group will be able to log in if the directory server is down";
 $lang['tooltip_admin_grp']="Members of the selected group will be administrators of the Mission Portal";
-
+$lang['tooltip_bluehost_threshold']="Blue-state threshold is an amount of time after which host state is considered to be unknown. Range: 5 minutes - 5 weeks";
 
 
 
