293fa133ef0911c2c7b150b83dd5e14b500e7934
Eystein Måløy Stenberg <eystein@cfengine.com>
2012-02-16 00:00:33 +0000

    RBAC for cfpr_host_compliance_list_blue

---
+++ cfmod/tests/cfpr_host_compliance_list_blue.php
@@ -0,0 +1,9 @@
+<?php
+$userName = getenv("CFENGINE_TEST_OVERRIDE_USERNAME");
+$resultsPerPage = 100;
+$pageNum = 1;
+
+$res = cfpr_host_compliance_list_blue($userName, $resultsPerPage, $pageNum);
+
+echo $res;
+?>
