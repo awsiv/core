8bf863536469d7841e7b8df0f658dd0b8ebd09fa
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-02-20 16:12:14 +0000

    Import experimental engineering page

--- GUI2/application/views/engineering.php
+++ GUI2/application/views/engineering.php
@@ -1,3 +1,112 @@
 <div id="body">
-    <p>Hello, World!</p>
+    <div class="outerdiv">
+        <div class="grid_4">
+            <div id="navigation" class="innerdiv">
+                <div id="astrolabe"></div>
+            </div>
+        </div>
+        <div class="grid_8">
+            <div class="innerdiv">
+                <div style="padding: 15px">
+                    <div id="astrolabeLocation" class="subTitle"></div>
+                </div>
+                
+                <div style="padding: 15px">
+                    <p class="subTitle"><?php echo $this->lang->line('header_promise_compliance'); ?></p>
+                    <div class="graphcontainer">
+                        <div id="hostMeter" style="height:120px;"></div>
+                    </div>
+                </div>
+
+                <div id="hostsComplianceContainer" style="padding: 15px">
+                    <p class="subTitle"><?php echo $this->lang->line('header_host_status'); ?></p>
+                    <div id="hostsCompliance"></div>
+                </div>
+                
+                <div id="hostInfoContainer" style="padding: 15px">
+                    <p class="subTitle"><?php echo $this->lang->line('host_info'); ?></p>
+                    <div id="hostInfo"></div>
+                </div>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
 </div>
+<div class="clear"></div>
+
+<!-- TODO: move to carabiner later -->
+<script type="text/javascript" src="<?php echo get_scriptdir()?>/widgets/astrolabe.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>/widgets/astrolabeLocation.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>/widgets/combobox.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>/widgets/searchEntry.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>/widgets/hostsCompliance.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>/widgets/hostmeter.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>/widgets/hostinfo.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>jScrollPane.js"></script>
+<script type="text/javascript" src="<?php echo get_scriptdir()?>jquery.mousewheel.js"></script>
+
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        
+        var genericOption = {
+            baseUrl: '<?php echo site_url() ?>'
+        };
+        
+        var hostFinderOption =   {
+            baseUrl:'<?php echo site_url() ?>'
+          };
+    
+        $('#findhost').hostfinder(hostFinderOption);
+        $('#findclass').classfinder(genericOption);
+        $('#findpolicy').policyfinder(genericOption);
+        $('#findreport').reportfinder(genericOption);
+        $('#summaryreport').ajaxyDialog({'title':'Summary report',dontOverrideTitle:true,
+            'width':'50%'});
+        $('#findcdpreport').cdpreportfinder(genericOption);
+        //for constellation
+        $('#findvbundle').ajaxyDialog({'title':'Virtual bundle',dontOverrideTitle:true,
+            'width':'50%'});
+
+        $.getJSON(genericOption.baseUrl + '/astrolabe/meters/', function(json) {
+            $('#hostMeter').hostMeter({
+                baseUrl: '<?php echo site_url() ?>',
+                data: {
+                    color: ['#779a62','#f0d76a','#cc4358'],
+                    'label': json.labels,
+                    'values': json.values
+                }
+            });
+            $('#hostMeter').hostMeter("initialize");
+        });
+        
+        $('#hostsCompliance').hostsCompliance();
+        $('#hostInfo').hostInfo();
+        $('#astrolabeLocation').astrolabeLocation();
+
+        $('#astrolabe').astrolabe({
+            hostSelected: function(event, args) {
+                $('#hostMeter').hostMeter('updateHostKey', args.hostKey);
+
+                $('#hostInfo').hostInfo('updateHostKey', args.hostKey);
+                $('#hostInfoContainer').show();
+
+                $('#hostsComplianceContainer').hide();
+
+                $('#astrolabeLocation').astrolabeLocation('setHostName', args.hostName);
+            },
+            
+            nodeSelected: function(event, args) {
+                $('#hostInfoContainer').hide();
+
+                $('#hostsCompliance').hostsCompliance('setContextPath',
+                    args.includes, args.excludes);
+                $('#hostsComplianceContainer').show();
+                
+
+                $('#astrolabeLocation').astrolabeLocation('setContextPath', args.path, args.count);
+            }
+        });
+        
+    });
+</script>
