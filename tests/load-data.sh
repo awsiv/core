7f474e2df077535ce1df10c80e96b2582aa03e7d
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2012-02-20 14:55:20 +0000

    Drop semicolons from ends of lines, it's not a C

--- tests/load-data.sh
+++ tests/load-data.sh
@@ -2,10 +2,9 @@
 
 DATADIR=$1
 
-if [ -z $DATADIR ]
-then
+if [ -z $DATADIR ]; then
     echo "Usage: load-data.sh <data-directory>"
-    exit 1;
+    exit 1
 fi
 
 PORT=27777
@@ -13,22 +12,21 @@ PORT=27777
 echo "Importing data from $1"
 
 load_data() {
-    local DB=$1;
-    local COLL=$2;
-    local FILE=${DATADIR}/${DB}.${COLL}.json;
-    echo "Importing ${FILE}";
-    if [ -f ${FILE} ] 
-    then
-	mongoimport --port ${PORT} -d phpcfengine -c $COLL --drop --file ${FILE} --jsonArray;
+    local DB=$1
+    local COLL=$2
+    local FILE=${DATADIR}/${DB}.${COLL}.json
+    echo "Importing ${FILE}"
+    if [ -f ${FILE} ]; then
+	mongoimport --port ${PORT} -d phpcfengine -c $COLL --drop --file ${FILE} --jsonArray
     fi
 }
 
-load_data phpcfengine users;
-load_data phpcfengine roles;
-load_data phpcfengine appsettings;
+load_data phpcfengine users
+load_data phpcfengine roles
+load_data phpcfengine appsettings
 
-load_data cfreport hosts;
-load_data cfreport logs_rep;
-load_data cfreport logs_nk;
+load_data cfreport hosts
+load_data cfreport logs_rep
+load_data cfreport logs_nk
 
 echo "Importing data complete"
