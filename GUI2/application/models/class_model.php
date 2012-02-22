4a9950150a01802aad834b529373c6d82a56c694
Sudhir Pandey <sudhir.pandey@cfengine.com>
2012-02-22 16:04:33 +0000

    function implementating of rbac connected to classes in this model

---
+++ GUI2/application/models/class_model.php
@@ -0,0 +1,28 @@
+<?php
+
+class class_model extends Cf_Model {
+  
+    /**
+     *
+     * @param type $username
+     * @param type $classRx
+     * @param type $rows
+     * @param type $page
+     * @return type json, plain array
+     */
+    function getAllClasses($username, $classRx, $rows=10, $page=1) {
+        try {
+            $rawdata = cfpr_class_list_distinct_by_name_rx($username, $classRx, $rows, $page);
+            $data = $this->checkData($rawdata);
+            if (is_array($data)) {
+                return json_encode($data['data']);
+            } else {
+                throw new Exception($this->lang->line('invalid_json'));
+            }
+        } catch (Exception $e) {
+            log_message('error', $e->getMessage()." ".$e->getFile()." line:".$e->getLine());
+            throw $e;
+        }
+    }
+}
+?>
