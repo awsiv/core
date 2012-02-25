470d2776c4e53f70b17c651587598e42700d193d
Eystein Måløy Stenberg <eystein@cfengine.com>
2012-02-25 00:30:48 +0000

    limit admin users to cfpr_show_topic_hits and add test (again failing)

--- cfmod/private-api.c
+++ cfmod/private-api.c
@@ -3099,10 +3099,22 @@ PHP_FUNCTION(cfpr_show_topic_hits)
 { const int bufsize = 100000;
  char buffer[bufsize];
  long id;
+ char *username;
+ int user_len;
 
- if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l",&id) == FAILURE)
+ if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sl",
+                           &username, &user_len,
+                           &id) == FAILURE)
     {
     zend_throw_exception(cfmod_exception_args, LABEL_ERROR_ARGS, 0 TSRMLS_CC);
+    RETURN_NULL();
+    }
+
+ ARGUMENT_CHECK_CONTENTS(user_len);
+
+ if(CFDB_UserIsAdminWhenRBAC(username) != ERRID_SUCCESS)
+    {
+    zend_throw_exception(cfmod_exception_rbac, LABEL_ERROR_RBAC_NOT_ADMIN, 0 TSRMLS_CC);
     RETURN_NULL();
     }
 
