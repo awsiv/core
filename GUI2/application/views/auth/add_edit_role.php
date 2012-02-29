487fe53ee8e8dbdf621e109c32a588b6776d02a5
Dmitry Shevchenko <dmitry84@gmail.com>
2012-02-29 13:02:13 +0100

    Changes to support drag&drop interface

--- GUI2/application/views/auth/add_edit_role.php
+++ GUI2/application/views/auth/add_edit_role.php
@@ -9,7 +9,6 @@
        if ($op == 'edit') {  
            $name['readonly'] = 'readonly';
        }
-       
        echo form_input($name);
        ?>
       </p>
@@ -21,34 +20,119 @@
        ?>
       </p>
 
-      <p>
-      <?php
-        echo form_label("Include classrx's (hosts)", $crxi['name'], array('class'=> 'regex'));
-        echo form_input($crxi);
-       ?>
-      </p>    
-      <p>
-      <?php
-        echo form_label("Exclude classrx's (hosts)", $crxx['name'], array('class'=> 'regex'));
-        echo form_input($crxx);
-       ?>
-      </p>  
-      <p>
-      <?php
-        echo form_label("Include bundlerx's", $brxi['name'],  array('class'=> 'regex'));
-        echo form_input($brxi);
-       ?>          
-      </p>  
-      <p>
-      <?php
-        echo form_label("Exclude bundlerx's", $brxx['name'],  array('class'=> 'regex'));
-        echo form_input($brxx);
-       ?>          
-      </p>  
-          
-      <p id="btnholder">
-      <?php echo form_submit(array('name'=>'submit','value'=>$operation,'class'=>'btn'));?>
-      </p>
+      <table cellpadding="0" cellspasing ="0" border="1"  id="maintable">
+          <tr>
+              <td colspan="3">  
+                  <h2>Classes</h2>
+                  <?php
+                  ?></td>
+          </tr>     
+
+          <tr>
+              <td class="edit_cell">
+                  <div class="DragNDrop assigned dialog_box_style ui-dialog">
+                      <div class="ui-dialog-titlebar"><h6>Assigned classes</h6></div>
+                      <div class="itemwrapper assigneditems" id="">
+                          <?php if (!empty($crxi['value'])) { ?>
+                              <?php echo roles_checkbox($crxi['value'], 'crxi', 'classlist itemlist assignedList'); ?>                
+                          <?php } else { ?>
+                              <ul id="crxi" class="classlist itemlist assignedList"><li class="empty">No classes assigned</li></ul>
+                          <?php } ?>
+                      </div>
+                  </div>            
+              </td>
+              <td class="arrows">
+                  <a href="javascript:void(0);" class="move_btn" dest="crxi" sourse="classList">&laquo; Left</a><br /><br />
+                  <a href="javascript:void(0);" class="move_btn" dest="classList" sourse="crxi">Right &raquo;</a><br /><br />
+              </td>
+              <td rowspan="2">
+                  <div id="finderWrapper" class="all_classes dialog_box_style DragNDrop">
+                      <div id="clfres" class="ui-dialog">
+                          <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"><h6>Available classes</h6></div>
+                          <div class="formItem addRegex"><input id="classRegexpText" type="text"> <a href="javascript:void(0);" id="addClassRegexp" style="font-size: 26px">+</a></div>
+                          <div id="classList_wrapper" style="display:block" class="result ui-dialog-content" title="Classes">
+                              <div id=""><?php echo anchor('widget/allclasses', $this->lang->line('class'), array('class' => 'classes ', 'id' => 'classList')); ?></div>
+                              <ul id="classList"></ul>
+                          </div>
+                      </div>
+                  </div>
+              </td>   
+          </tr>            
+          <tr>
+              <td class="edit_cell">
+
+                  <div class="DragNDrop excluded customdlg dialog_box_style ui-dialog">
+                      <div class="ui-dialog-titlebar"><h6>Excluded classes</h6></div>
+
+                      <div class="itemwrapper excludeditems " id="">
+                          <?php if (!empty($crxx['value'])) { ?>
+                              <?php echo roles_checkbox($crxx['value'], 'crxx', 'classlist itemlist excludedList'); ?>                          
+                          <?php } else { ?>
+                              <ul id="crxx" class="classlist itemlist excludedList"><li class="empty">No classes assigned</li></ul>
+                          <?php } ?>
+                      </div>
+                  </div>   
+
+              </td>
+              <td></td>
+          </tr>
+          <tr><td colspan="3"><hr /></td></tr>
+          <tr>
+              <td class="edit_cell">
+                  <div class="DragNDrop assigned dialog_box_style ui-dialog">
+                      <div class="ui-dialog-titlebar"><h6>Assigned bundlerx's</h6></div>
+                      <div class="itemwrapper assigneditems" id="">
+                          <?php if (!empty($brxi['value'])) { ?>
+                              <?php echo roles_checkbox($brxi['value'], 'brxi', 'bundlelist itemlist assignedList'); ?>                       
+                          <?php } else { ?>
+                              <ul id="brxi" class="bundlelist itemlist assignedList"><li class="empty">No bundles assigned</li></ul>
+                          <?php } ?>
+                      </div>
+                  </div>            
+              </td>
+              <td class="arrows">
+                  <a href="javascript:void(0);" class="move_btn" dest="brxi" sourse="bundlesList">&laquo; Left</a><br /><br />
+                  <a href="javascript:void(0);" class="move_btn" dest="bundlesList" sourse="brxi">Right &raquo;</a><br /><br />
+              </td>
+              <td rowspan="2">
+                  <div id="finderWrapper" class="all_classes dialog_box_style DragNDrop">
+                      <div id="" class="ui-dialog">
+                          <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"><h6>Available Bundles</h6></div>
+                          <div class="formItem addRegex"><input id="classRegexpText" type="text"> <a href="javascript:void(0);" id="addClassRegexp" style="font-size: 26px">+</a></div>
+                          <div id="bundlessList_wrapper" style="display:block" class="result ui-dialog-content" title="Classes">
+                              <div id=""><?php echo anchor('widget/bundlesNotAssignedToRole/' . $name['value'], $this->lang->line('class'), array('class' => 'classes ', 'id' => 'bundlessList')); ?></div>
+                          </div>
+                      </div>
+                  </div>
+              </td>  
+          </tr>
+
+          <tr>
+
+
+              <td class="edit_cell">
+                  <div class="DragNDrop assigned dialog_box_style ui-dialog">
+                      <div class="ui-dialog-titlebar"><h6>Excluded bundlerx's</h6></div>
+                      <div class="itemwrapper assigneditems" id="">
+                          <?php if (!empty($brxx['value'])) { ?>
+                              <?php echo roles_checkbox($brxx['value'], 'brxx', 'bundlelist itemlist excludedList'); ?>                       
+                          <?php } else { ?>
+                              <ul id="brxi" class="bundlelist itemlist assignedList"><li class="empty">No bundles assigned</li></ul>
+                          <?php } ?>
+                      </div>
+                  </div>            
+              </td>
+              
+              <td></td>
+          </tr>
+          <tr>
+              <td colspan="3">
+                  <p id="btnholder">
+                      <?php echo form_submit(array('name' => 'submit', 'value' => $operation, 'class' => 'btn')); ?>
+                  </p>
+              </td>
+          </tr> 
+      </table>
 
 
     <?php echo form_close();?>
