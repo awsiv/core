4a149ff2ed3477894c8cef712f0e9fc4fc92d2cf
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-02-29 15:49:39 +0100

    Fix timeseries y-axis format, kill whitespace

--- GUI2/scripts/widgets/hostsComplianceTimeseries.js
+++ GUI2/scripts/widgets/hostsComplianceTimeseries.js
@@ -3,16 +3,15 @@
         options: {
             baseUrl: '../',
             defaultbehaviour: true,
-            
+
             plot: {
                 series: {
-                    stack: true,             
+                    stack: true,
                     bars: {
                         show: true,
                         lineWidth: 0,
-                        //barWidth: ,
                         fill: 1,
-                        align: 'center' 
+                        align: 'center'
                     },
                     lines: {
                         show: false,
@@ -26,23 +25,24 @@
                     show: false
                 },
                 xaxis: {
-                    //color: '#FFF',
+                    show: true,
                     mode: "time",
                     timeformat: "%b-%d",
                     minTickSize: [1, "day"]
                 },
                 yaxis: {
                     show: true,
-                    legend: "Number of Reported Hosts"
+                    minTickSize: 1,
+                    tickDecimals: 0
                 },
                 grid: {
-                    hoverable: true, 
+                    hoverable: true,
                     clickable: true,
-                    autoHighlight: true 
+                    autoHighlight: true
                 },
                 colors: [
-                    common.colours.green, 
-                    common.colours.yellow, 
+                    common.colours.green,
+                    common.colours.yellow,
                     common.colours.red
                 ]
             }
@@ -50,59 +50,59 @@
 
         _create: function () {
             var $self = this;
-            
+
             $self.element.addClass('hostsComplianceTimeseries');
 
         },
-        
+
         setContext: function(includes, excludes) {
             var $self = this;
 
             $self._context.includes = includes;
             $self._context.excludes = excludes;
-            
+
             $self.refresh();
         },
-        
+
         refresh: function() {
             var $self = this;
-            
-            $.getJSON($self._requestUrls.timeseries($self), 
+
+            $.getJSON($self._requestUrls.timeseries($self),
                 function(timeseries) {
-                    
+
                     var kept = common.arrayWithValue(0, timeseries.count);
                     var notkept = common.arrayWithValue(0, timeseries.count);
                     var repaired = common.arrayWithValue(0, timeseries.count);
                     var i = 0;
-                    
+
                     var convertToTimeDomain = function(positionSeries) {
                         var timeEntry = function (position) {
                             return common.unixTimeToJavascriptTime(timeseries.from) +
                             (position * common.unixTimeToJavascriptTime(timeseries.resolution));
                         };
-                        
+
                         var timeArray = [];
                         for (var pos = 0; pos < positionSeries.length; pos++) {
                             timeArray.push([timeEntry(pos), positionSeries[pos]]);
                         }
                         return timeArray;
                     }
-                    
+
                     for (i = 0; i < timeseries.data.length; i++) {
                         var entry = timeseries.data[i];
-                        
+
                         kept[entry.position] = (entry.kept / 100) * entry.hostcount;
                         notkept[entry.position] = (entry.notkept / 100) * entry.hostcount;
                         repaired[entry.position] = (entry.repaired / 100) * entry.hostcount;
                     }
-                    
+
                     kept = convertToTimeDomain(kept);
                     notkept = convertToTimeDomain(notkept);
                     repaired = convertToTimeDomain(repaired);
-                    
-                    $self.options.plot.series.bars.barWidth = 
+
+                    $self.options.plot.series.bars.barWidth =
                         common.unixTimeToJavascriptTime(timeseries.resolution);
-                    
+
                     $.plot($self.element, [
                     {
                         label: "Promises Kept",
@@ -124,7 +124,7 @@
             includes: [],
             excludes: []
         },
-        
+
         _requestUrls: {
             timeseries: function($self) {
                 var url = $self.options.baseUrl + 'host/compliance/timeseries?';
@@ -132,11 +132,11 @@
                 if ($self._context.includes.length > 0) {
                     url = url + 'includes=' + encodeURIComponent($self._context.includes) + '&';
                 }
-                
+
                 if ($self._context.excludes.length > 0) {
                     url = url + 'excludes=' + encodeURIComponent($self._context.excludes);
                 }
-                
+
                 return url;
             }
         }
