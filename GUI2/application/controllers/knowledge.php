d0362d445f494603ca1188ead0a3060bfef8ad44
Nishes Joshi <nishes.joshi@cfengine.com>
2012-03-02 11:53:57 +0100

    Indentation and formatting fix for the knowledge controller.

--- GUI2/application/controllers/knowledge.php
+++ GUI2/application/controllers/knowledge.php
@@ -20,8 +20,8 @@ class Knowledge extends Cf_Controller {
         $rbac = $this->settings_model->app_settings_get_item('rbac');
         $isAdmin = $this->ion_auth->is_admin();
         if ($rbac && !$isAdmin) {
-            $errorMessage =  $this->lang->line('knowledge_access_denied');
-            if (is_ajax()) {                
+            $errorMessage = $this->lang->line('knowledge_access_denied');
+            if (is_ajax()) {
                 show_error_custom($errorMessage, 403);
             } else {
                 show_error($errorMessage, 403);
@@ -45,7 +45,7 @@ class Knowledge extends Cf_Controller {
             'breadcrumbs' => $this->breadcrumblist->display()
         );
         $requiredjs = array(
-            array('widgets/notes.js'),           
+            array('widgets/notes.js'),
             array('widgets/topicfinder.js'),
         );
         $this->carabiner->js($requiredjs);
@@ -76,12 +76,12 @@ class Knowledge extends Cf_Controller {
         );
 
         $root = getcwd();
-        
-        $root = $root . '/docs/';
+
+        $root = $root.'/docs/';
         try {
             $data['docs'] = $this->knowledge_model->listDocuments($root);
-        } catch(Exception $e) {
-            show_error($e->getMessage(),500);
+        } catch (Exception $e) {
+            show_error($e->getMessage(), 500);
         }
         $this->template->load('template', 'knowledge/docs', $data);
     }
@@ -96,17 +96,17 @@ class Knowledge extends Cf_Controller {
         $search = isset($getparams['search']) ? urldecode($getparams['search']) : $this->input->post('search');
         $data['search'] = trim($search);
         try {
-        if ($data['search']) {
-            $data['searchData'] = $this->knowledge_model->searchTopics($this->username, $data['search'], true);
-            $this->load->view('/knowledge/search_result', $data);
-        } else {
-            // search for default manuals
-            $pid = $this->knowledge_model->getPidForTopic($this->username, "any", "manuals");            
-            $data['searchData'] = $this->knowledge_model->showTopicHits($this->username, $pid);
-            $this->load->view('/knowledge/searchmanual', $data);
-        }
-        } catch(Exception $e) {
-            show_error_custom($e->getMessage(),500);
+            if ($data['search']) {
+                $data['searchData'] = $this->knowledge_model->searchTopics($this->username, $data['search'], true);
+                $this->load->view('/knowledge/search_result', $data);
+            } else {
+                // search for default manuals
+                $pid = $this->knowledge_model->getPidForTopic($this->username, "any", "manuals");
+                $data['searchData'] = $this->knowledge_model->showTopicHits($this->username, $pid);
+                $this->load->view('/knowledge/searchmanual', $data);
+            }
+        } catch (Exception $e) {
+            show_error_custom($e->getMessage(), 500);
         }
     }
 
@@ -122,55 +122,54 @@ class Knowledge extends Cf_Controller {
         $this->carabiner->group('iefix', array('js' => $jsIE));
         $this->carabiner->css('tabs-custom.css');
         $this->load->model('stories_model');
-
         $getparams = $this->uri->uri_to_assoc(3);
         $search = isset($getparams['search']) ? $getparams['search'] : $this->input->post('search');
         $topic = isset($getparams['topic']) ? $getparams['topic'] : $this->input->post('topic');
         $pid = isset($getparams['pid']) ? $getparams['pid'] : $this->input->post('pid');
         // chech for integer
         try {
-        $pid = intval($pid, 10);
-        if (!$pid)
-            $pid = $this->knowledge_model->getPidForTopic($this->username,"", "system policy");
+            $pid = intval($pid, 10);
+            if (!$pid) {
+                $pid = $this->knowledge_model->getPidForTopic($this->username, "", "system policy");
+            }
+            $breadcrumbs_url = "knowledge/knowledgemap/pid/$pid";
+            $bc = array(
+                'title' => $this->lang->line('breadcrumb_kw_bank'),
+                'url' => $breadcrumbs_url,
+                'isRoot' => false,
+                'replace_existing' => true
+            );
+            $this->breadcrumb->setBreadCrumb($bc);
 
-        $breadcrumbs_url = "knowledge/knowledgemap/pid/$pid";
-        $bc = array(
-            'title' => $this->lang->line('breadcrumb_kw_bank'),
-            'url' => $breadcrumbs_url,
-            'isRoot' => false,
-            'replace_existing' => true
-        );
-        $this->breadcrumb->setBreadCrumb($bc);
+            $data = array(
+                'search' => $search,
+                'topic' => $topic,
+                'pid' => $pid,
+                'title' => $this->lang->line('mission_portal_title')." - ".$this->lang->line('breadcrumb_kw_bank'),
+                'breadcrumbs' => $this->breadcrumblist->display(),
+            );
 
-        $data = array(
-            'search' => $search,
-            'topic' => $topic,
-            'pid' => $pid,
-            'title' => $this->lang->line('mission_portal_title') . " - " . $this->lang->line('breadcrumb_kw_bank'),
-            'breadcrumbs' => $this->breadcrumblist->display(),
-        );
+            $data['graphdata'] = $this->knowledge_model->getKnowledgeView($this->username, $pid);
 
-        $data['graphdata'] = $this->knowledge_model->getKnowledgeView($this->username, $pid);
+            $data['topicDetail'] = $this->knowledge_model->showTopics($this->username, $pid);
+            $data['topicHits'] = $this->knowledge_model->showTopicHits($this->username, $pid);
+            $data['topicLeads'] = $this->knowledge_model->showTopicLeads($this->username, $pid);
+            $data['topicCategory'] = $this->knowledge_model->showTopicCategory($this->username, $pid);
 
-        $data['topicDetail'] = $this->knowledge_model->showTopics($this->username, $pid);
-        $data['topicHits'] =    $this->knowledge_model->showTopicHits($this->username,$pid);
-        $data['topicLeads'] = $this->knowledge_model->showTopicLeads($this->username, $pid);
-        $data['topicCategory'] = $this->knowledge_model->showTopicCategory($this->username, $pid);
+            $data['showLeads'] = (!is_array($data['topicLeads']) || empty($data['topicLeads'])) ? false : true;
+            $data['showTopicHits'] = (!is_array($data['topicHits']) || empty($data['topicHits'])) ? false : true;
+            $data['showSameContext'] = (!is_array($data['topicCategory']['other_topics']) || empty($data['topicCategory']['other_topics'])) ? false : true;
+            $data['showSubTopics'] = (!is_array($data['topicCategory']['topic']['sub_topics']) || empty($data['topicCategory']['topic']['sub_topics'])) ? false : true;
 
-        $data['showLeads'] = (!is_array($data['topicLeads']) || empty($data['topicLeads'])) ? false : true;
-        $data['showTopicHits'] = (!is_array($data['topicHits']) || empty($data['topicHits'])) ? false : true;
-        $data['showSameContext'] = (!is_array($data['topicCategory']['other_topics']) || empty($data['topicCategory']['other_topics'])) ? false : true;
-        $data['showSubTopics'] = (!is_array($data['topicCategory']['topic']['sub_topics']) || empty($data['topicCategory']['topic']['sub_topics'])) ? false : true;
-
-        //for story generation
-        if (is_constellation()) {
-            $data['story'] = $this->stories_model->getStoryByName($data['topicDetail']['topic']);
-            $stories = json_decode(utf8_encode($data['story']), true);
-            $data['showStory'] = (!is_array($stories) || empty($stories['F'])) ? false : true;
-        }
-        $this->template->load('template', 'knowledge/knowledge', $data);
+            //for story generation
+            if (is_constellation()) {
+                $data['story'] = $this->stories_model->getStoryByName($data['topicDetail']['topic']);
+                $stories = json_decode(utf8_encode($data['story']), true);
+                $data['showStory'] = (!is_array($stories) || empty($stories['F'])) ? false : true;
+            }
+            $this->template->load('template', 'knowledge/knowledge', $data);
         } catch (Exception $e) {
-            show_error($e->getMessage(),500);
+            show_error($e->getMessage(), 500);
         }
     }
 
@@ -198,19 +197,18 @@ class Knowledge extends Cf_Controller {
         $data = array(
             'search' => htmlspecialchars($search),
             'topic' => $topic,
-            'title' => $this->lang->line('mission_portal_title') . " - " . $this->lang->line('breadcrumb_kw_bank'),
+            'title' => $this->lang->line('mission_portal_title')." - ".$this->lang->line('breadcrumb_kw_bank'),
             'breadcrumbs' => $this->breadcrumblist->display(),
-        );        
+        );
         try {
-        $data['searchData'] =  $this->knowledge_model->searchTopics($this->username, strtolower($search));
+            $data['searchData'] = $this->knowledge_model->searchTopics($this->username, strtolower($search));
         } catch (Exception $e) {
-            show_error($e->getMessage(),500);
+            show_error($e->getMessage(), 500);
         }
-
         // if only one result is found with id redirect to knowledge map else show list
         if (is_array($data['searchData']) && count($data['searchData']) == 1 && isset($data['searchData'][0]['id'])) {
             $pid = $data['searchData'][0]['id'];
-            redirect('/knowledge/knowledgemap/pid/' . $pid);
+            redirect('/knowledge/knowledgemap/pid/'.$pid);
             exit;
         }
 
