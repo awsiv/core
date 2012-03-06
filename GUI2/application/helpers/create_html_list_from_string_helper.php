dab477330bf3e93aaee53cf5686684c613ea2c7f
Dmitry Shevchenko <dmitry84@gmail.com>
2012-03-06 12:35:38 +0100

    added create_html_list_from_string

--- GUI2/application/helpers/create_html_list_from_string_helper.php
+++ GUI2/application/helpers/create_html_list_from_string_helper.php
@@ -1,9 +1,18 @@
 <?php
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
+/**
+ * Create html list from string.  Used for showing classes and bundles at create/edit roles page
+ * 
+ * @param string $value  string which can be converted to array with explode
+ * @param string $name - field name for checkboxes
+ * @param string $id  ul id=$id
+ * @param type $class ul class=$class
+ * @return string 
+ */
+function create_html_list_from_string($value, $name, $id, $class) {
 
-function roles_checkbox($value, $id, $class) {
-
+    //convert string to array
     $value = str_replace(' ', '', $value);
     $arr = explode(',', $value);
 
@@ -11,15 +20,14 @@ function roles_checkbox($value, $id, $class) {
     $first = true;
     $fist_item_class = ' class="first"';
  
-    
 
 $ret = '<ul id="'.$id .'" class="'.$class.'">';
 
     foreach ($arr as $item) {
-            $ret.= '<li id="" itemid="'.$i.'" '.($first === true ?  $fist_item_class : "").' >';
+            $ret.= '<li itemid="'.$i.'" '.($first === true ?  $fist_item_class : "").' >';
             $data = array(
-                'name'        => "crxi[]",
-                'id'          => '',
+                'name'        => $name,
+                'id'          => $item,
                 'value'       => $item,
                 'checked'     => TRUE,
                 'style'       => '',
