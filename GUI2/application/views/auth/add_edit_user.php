ecd034baf3dec2306a6039cee389b8c909981df0
Dmitry Shevchenko <dmitry84@gmail.com>
2012-03-06 12:37:56 +0100

    new view for ad and edit user

--- GUI2/application/views/auth/add_edit_user.php
+++ GUI2/application/views/auth/add_edit_user.php
@@ -1,79 +1,136 @@
-<div id="infoMessage"><?php echo $message;?></div>
-    <div class="form">
-    <?php echo form_open("auth/edit_user/".$this->uri->segment(3),array('id'=>'edit_user'));?>
-    
-      <p>
-       <?php
-       echo form_label('User Name',$user_name['name']);
-       echo form_input($user_name);?>
-      </p>
-      
-      <p>
-      <?php
-       echo form_label('Email',$email['name']);
-       echo form_input($email);?>
-      </p>
-      
-      <p>
-        <label for="reset_password">Reset Password</label>
-      	<input type=checkbox name="reset_password"> 
-      </p>
-      
-      <div class="roles_wrapper">
-          <h2>Roles</h3>
-            <div class="role_list">
-                <h3>Assigned</h3>
-                <ul id="user_roles">
-                <?php 
-                 $i=1;
-                 $first = true;
-                 $fist_item_class = ' class="first"';
-                 
-                 foreach($roles as $role)
-                    {
-                      if ($role['checked'] != '') { 
-                          //$role['checked'] = false; // unc
-                          echo '<li id="li_item'.$i.'" itemid="'.$i.'" '.($first === true ?  $fist_item_class : "").' >';
-                              echo form_checkbox($role, '','','class="selectit"');
-                              echo form_label($role['id'], $role['id'],array('class'=>'role'));
-                          echo "</li>";     
-                          $i++;
-                          $first = false;
-                      }
-                    }
-                ?>
-                </ul>
-                <div class="clearboth">&nbsp;</div>
-            </div>  
-            <div class="switchers">
-                <a href="javascript:void(0);" id="move_left">&laquo; Left</a><br /><br />
-                <a href="javascript:void(0);" id="move_right">Right &raquo;</a><br /><br />
-                <div class="clearboth">&nbsp;</div>
-            </div>
-            <div class="role_list">
-                <h3>Available</h3>                
-                <ul id="all_roles">
-                <?php 
-                 $first = true;
-                  foreach($roles as $role)
-                    {
-                      if ($role['checked'] == '') {
-                        echo '<li id="li_item'.$i.'" itemid="'.$i.'" '.($first === true ? $fist_item_class : "").' >';
-                            echo form_checkbox($role, '','','class="selectit" disabled="disabled"');
-                            echo form_label($role['id'], $role['id'],array('class'=>'role'));
-                        echo "</li>";     
-                        $i++;
-                        $first = false;
-                      }
-                    }
-                ?>
-                 </ul>
-                 <div class="clearboth">&nbsp;</div>
-            </div>
-          <div class="clearboth">&nbsp;</div>
-          </div>
+<div id="infoMessage"><?php echo $message; ?></div>
+<div class="form">
 
- 
-      <p id="btnholder"><?php echo form_submit(array('name'=>'submit','class'=>'btn','value'=>'Update'));?></p>
-    <?php echo form_close();?>
-  </div>
+    <?php
+    if ($op == 'edit')
+        echo form_open("auth/edit_user/" . $this->uri->segment(3), array('id' => 'edit_user'));
+    else
+        echo form_open("auth/create_user", array('id' => 'create_user'));
+    ?>
+
+    <p>
+        <?php
+        echo form_label('User Name', $user_name['name']);
+        echo form_input($user_name);
+        ?>
+    </p>
+
+    <p>
+        <?php
+        echo form_label('Email', $email['name']);
+        echo form_input($email);
+        ?>
+    </p>
+        <?php if ($op == 'create') { ?>
+        <p>
+    <?php echo form_label('Password', $password['name']);
+    echo form_input($password);
+    ?>
+        </p>
+
+        <p>
+        <?php echo form_label('Confirm Password', $password_confirm['name']);
+        echo form_input($password_confirm);
+        ?>
+        </p>
+
+    <? } else { ?>
+        <p>
+            <label for="reset_password">Reset Password</label>
+            <input type=checkbox name="reset_password"> 
+        </p>
+<?php } ?>
+
+    <table cellpadding="0" cellspasing ="0" border="1"  id="maintable">
+        <tr>
+            <td colspan="3">  
+                <h2>Roles</h2>
+<?php ?></td>
+        </tr>     
+
+        <tr>
+            <td class="edit_cell">
+                <div class="DragNDrop assigned dialog_box_style ui-dialog"  style="position: relative">
+                    <div class="ui-dialog-titlebar"><h6>Assigned</h6></div>
+                    <div class="itemwrapper assigneditems" id="">
+
+                        <?php if (!empty($user_roles)) { 
+                            $i = 1;
+                            $first = true;
+                            $fist_item_class = ' class="first"';
+
+
+                            echo '<ul id="roles" class="roleslist itemlist assignedList">';
+
+                            foreach ($user_roles as $item) {
+                                echo '<li " ' . ($first === TRUE ? $fist_item_class : "") . ' >';
+                                $data = array(
+                                    'name' => $item['name'],
+                                    'id' => $item['value'],
+                                    'value' => $item['value'],
+                                    'checked' => TRUE,
+                                    'style' => '',
+                                    'class' => "selectit"
+                                );
+                                echo form_checkbox($data);
+                                echo form_label($item['value'], $item['value']);
+                                echo "</li>";
+                                $i++;
+                                $first = FALSE;
+                            }
+
+                            echo '</ul>';
+                          } else { ?>
+                            <ul id="roles" class="roleslist itemlist assignedList empty_list"></ul>
+                            <div class="empty_list_warning">No roles assigned</div>
+                        <?php } ?>
+                    </div>
+                </div>            
+            </td>
+            <td class="arrows">
+                <a href="javascript:void(0);" class="move_btn" dest="roles" sourse="all_roles">&laquo; Left</a><br /><br />
+                <a href="javascript:void(0);" class="move_btn" dest="all_roles" sourse="roles">Right &raquo;</a><br /><br />
+            </td>
+            <td rowspan="2">
+                <div class="DragNDrop assigned dialog_box_style ui-dialog" style="position: relative">
+                    <div class="ui-dialog-titlebar"><h6>Available</h6></div>
+                    <div class="itemwrapper assigneditems" id="">
+
+                        <?php if (!empty($roles)) { 
+                            $i = 1;
+                            $first = true;
+                            $fist_item_class = ' class="first"';
+
+
+                            echo '<ul id="all_roles" class="roleslist itemlist assignedList">';
+
+                            foreach ($roles as $item) {
+                                echo '<li " ' . ($first === true ? $fist_item_class : "") . ' >';
+                                echo '<span>' . $item['value'] . '</span>';
+                                echo "</li>";
+                                $i++;
+                                $first = false;
+                            }
+
+                            echo '</ul>';
+                  
+                            } else { ?>
+                            <ul id="all_roles" class="rolelist itemlist assignedList empty_list"></ul>
+                            <div class="empty_list_warning">No roles available </div>
+                      <?php } ?>
+                    </div>
+                </div>     
+            </td>   
+        </tr>            
+    </table>     
+
+    <p id="btnholder"><?php
+        if ($op == 'edit') {
+            echo form_submit(array('name' => 'submit', 'class' => 'btn', 'value' => 'Update'));
+        } else {
+            echo form_submit(array('name' => 'submit', 'class' => 'btn', 'value' => 'Create User'));
+        }
+        ?>
+    </p>
+        <?php echo form_close(); ?>
+</div>
