981dc3a944deb1deb372ac4c85884007de87282b
sudhirpandey <sudhir.pandey@cfengine.com>
2012-03-07 15:11:40 +0100

    Partial views for search filters updated according to the new reports design

--- GUI2/application/views/searchpages/last_saw_hosts.php
+++ GUI2/application/views/searchpages/last_saw_hosts.php
@@ -1,38 +1,47 @@
  <div id="searchform" class="panel"><div class="panelhead"><?php echo $report_type?> query</div>
  <div class="panelcontent">
           <form method="post" action="<?php echo site_url('search')?>">
-          <p>Remote host name: (.*+[])<input class="searchfield" type="text" name="name" value="<?php echo (is_array($paramArray) && isset($paramArray['name'])) ? trim($paramArray['name']) : ''; ?>"></p>
-          <p>Remote IP address: (.*+[])<input class="searchfield" type="text" name="address" value="<?php echo (is_array($paramArray) && isset($paramArray['address'])) ? trim($paramArray['address']) : ''; ?>"></p>
-          <p>Remote host key:<input class="searchfield" type="text" name="key" value="<?php echo (is_array($paramArray) && isset($paramArray['key'])) ? trim($paramArray['key']) : ''; ?>"></p>
-          <p>Minimum hours ago:<p><input class="searchfield" type="text" name="ago" value="<?php echo (is_array($paramArray) && isset($paramArray['ago'])) ? trim($paramArray['ago']) : ''; ?>"></p>
-          <p>Host class: (.*+[])
-            <a href="<?php echo site_url('widget/allclasses')?>" id="bphghelp" class="help"
-             title="<?php echo $this->lang->line('report_hostgp_help');?>">Help ?</a>
-          <input class="searchfield" type="text" name="class_regex" size="80" id="hg" value="<?php echo (is_array($paramArray) && isset($paramArray['class_regex'])) ? trim($paramArray['class_regex']) : ''; ?>"></p>
-          <!--<input type="hidden" name="manyhosts" value="true">-->
+          <p><label>Remote host name: (.*+[])</label><input class="searchfield" type="text" name="name" value="<?php echo (is_array($paramArray) && isset($paramArray['name'])) ? trim($paramArray['name']) : ''; ?>"></p>
+           
+          <div id="morefilters" style="display:none">
+          <p><label>Remote IP address: (.*+[])<label><input class="searchfield" type="text" name="address" value="<?php echo (is_array($paramArray) && isset($paramArray['address'])) ? trim($paramArray['address']) : ''; ?>"></p>
+          <p><label>Remote host key:</label><input class="searchfield" type="text" name="key" value="<?php echo (is_array($paramArray) && isset($paramArray['key'])) ? trim($paramArray['key']) : ''; ?>"></p>
+          <p><label>Minimum hours ago:</label><input class="searchfield" type="text" name="ago" value="<?php echo (is_array($paramArray) && isset($paramArray['ago'])) ? trim($paramArray['ago']) : ''; ?>"></p>
+           </div>
+          
+          <p>
+             <a href="<?php echo site_url('widget/contextfinder')?>" id="hclist" class="hostcontextddl floatleft" title="<?php echo $this->lang->line('report_hostgp_help');?>">Hostcontext</a>
+             <span class="floatright"> Return host names only: <input type="checkbox" name="hosts_only" value="true"></span>
+             <span class="clear"></span>
+          </p>
+          
+          <p class="alignrightbuttoncontainer">
+             <span class="green_btn"><input class="green_btn" type="submit" value="Generate report"></input></span>
+             <a  id="morefiltertoggle"class="green_btn" href="#"><span>More Filters</span></a>
+          </p>
+          
           <input type="hidden" name="host" value="All">
+          <input type="hidden" name="inclist" value="<?php echo (is_array($paramArray) && isset($paramArray['inclist'])) ? trim($paramArray['inclist']) : ''; ?>">
+          <input type="hidden" name="exlist" value="<?php echo (is_array($paramArray) && isset($paramArray['exlist'])) ? trim($paramArray['exlist']) : ''; ?>">
           <input type="hidden" name="report" value="<?php echo $report_type?>">
-          <p>Return hostnames only: <input type="checkbox" name="hosts_only" value="true"></p>
-          <p><input class="btn" type="submit" value="Generate report"> <span>       <a href="<?php echo site_url("/savedsearch/listSavedSearches/$report_type"); ?>"  class="btn loadsavedsearch">Load saved searches</a>
-</span> </p>
+          
         </form>
     </div>
  </div>
 <script type="text/javascript">
-    $('.loadsavedsearch').ajaxyDialog({title:'Saved Searches'});
+ var $incList=$('input:hidden[name=inclist]');
+ var $exList=$('input:hidden[name=exlist]');
+ 
+ $('#hclist').contextfinder({
+          baseUrl: '<?php echo site_url() ?>',
+          complete:function(event,data){
+              $('#hg').data('list',data.inccl).val('includes='+data.inccl.incList+' exclude='+data.inccl.exList);
+               $incList.val(data.inccl.incList);
+               $exList.val(data.inccl.exList);
+          }
+  }); 
 
-// $('#hg').smartTextBox({separator : "|",editOnFocus:true});
-$('#bphghelp').classfinder({
-    defaultbehaviour:false,
-    complete:function(event,data){
-        //console.log(data.selectedclass);
-        //$('#hg') .smartTextBox('add', data.selectedclass);
-            var val=$('#hg').val()
-        if(val==""){
-             $('#hg').val(data.selectedclass);
-        }else{
-            $('#hg').val(val+'|'+data.selectedclass);
-        }
-    }
+$('#morefiltertoggle').live('click',function(){
+     $('#morefilters').toggle();
 });
 </script>
--- GUI2/application/views/searchpages/variables.php
+++ GUI2/application/views/searchpages/variables.php
@@ -2,10 +2,12 @@
     <div class="panelhead"><?php echo $report_type ?> query</div>
     <div class="panelcontent">
         <form method="post" action="<?php echo site_url('search') ?>">
-            <p>Scope or bundle: (.*+[])<input class="searchfield" type="text" name="scope" value="<?php echo (is_array($paramArray) && isset($paramArray['scope'])) ? trim($paramArray['scope']) : ''; ?>"></p>
-            <p>Lvalue or name: (.*+[])<input class="searchfield" type="text" name="lval" value="<?php echo (is_array($paramArray) && isset($paramArray['lval'])) ? trim($paramArray['lval']) : ''; ?>"></p>
-            <p>Rvalue or content: (.*+[])<input class="searchfield" type="text" name="rval" value="<?php echo (is_array($paramArray) && isset($paramArray['rval'])) ? trim($paramArray['rval']) : ''; ?>"></p>
-            <p>Type:<p>
+            <p><label>Scope or bundle: (.*+[])</label><input class="searchfield" type="text" name="scope" value="<?php echo (is_array($paramArray) && isset($paramArray['scope'])) ? trim($paramArray['scope']) : ''; ?>"></p>
+            
+            <div id="morefilters" style="display:none">
+            <p><label>Lvalue or name: (.*+[])</label><input class="searchfield" type="text" name="lval" value="<?php echo (is_array($paramArray) && isset($paramArray['lval'])) ? trim($paramArray['lval']) : ''; ?>"></p>
+            <p><label>Rvalue or content: (.*+[])</label><input class="searchfield" type="text" name="rval" value="<?php echo (is_array($paramArray) && isset($paramArray['rval'])) ? trim($paramArray['rval']) : ''; ?>"></p>
+            <p><label>Type:</label>
                 <select name="type">
                     <option value="s" <?php echo (is_array($paramArray) && isset($paramArray['type']) && $paramArray['type'] == 's') ? 'selected' : ''; ?>>String</option>
                     <option value="sl" <?php echo (is_array($paramArray) && isset($paramArray['type']) && $paramArray['type'] == 'sl') ? 'selected' : ''; ?>>String list</option>
@@ -14,33 +16,41 @@
                     <option value="r" <?php echo (is_array($paramArray) && isset($paramArray['type']) && $paramArray['type'] == 'r') ? 'selected' : ''; ?>>Real</option>
                     <option value="rl" <?php echo (is_array($paramArray) && isset($paramArray['type']) && $paramArray['type'] == 'rl') ? 'selected' : ''; ?>>Real list</option>
                 </select>
-            <p>Host class: (.*+[])
-                <a href="<?php echo site_url('widget/allclasses') ?>" id="bphghelp" class="help"
-                   title="<?php echo $this->lang->line('report_hostgp_help'); ?>">Help ?</a>
-                <input class="searchfield" type="text" name="class_regex" size="80" id="hg" value="<?php echo (is_array($paramArray) && isset($paramArray['class_regex'])) ? trim($paramArray['class_regex']) : ''; ?>"></p>
+            </div>
+            
+             <p>
+             <a href="<?php echo site_url('widget/contextfinder')?>" id="hclist" class="hostcontextddl floatleft" title="<?php echo $this->lang->line('report_hostgp_help');?>">Hostcontext</a>
+             <span class="floatright"> Return host names only: <input type="checkbox" name="hosts_only" value="true"></span>
+             <span class="clear"></span>
+            </p>
+            
+             <p class="alignrightbuttoncontainer">
+             <span class="green_btn"><input class="green_btn" type="submit" value="Generate report"></input></span>
+             <a  id="morefiltertoggle"class="green_btn" href="#"><span>More Filters</span></a>
+             </p>
+             
             <input type="hidden" name="host" value="All">
+            <input type="hidden" name="inclist" value="<?php echo (is_array($paramArray) && isset($paramArray['inclist'])) ? trim($paramArray['inclist']) : ''; ?>">
+            <input type="hidden" name="exlist" value="<?php echo (is_array($paramArray) && isset($paramArray['exlist'])) ? trim($paramArray['exlist']) : ''; ?>">
             <input type="hidden" name="report" value="<?php echo $report_type ?>">
-            <p>Return hostnames only: <input type="checkbox" name="hosts_only" value="true"></p>
-            <p><input class="btn" type="submit" value="Generate report"> <span>       <a href="<?php echo site_url("/savedsearch/listSavedSearches/$report_type"); ?>"  class="btn loadsavedsearch">Load saved searches</a>
-                </span> </p>
+            
         </form>
     </div>
 </div>
 <script type="text/javascript">
-    $('.loadsavedsearch').ajaxyDialog({title:'Saved Searches'});
+ var $incList=$('input:hidden[name=inclist]');
+ var $exList=$('input:hidden[name=exlist]');
+ 
+ $('#hclist').contextfinder({
+          baseUrl: '<?php echo site_url() ?>',
+          complete:function(event,data){
+              $('#hg').data('list',data.inccl).val('includes='+data.inccl.incList+' exclude='+data.inccl.exList);
+               $incList.val(data.inccl.incList);
+               $exList.val(data.inccl.exList);
+          }
+  }); 
 
-    //$('#hg').smartTextBox({separator : "|",editOnFocus:true});
-    $('#bphghelp').classfinder({
-        defaultbehaviour:false,
-        complete:function(event,data){
-            //console.log(data.selectedclass);
-            //$('#hg') .smartTextBox('add', data.selectedclass);
-               var val=$('#hg').val()
-        if(val==""){
-             $('#hg').val(data.selectedclass);
-        }else{
-            $('#hg').val(val+'|'+data.selectedclass);
-        }
-        }
-    });
+$('#morefiltertoggle').live('click',function(){
+     $('#morefilters').toggle();
+});
 </script>
