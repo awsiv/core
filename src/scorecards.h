2bc6426c7543e4e270ed5ce1723162248c2b62ff
maciej.mrowiec <maciej.mrowiec@cfengine.com>
2012-03-08 01:32:42 +0100

    remove unused functions Nova_{Green,Yellow,Red,Blue}Hosts and Nova_ClassifyHostState

--- src/scorecards.h
+++ src/scorecards.h
@@ -13,12 +13,7 @@ const char *Nova_HostColourToString(HostColour colour);
 int Nova_GetComplianceScore(HostRankMethod method, double *k, double *rep);
 
 // gets meters from DB, uses GetComplianceScore. rework.
-Item *Nova_ClassifyHostState(HostClassFilter *hostClassFilter, HostRankMethod method);
 int Nova_GetHostColour(char *lkeyhash);
-Item *Nova_GreenHosts(HostClassFilter *hostClassFilter);
-Item *Nova_YellowHosts(HostClassFilter *hostClassFilter);
-Item *Nova_RedHosts(HostClassFilter *hostClassFilter);
-Item *Nova_BlueHosts(HostClassFilter *hostClassFilter);
 
 // legacy
 void ComplianceSummaryGraph(char *hubKeyHash, char *policy, bool constellation, char *buffer, int bufsize);
