70abde921faa8cd763c4a81ec4fdb53014fb8d7d
sudhirpandey <sudhir.pandey@cfengine.com>
2012-03-12 01:36:18 +0100

    test cases for the report model's functions

--- GUI2/tests/models/test_report_model.php
+++ GUI2/tests/models/test_report_model.php
@@ -1,12 +1,13 @@
 <?php
 class test_report_model extends CodeIgniterUnitTestCase {
-  
+  private $report_id;
     public function __construct() {
         parent::__construct();
     }
 
     public function setUp() {
         $this->_ci->load->model('report_model');
+        $this->report_id='promises-repaired-summary';
     }
 
     public function tearDown() {
@@ -20,9 +21,28 @@ class test_report_model extends CodeIgniterUnitTestCase {
     }
    
     public function test_getReportCategory(){
-         $report_category = $this->_ci->report_model->getReportCategory('promises-repaired-summary');
-         //$this->assertTrue(is_string($report_category),"Must be a valid name");
+         $report_category = $this->_ci->report_model->getReportCategory($this->report_id);
+         $this->assertTrue(is_string($report_category),"Must be a valid name");
          $this->dump($report_category); 
+    }
+    
+    public function test_getReportTitle(){
+        $report_title= $this->_ci->report_model->getReportTitle($this->report_id);
+        $this->assertTrue(is_string($report_title),"Must be a valid name"); 
+        $this->dump($report_title);
+    }
+   
+    public function test_getReportDetails(){
+        $report_details= $this->_ci->report_model->getReportDetail($this->report_id);
+        $this->assertTrue(is_array($report_details),"Must be a valid array with details"); 
+        $this->dump($report_details);
+    }
+    
+   
+     public function test_getReportDescription(){
+        $report_details= $this->_ci->report_model->getReportDescription($this->report_id);
+        $this->assertTrue(is_string($report_details),"Must be a valid array with details"); 
+        $this->dump($report_details);
     }
 }
 ?>
