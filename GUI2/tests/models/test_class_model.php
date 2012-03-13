b86c3bb16add10dc871fe63d6194219bd760e04d
Dmitry Shevchenko <dmitry84@gmail.com>
2012-03-13 16:25:42 +0100

    tests for include/exclude classes support

--- GUI2/tests/models/test_class_model.php
+++ GUI2/tests/models/test_class_model.php
@@ -1,12 +1,15 @@
 <?php
 class test_class_model extends CodeIgniterUnitTestCase {
-    private $username="admin";
     public function __construct() {
         parent::__construct();
     }
 
     public function setUp() {
         $this->_ci->load->model('class_model');
+        $this->username = 'admin';
+        
+        $this->default_include = array("cf.*");
+        $this->default_exclude = array("zz.*");
     }
 
     public function tearDown() {
@@ -14,21 +17,42 @@ class test_class_model extends CodeIgniterUnitTestCase {
     }
     
      public function test_getClassList(){
-         $name = $this->_ci->class_model->getAllClasses($this->username,".*",10,1);
+         $name = $this->_ci->class_model->getAllClasses($this->username, array( ".*"), array(''), 10,1);
+         $this->assertTrue(is_array(json_decode($name)),"Must be a valid set of class");
+         $this->dump($name); 
+    }
+    
+    public function test_getClassListWithParam(){
+         $name = $this->_ci->class_model->getAllClasses($this->username, $this->default_include, $this->default_exclude, 10,1);
          $this->assertTrue(is_array(json_decode($name)),"Must be a valid set of class");
          $this->dump($name); 
     }
     
     public function test_getAllTimeClasses(){
-         $name = $this->_ci->class_model->getAllTimeClasses($this->username,".*",10,1);
+         $name = $this->_ci->class_model->getAllTimeClasses($this->username,".*", '',10,1);
          $this->assertTrue(is_array(json_decode($name)),"Must be a valid set of class");
          $this->dump($name); 
     }
     
-      public function test_getAllSoftClasses(){
-         $name = $this->_ci->class_model->getAllSoftClasses($this->username,".*",10,1);
+    
+    public function test_getAllTimeClassesWithParam(){
+         $name = $this->_ci->class_model->getAllTimeClasses($this->username, $this->default_include, $this->default_exclude, 10,1);
          $this->assertTrue(is_array(json_decode($name)),"Must be a valid set of class");
          $this->dump($name); 
     }
+    
+    public function test_getAllSoftClasses(){
+         $name = $this->_ci->class_model->getAllSoftClasses($this->username,".*", '',10,1);
+         $this->assertTrue(is_array(json_decode($name)),"Must be a valid set of class");
+         $this->dump($name); 
+    }
+    
+    
+    public function test_getAllSoftClassesWithParam(){
+        $name = $this->_ci->class_model->getAllSoftClasses($this->username, $this->default_include, $this->default_exclude,10,1);
+        $this->assertTrue(is_array(json_decode($name)),"Must be a valid set of class");
+        $this->dump($name); 
+    }
+    
 }
 ?>
