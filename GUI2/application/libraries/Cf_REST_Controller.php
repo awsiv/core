47ce979f17aa90ae89c8cba69ad33e2371683adf
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-03-15 14:44:32 +0100

    Add authentication to Cf_REST_Controller

--- GUI2/application/libraries/Cf_REST_Controller.php
+++ GUI2/application/libraries/Cf_REST_Controller.php
@@ -3,7 +3,8 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Cf_REST_Controller extends CI_Controller {
+class Cf_REST_Controller extends CI_Controller
+{
 
     protected $request = NULL;
     protected $response = NULL;
@@ -11,22 +12,36 @@ class Cf_REST_Controller extends CI_Controller {
     protected $_post_args = array();
     protected $_put_args = array();
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
 
         initializeHub();
 
         $this->load->library(array(
             'security',
-            'setting_lib'
-            ));
+            'setting_lib',
+            'ion_auth',
+            'OnlineUsers'
+        ));
+
+        if (!$this->ion_auth->logged_in() && !is_ajax())
+        {
+            $this->respond_unauthorized();
+        }
+        else
+        {
+            $this->onlineusers->OnlineUsers();
+        }
 
         $this->request->method = $this->_detect_method();
         $this->_get_args = array_merge($this->_get_args, $this->uri->ruri_to_assoc());
         $this->request->body = NULL;
 
-        switch ($this->request->method) {
+        switch ($this->request->method)
+        {
             case 'get':
+                $get = null;
                 parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $get);
                 empty($get) OR $this->_get_args = $get;
                 break;
@@ -45,34 +60,46 @@ class Cf_REST_Controller extends CI_Controller {
         }
     }
 
-    public function _remap($object_called, $arguments) {
+    public function _remap($object_called, $arguments)
+    {
 
         $controller_method = $object_called . '_' . $this->request->method;
 
-        if (!method_exists($this, $controller_method)) {
+        if (!method_exists($this, $controller_method))
+        {
             $this->respond(array('status' => false, 'error' => 'Unknown method.'), 404);
         }
 
         call_user_func_array(array($this, $controller_method), $arguments);
     }
 
-    public function respond($http_code, $data = NULL) {
+    public function respond($http_code, $data = NULL)
+    {
         header('HTTP/1.1: ' . $http_code);
         header('Status: ' . $http_code);
         header('Content-Length: ' . strlen($data));
         exit($data);
     }
 
-    public function respond_ok($data) {
-        if (is_null($data)) {
+    public function respond_ok($data)
+    {
+        if (is_null($data))
+        {
             return $this->respond(204); // No Content
         }
-        else {
+        else
+        {
             return $this->respond(200, $data); // OK
         }
     }
 
-    public function respond_not_found() {
+    public function respond_unauthorized()
+    {
+        return $this->respond(401);
+    }
+
+    public function respond_not_found()
+    {
         return $this->respond(404);
     }
 
@@ -86,17 +113,19 @@ class Cf_REST_Controller extends CI_Controller {
         return $this->respond(500, $message);
     }
 
-    protected function _detect_method() {
+    protected function _detect_method()
+    {
         $method = strtolower($this->input->server('REQUEST_METHOD'));
 
-        if (in_array($method, array('get', 'delete', 'post', 'put'))) {
+        if (in_array($method, array('get', 'delete', 'post', 'put')))
+        {
             return $method;
         }
 
         return 'get';
     }
 
-     protected function param_includes()
+    protected function param_includes()
     {
         $value = $this->param_list('includes', TRUE);
         if ($value)
