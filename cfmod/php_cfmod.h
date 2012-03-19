1b78b40e7340bd0471358f66245e0792e1084f99
Mikhail Gusarov <mikhail.gusarov@cfengine.com>
2012-03-19 09:29:27 +0100

    Fix indentation in php_cfmod.h

--- cfmod/php_cfmod.h
+++ cfmod/php_cfmod.h
@@ -5,25 +5,25 @@
 */
 
 #ifndef PHP_CFMOD_H
-# define PHP_CFMOD_H
+#define PHP_CFMOD_H
 
-# ifdef HAVE_CONFIG_H
-#  include "conf.h"
-# endif
+#ifdef HAVE_CONFIG_H
+# include "conf.h"
+#endif
 
 /* PHP has HAVE_LCHOWN in php_config.h */
-# ifdef HAVE_LCHOWN
-#  undef HAVE_LCHOWN
-# endif
+#ifdef HAVE_LCHOWN
+# undef HAVE_LCHOWN
+#endif
 
-# include "php.h"
-# include "php_ini.h"
-# include "ext/standard/info.h"
-# include "zend_exceptions.h"
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "zend_exceptions.h"
 
 extern zend_module_entry cfmod_module_entry;
 
-# define phpext_cfmod_ptr &cfmod_module_entry
+#define phpext_cfmod_ptr &cfmod_module_entry
 
 extern zend_class_entry *cfmod_ldap_exception_ce;
 extern zend_class_entry *cfmod_exception_db;
@@ -31,17 +31,17 @@ extern zend_class_entry *cfmod_exception_args;
 extern zend_class_entry *cfmod_exception_rbac;
 extern zend_class_entry *cfmod_exception_generic;       // split if desireable
 
-# ifdef PHP_WIN32
-#  	define PHP_CFMOD_API __declspec(dllexport)
-# elif defined(__GNUC__) && __GNUC__ >= 4
-#  	define PHP_CFMOD_API __attribute__ ((visibility("default")))
-# else
-#  	define PHP_CFMOD_API
-# endif
+#ifdef PHP_WIN32
+# define PHP_CFMOD_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+# define PHP_CFMOD_API __attribute__ ((visibility("default")))
+#else
+# define PHP_CFMOD_API
+#endif
 
-# ifdef ZTS
-#  include "TSRM.h"
-# endif
+#ifdef ZTS
+# include "TSRM.h"
+#endif
 
 PHP_MINIT_FUNCTION(cfmod);
 PHP_MSHUTDOWN_FUNCTION(cfmod);
@@ -69,11 +69,11 @@ ZEND_END_MODULE_GLOBALS(cfmod)
    examples in any other php module directory.
 */
 
-# ifdef ZTS
-#  define CFMOD_G(v) TSRMG(cfmod_globals_id, zend_cfmod_globals *, v)
-# else
-#  define CFMOD_G(v) (cfmod_globals.v)
-# endif
+#ifdef ZTS
+# define CFMOD_G(v) TSRMG(cfmod_globals_id, zend_cfmod_globals *, v)
+#else
+# define CFMOD_G(v) (cfmod_globals.v)
+#endif
 
 /*
  * Local variables:
