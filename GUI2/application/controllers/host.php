b3169cb2cd7f85ce76521ec0d69f5a38c8e0ef31
sudhirpandey <sudhir.pandey@cfengine.com>
2012-03-23 17:28:30 +0100

    Introduction of new function to get compliance count and connectivity count to reduce the number of request send to server

--- GUI2/application/controllers/host.php
+++ GUI2/application/controllers/host.php
@@ -7,6 +7,7 @@ class Host extends Cf_REST_Controller {
     function __construct() {
         parent::__construct();
         $this->username = $this->session->userdata('username');
+        $this->load->model('host_model');
     }
 
     function count_get()
@@ -16,13 +17,39 @@ class Host extends Cf_REST_Controller {
             $result = cfpr_host_count($this->username,
                     $this->param('colour'), $this->param_includes(),
                     $this->param_excludes());
-
             $this->respond_ok((string)$result);
         }
         catch (CFModExceptionRBAC $e)
         {
             $this->respond_forbidden();
         }
+    }
+    
+    function countCompliance_get(){
+      try{
+         $hostcount=array(
+                  "red"=> $this->host_model->getHostCount($this->username,'red',$this->param_includes(),$this->param_excludes()),
+                  "green"=>$this->host_model->getHostCount($this->username,'green',$this->param_includes(),$this->param_excludes()),
+                  "yellow"=>$this->host_model->getHostCount($this->username,'yellow',$this->param_includes(),$this->param_excludes())
+         );
+         sleep(10);
+         echo json_encode($hostcount);
+         }catch(Exception $e){
+          show_error_custom($e->getMessage(), $e->getCode());
+       }
+    }
+    
+    function countConnectivity_get(){
+        try{
+         $hostcount=array(
+                  "blue"=> $this->host_model->getHostCount($this->username,'blue',$this->param_includes(),$this->param_excludes()),
+                  "black"=>$this->host_model->getHostCount($this->username,'black',$this->param_includes(),$this->param_excludes())
+         );
+         sleep(10);
+         echo json_encode($hostcount);
+         }catch(Exception $e){
+          show_error_custom($e->getMessage(), $e->getCode());
+       } 
     }
 
     function info_get($hostKey = NULL) {
