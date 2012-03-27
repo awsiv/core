f7a768d00c0f297b393c825a89eba59ee306eb8c
Dmitry Shevchenko <dmitry84@gmail.com>
2012-03-27 02:12:20 +0200

    restyle paginator

--- GUI2/application/views/searchpages/paging_footer.php
+++ GUI2/application/views/searchpages/paging_footer.php
@@ -19,19 +19,20 @@ $path = isset($url) ? $url : 'search/index/';
 
                 <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . "/page/$i") ?>" title="Go to Page <?php echo $i ?>" class="page <?php echo $current_class; ?>"><span><?php echo $i; ?></span></a>
 
-<?php } ?>
+            <?php } ?>
 
             <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . '/page/' . $pg['next']) ?>" title="Go to Next Page" class="next"><span>></span></a>
             <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . '/page/' . $pg['last']) ?>" title="Go to Last Page" class="last"><span>Last</span></a>
-        </div>
+        
+           <div class="page_counter">
+        <?php
+        echo form_open($path . $params);
+        echo form_input('rows', $number_of_rows);
+        echo "<span>  Rows/Page</span>";
+        echo form_close();
+        ?>
     </div>
-
-</div>
-<div>
-    <?php
-    echo form_open($path . $params);
-    echo form_input('rows', $number_of_rows);
-    echo "  Rows/Page";
-    echo form_close();
-    ?>
+        </div>
+         
+    </div>
 </div>
