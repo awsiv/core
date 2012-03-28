ed3a61fbe4389ba1833c24ecacbfa406d306d8b6
Dmitry Shevchenko <dmitry84@gmail.com>
2012-03-28 16:14:54 +0200

    include css and js for view via carabiner delete them from view

--- GUI2/application/controllers/engineering.php
+++ GUI2/application/controllers/engineering.php
@@ -13,9 +13,48 @@ class Engineering extends Cf_Controller
 
         $requiredjs = array(
             array('flot/jquery.flot.js'),
-            array('flot/jquery.flot.stack.js')
-        );
+            array('flot/jquery.flot.stack.js'),
+            array('common.js'),
+            array('widgets/astrolabe.js'),
+            array('widgets/astrolabeLocation.js'),
+            array('widgets/combobox.js'),
+            array('widgets/searchEntry.js'),
+            array('widgets/hostsCompliance.js'),
+            array('widgets/hostsConnectivity.js'),
+            array('widgets/hostsComplianceTimeseries.js'),
+            array('widgets/hostInfo.js'),
+            array('widgets/classfinder.js'),
+            array('widgets/hostfinder.js'),
+            array('widgets/policyfinder.js'),
+            array('jScrollPane.js'),
+            array('jquery.mousewheel.js'),
+            array('jquery.contextMenu.js'),
 
+
+            array('widgets/notes.js'),
+            array('widgets/classfinder.js'),
+            array('widgets/contextfinder.js'),
+            array('widgets/reportUI.js')
+    );
+        
+        $this->carabiner->css(
+                array(
+                    array('classfinderbox.css'), 
+                    array('view/engineering.css'),
+                    array('astrolabe.css'),
+                    array('astrolabeLocation.css'),
+                    array('combobox.css'),
+                    array('searchEntry.css'),
+                    array('hostsCompliance.css'),
+                    array('hostsConnectivity.css'),
+                    array('hostsComplianceTimeseries.css'),
+                    array('hostInfo.css'),
+                    array('jScrollPane.css'),
+                    array('contextfinder.css'),   
+                    )
+            );        
+
+        
         $jsIE = array('flot/excanvas.min.js');
         $this->carabiner->group('iefix', array('js' => $jsIE));
         $this->carabiner->js($requiredjs);
