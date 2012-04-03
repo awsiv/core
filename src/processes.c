awsiv/core

commit 03846c0f6152b15ac1f43a5ab080399c85270b4b

Decrease code duplication in file diffs reports generation
File diffs reports contained a code which (nearly) duplicated the logic from
files_repository.c. Use newly introduced GetRepositoryPath to amend it.

src/processes.c
@@ -56,28 +56,10 @@ if (a.havedepthsearch)
    PromiseRef(cf_error,pp);
    return;
    }
-  
-if (a.repository == NULL && VREPOSITORY == NULL)
+
+if (!GetRepositoryPath(file, a, destination))
    {
-   strncpy(destination,file,CF_BUFSIZE-2);
-   }
-else
-   {
-   if (a.repository != NULL)
-      {
-      strncpy(destination,a.repository,CF_BUFSIZE-2);
-      }
-   else if (VREPOSITORY != NULL)
-      {
-      strncpy(destination,VREPOSITORY,CF_BUFSIZE-2);
-      }
-   
-   if (!JoinPath(destination,CanonifyName(file)))
-      {
-      CfOut(cf_error,"","Buffer overflow for long filename\n");
-      return;
-      }
-   
+   strlcpy(destination, file, CF_BUFSIZE);
    }
 
 if (!JoinPath(destination,"_cfchanges"))

commit e3a42ea8eb263fa3abc1cc12c605334b1a4eda81

File diff generation became broken after refactoring attempt in 03846c0f6152b15ac1f43a5ab080399c85270b4b. Fixes #342.

src/processes.c
@@ -51,20 +51,16 @@ void LogFileChange(char *file, int change, Attributes a, Promise *pp)
         return;
     }
 
-    if (!GetRepositoryPath(file, a, destination))
+    if (GetRepositoryPath(file, a, destination))
     {
-        strlcpy(destination, file, CF_BUFSIZE);
+        JoinPath(destination, CanonifyName(file));
+        MakeParentDirectory(destination, a.move_obstructions);
+    }
+    else
+    {
+        snprintf(destination, sizeof(destination), "%s_cfchanges", file);
     }
 
-    if (!JoinPath(destination, "_cfchanges"))
-    {
-        CfOut(cf_error, "", "Buffer overflow for long filename\n");
-        return;
-    }
-
-    if (!MakeParentDirectory(destination, a.move_obstructions))
-    {
-    }
 
 /* Check if the file already exists, if so do a diff */
 