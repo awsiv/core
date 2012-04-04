f12f5f0e6c6407fb4b30b01a9a6bec4b58c423d8
Sudhir Pandey <sudhir.pandey@cfengine.com>
2012-04-04 20:07:24 +0200

    script to create initial database example run is ./mongo phpcfengine export.js

---
+++ GUI2/phpcfenginenova/export.js
@@ -0,0 +1,84 @@
+
+db["onlineusers"].drop();
+db["roles"].drop();
+db["users"].drop();
+db["appsettings"].drop();
+
+/** appsettings indexes **/
+db["appsettings"].ensureIndex({
+  "_id": 1
+},[
+  
+]);
+
+/** onlineusers indexes **/
+db["onlineusers"].ensureIndex({
+  "_id": 1
+},[
+  
+]);
+
+/** roles indexes **/
+db["roles"].ensureIndex({
+  "_id": 1
+},[
+  
+]);
+
+/** users indexes **/
+db["users"].ensureIndex({
+  "_id": 1
+},[
+  
+]);
+
+/** appsettings records **/
+db["appsettings"].insert({
+  "_id": ObjectId("4e36aa774a235a3e49000003"),
+  "active_directory_domain": "",
+  "admin_group": "admin",
+  "appemail": "",
+  "base_dn": "",
+  "bluehost_threshold_global": "3000000",
+  "encryption": "plain",
+  "experimental": true,
+  "external_admin_username": "sudhir",
+  "fall_back_for": "admin",
+  "host": "",
+  "login_attribute": "",
+  "member_attribute": "",
+  "mode": "database",
+  "rbac": "false",
+  "users_directory": ""
+});
+
+/** onlineusers records **/
+db["onlineusers"].insert({
+  "_id": ObjectId("4f7c87d54a235a594a000000"),
+  "ip": "10.0.0.42",
+  "user_id": "admin",
+  "time": 1333561301,
+  "uri": "\/nova\/index.php\/welcome\/engg",
+  "bot": false
+});
+
+/** roles records **/
+db["roles"].insert({
+  "_id": ObjectId("4df78a754a235ac761000000"),
+  "name": "admin"
+});
+
+/** users records **/
+db["users"].insert({
+  "_id": ObjectId("4df786844a235a7d61000002"),
+  "active": 1,
+  "created_on": 1308067460,
+  "email": " ",
+  "ip_address": "10.0.0.9",
+  "last_login": 1333561299,
+  "password": "fbca3dac873569d2152483c148e14eb5d84a1e87",
+  "roles": [
+    "admin"
+  ],
+  "username": "admin"
+});
