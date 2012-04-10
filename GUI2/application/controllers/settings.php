78ea58c9e8280239e4f50fc04ec1af6ee5568509
Dmitry Shevchenko <dmitry84@gmail.com>
2012-04-10 16:26:39 +0200

    disable check for the same user. This could happen if we edit user, and then tried to assign admin role from settngs.
    previous behavior - admin role won't assign to that user

--- GUI2/application/controllers/settings.php
+++ GUI2/application/controllers/settings.php
@@ -178,7 +178,7 @@ class Settings extends Cf_Controller {
             // check auth mode and create admin user in ldap_users
             if ($form_data['mode'] == 'ldap' 
                     && (trim($form_data['external_admin_username']) != '')
-                    && (trim($settings->external_admin_username) != trim($form_data['external_admin_username']))
+                  //  && (trim($settings->external_admin_username) != trim($form_data['external_admin_username']))
                ) {
                 
                 $user = array();
