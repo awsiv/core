awsiv/core

commit 02597d5fbc766872fe07f75d41fd0313e2223197

add support to filter by regex on bundle type and promise type

src/install.c
@@ -1196,11 +1196,25 @@ void PromiseFilterAddPromiseType(PromiseFilter *filter, const char *promiseTypeI
     filter->promiseTypeInclude = SafeStringDuplicate(promiseTypeInclude);
 }
 
+void PromiseFilterAddPromiseTypeRx(PromiseFilter *filter, const char *promiseTypeRxInclude)
+{
+    assert(!filter->promiseTypeRxInclude && "PromiseFilterAddPromiseTypeRx: promiseTypeRxInclude is already set");
+
+    filter->promiseTypeRxInclude = SafeStringDuplicate(promiseTypeRxInclude);
+}
+
 void PromiseFilterAddBundleType(PromiseFilter *filter, const char *bundleTypeInclude)
 {
     assert(!filter->bundleTypeInclude && "PromiseFilterAddBundleType: bundleTypeInclude is already set");
 
     filter->bundleTypeInclude = SafeStringDuplicate(bundleTypeInclude);
+}
+
+void PromiseFilterAddBundleTypeRx(PromiseFilter *filter, const char *bundleTypeRxInclude)
+{
+    assert(!filter->bundleTypeRxInclude && "PromiseFilterAddBundleTypeRx: bundleTypeRxInclude is already set");
+
+    filter->bundleTypeRxInclude = SafeStringDuplicate(bundleTypeRxInclude);
 }
 
 void PromiseFilterAddBundles(PromiseFilter *filter, const char *bundleInclude, const char *bundleExclude)

cfmod/private-api.c
@@ -3786,14 +3786,14 @@ PHP_FUNCTION(cfpr_promise_list_by_bundle)
 
 PHP_FUNCTION(cfpr_promise_list_by_bundle_rx)
 {
-    char *userName, *bundleNameRx, *bundleType;
+    char *userName, *bundleNameRx, *bundleTypeRx;
     int user_len, b_len, btype_len;
     char buffer[CF_WEBBUFFER];
     PageInfo page = { 0 };
 
     if (zend_parse_parameters(ZEND_NUM_ARGS()TSRMLS_CC, "sssll",
                               &userName , &user_len,
-                              &bundleType, &btype_len,
+                              &bundleTypeRx, &btype_len,
                               &bundleNameRx, &b_len,
                               &(page.resultsPerPage),
                               &(page.pageNum)) == FAILURE)
@@ -3805,7 +3805,7 @@ PHP_FUNCTION(cfpr_promise_list_by_bundle_rx)
     ARGUMENT_CHECK_CONTENTS(user_len);
 
     char *fBundleNameRx = (b_len == 0) ? NULL : bundleNameRx;
-    char *fBundleType = (btype_len == 0) ? NULL : bundleType;
+    char *fBundleTypeRx = (btype_len == 0) ? NULL : bundleTypeRx;
 
     buffer[0] = '\0';
 
@@ -3815,7 +3815,7 @@ PHP_FUNCTION(cfpr_promise_list_by_bundle_rx)
 
     PromiseFilter *filter = HubQueryGetFirstRecord(hqPromiseFilter);
 
-    PromiseFilterAddBundleType(filter, fBundleType);
+    PromiseFilterAddBundleTypeRx(filter, fBundleTypeRx);
     PromiseFilterAddBundlesRx(filter, fBundleNameRx, NULL);
 
     Nova2PHP_promise_list(filter, buffer, sizeof(buffer), &page);

src/cf.nova.h
@@ -838,6 +838,8 @@ void PromiseFilterAddPromiseBody(PromiseFilter *filter, const char *handleInclud
 void PromiseFilterAddPromiseBodyRx(PromiseFilter *filter, const char *handleRxInclude, const char *promiserRxInclude);
 void PromiseFilterAddPromiseType(PromiseFilter *filter, const char *promiseTypeInclude);
 void PromiseFilterAddBundleType(PromiseFilter *filter, const char *bundleTypeInclude);
+void PromiseFilterAddPromiseTypeRx(PromiseFilter *filter, const char *promiseTypeRxInclude);
+void PromiseFilterAddBundleTypeRx(PromiseFilter *filter, const char *bundleTypeRxInclude);
 void PromiseFilterAddBundles(PromiseFilter *filter, const char *bundleInclude, const char *bundleExclude);
 void PromiseFilterAddBundlesRx(PromiseFilter *filter, const char *bundleRxInclude, const char *bundleRxExclude);
 void DeletePromiseFilter(PromiseFilter *filter);

src/cf.nova.web_api.h
@@ -65,7 +65,9 @@ typedef struct
     char *promiserInclude;
     char *promiserRxInclude;
     char *promiseTypeInclude;
+    char *promiseTypeRxInclude;
     char *bundleTypeInclude;
+    char *bundleTypeRxInclude;
     Rlist *bundleIncludes;
     Rlist *bundleRxIncludes;
     Rlist *bundleExcludes;

src/db_query.c
@@ -6378,6 +6378,7 @@ static bool BsonAppendPromiseFilter(bson_buffer *queryBuffer, PromiseFilter *fil
     modified |= BsonAppendStringSafe(queryBuffer, cfp_promisetype, filter->promiseTypeInclude);
 
     modified |= BsonAppendStringSafe(queryBuffer, cfp_bundletype, filter->bundleTypeInclude);
+    modified |= BsonAppendRegexSafe(queryBuffer, cfp_bundletype, filter->bundleTypeRxInclude);
 
     modified |= BsonAppendIncludeList(queryBuffer, cfp_bundlename, filter->bundleIncludes);
     modified |= BsonAppendIncludeRxList(queryBuffer, cfp_bundlename, filter->bundleRxIncludes);

commit 76554accecb0f16aa050d37e40d2a969e4c3b93e

Fix memory leak introduced in 02597d5

src/install.c
@@ -1253,6 +1253,8 @@ void DeletePromiseFilter(PromiseFilter *filter)
     free(filter->promiserRxInclude);
     free(filter->promiseTypeInclude);
     free(filter->bundleTypeInclude);
+    free(filter->promiseTypeRxInclude);
+    free(filter->bundleTypeRxInclude);
     DeleteRlist(filter->bundleIncludes);
     DeleteRlist(filter->bundleRxIncludes);
     DeleteRlist(filter->bundleExcludes);