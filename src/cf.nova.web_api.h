73b7992b40991975a57e4f63809425de75717fcc
Maciej Mrowiec <maciej.mrowiec@cfengine.com>
2012-04-17 10:16:26 +0200

    Code cleanup

--- src/cf.nova.web_api.h
+++ src/cf.nova.web_api.h
@@ -231,10 +231,8 @@ int Nova2PHP_validate_policy(char *file, char *buffer, int bufsize);
 
 char *Nova2PHP_get_help_text(char *key);
 
-int Nova2PHP_add_new_note(char *keyhash, char *repid, int reportType, char *username, time_t datetime, char *note,
-                          char *returnval, int bufsize);
-int Nova2PHP_add_note(char *noteid, char *username, time_t datetime, char *note, char *returnval, int bufsize);
-int Nova2PHP_get_notes(char *keyhash, char *cid, char *username, time_t from, time_t to, PageInfo *page,
+int Nova2PHP_add_note(char *noteid, char *keyhash, char *username, time_t datetime, char *note, char *returnval, int bufsize);
+int Nova2PHP_get_notes(char *keyhash, char *cid, char *username, char *filter_username, time_t from, time_t to, PageInfo *page,
                        char *returnval, int bufsize);
 int Nova2PHP_get_host_noteid(char *hostkey, char *returnval, int bufsize);
 
