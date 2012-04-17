53a549b435c2f1c54d30cc052faf1c7f0da3c31e
Dmitry Shevchenko <dmitry84@gmail.com>
2012-04-17 15:42:52 +0200

    set incList and exList in controller and use them in navigation template, in this way we can keep context for other reports

--- GUI2/application/views/searchpages/reportsMenu.php
+++ GUI2/application/views/searchpages/reportsMenu.php
@@ -1,12 +1,12 @@
 <div class="dd_menu">
     <ul>
-        <?php foreach ($reports_menu as $key => $val)
+    <?php foreach ($reports_menu as $key => $val)
         { ?>
             <li><span class="<?php echo $report_category == $key ? "selected" : "" ?>"> <?php echo $key ?></span>
                 <ul>
                     <?php foreach ($val as $reportdata)
                     { ?>
-                        <li><a id='<?php echo  $reportdata['id']; ?>' href="<?php echo site_url('/search/index/host/All/report') . '/' . urlencode($reportdata['id']) ?>" class="<?php echo $reportdata['id'] == $report_type ? "selected" : "" ?>"><?php echo $reportdata['name'] ?></a></li>
+                        <li><a id='<?php echo  $reportdata['id']; ?>' href="<?php echo site_url('/search/index/host/All/report') . '/' . urlencode($reportdata['id']) . (!empty($incList) ? "/inclist/".urlencode($incList) : '') . (!empty($exList) ? "/exlist/".urlencode($exList) : '') ?>" class="<?php echo $reportdata['id'] == $report_type ? "selected" : "" ?>"><?php echo $reportdata['name'] ?></a></li>
             <?php } ?>
                 </ul>
             </li>
