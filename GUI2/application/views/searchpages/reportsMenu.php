d6131972c63fc104435d875d46b56d8961d19cb2
Nishes Joshi <nishes.joshi@cfengine.com>
2012-05-07 12:24:46 +0200

    js fixed for expand and close for save search menu.

--- GUI2/application/views/searchpages/reportsMenu.php
+++ GUI2/application/views/searchpages/reportsMenu.php
@@ -13,31 +13,35 @@
         <?php } ?>
     </ul>
 
-        <ul id="saved_searches_wrapper">
-            <li id="saved-searches"><em class="separater"></em><span>Saved Searches</span>
-                <ul><li>
-                        <div id="searchaccordian">
+    <ul id="saved_searches_wrapper">
+        <li id="saved-searches"><em class="separater"></em><span>Saved Searches</span>
+            <ul><li>
+                    <div id="searchaccordian">
 
-                        </div>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+                    </div>
+                </li>
+            </ul>
+        </li>
+    </ul>
 </div>
 <script type="text/javascript">
     jQuery(document).ready(function(){
-        $('#searchaccordian .head').click(function() {
-            $(this).next().toggle();
-            $(this).toggleClass('expanded');
-            return false;
-        }).next().hide();
+
 
         $("#saved-searches").bind("mouseenter",function(){
+
+
             var menuUrl = '<?php echo site_url(); ?>'+'/search/generateSaveSearchMenu';
             $("#searchaccordian").html('loading ....');
             $("#searchaccordian").load(menuUrl,"", function(responseText, textStatus, XMLHttpRequest) {
                 if(textStatus == 'error') {
                     $('#searchaccordian').html('Cannot load saved searches.');
+                } else {
+                    $('#searchaccordian .head').click(function() {
+                        $(this).next().toggle();
+                        $(this).toggleClass('expanded');
+                        return false;
+                    }).next().hide();
                 }
             });
         });
