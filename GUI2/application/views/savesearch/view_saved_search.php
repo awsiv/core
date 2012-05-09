e9b20aea4b85aab774aded50b4194295df43d16c
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2012-05-09 13:42:52 +0200

    Consistent button text and warning language.

--- GUI2/application/views/savesearch/view_saved_search.php
+++ GUI2/application/views/savesearch/view_saved_search.php
@@ -44,7 +44,7 @@
 
 $('a.jDelete').click(function(e) {
     e.preventDefault();
-    if (!confirm('Are you sure to delete this search?')) {
+    if (!confirm('Are you sure you want to delete this search?')) {
         return false; 
         
     }
