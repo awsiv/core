bc1aaedc04de9742b220cce621f50316e7adec8c
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-05-14 12:04:26 +0200

    Add default time range in datepicker for log/summary reports (4 hours)

--- GUI2/application/views/searchpages/promises_not_kept.php
+++ GUI2/application/views/searchpages/promises_not_kept.php
@@ -5,8 +5,8 @@
               <span class="floatright"> Return host names only: <input type="checkbox" name="hosts_only" value="true"  <?php echo (is_array($paramArray) && isset($paramArray['hosts_only'])) ? 'checked' : ''; ?>></span>
                <span class="clear"></span>
               <input class="searchfield" type="text" name="name" value="<?php echo (is_array($paramArray) && isset($paramArray['name'])) ? trim($paramArray['name']) : ''; ?>"></p>
-     
-         
+
+
             <p>
                 Cause: (.*+[])<input type="text" name="cause" class="searchfield" value="<?php echo (is_array($paramArray) && isset($paramArray['cause'])) ? trim($paramArray['cause']) : ''; ?>">
             </p>
@@ -16,15 +16,19 @@
                 <span class="green_btn floatright" style="margin-top: 5px"><input class="green_btn" type="submit" value="Generate Report"></input></span>
                 <span class="clear"></span>
             </p>
-            <?php $this->load->view('searchpages/hidden_fields_for_set_context'); ?>   
+            <?php $this->load->view('searchpages/hidden_fields_for_set_context'); ?>
           </form>
       </div>
   </div>
 <script type="text/javascript">
+var horizon = 4 * 60 * 60 * 1000;
+var to = new Date();
+var from = new Date(to - horizon);
+
 $( "input:text[name=from]" ).datetimepicker({
       dateFormat: 'yy-mm-dd'
-});
+}).datetimepicker('setDate', from);
 $( "input:text[name=to]" ).datetimepicker({
       dateFormat: 'yy-mm-dd'
-});
+}).datetimepicker('setDate', to);
 </script>
