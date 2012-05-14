82d85e595e69dcc344ce4e446e1e4516dd76f797
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-05-14 12:04:26 +0200

    Add default time range in datepicker for log/summary reports (4 hours)

--- GUI2/application/views/searchpages/promises_repaired_log.php
+++ GUI2/application/views/searchpages/promises_repaired_log.php
@@ -12,20 +12,24 @@
             <p style="padding-top:15px">
                 <label>From </label> <input type="text" name="from" size="17" value="<?php echo (is_array($paramArray) && isset($paramArray['from'])) ? trim($paramArray['from']) : ''; ?>">
                 <label style="margin-left: 10px">To </label> <input type="text" name="to" size="17" value="<?php echo (is_array($paramArray) && isset($paramArray['to'])) ? trim($paramArray['to']) : ''; ?>">
-                
+
                 <span class="green_btn floatright" style="margin-top: 5px"><input class="green_btn" type="submit" value="Generate Report"></input></span>
                 <span class="clear"></span>
             </p>
-           
-        <?php $this->load->view('searchpages/hidden_fields_for_set_context'); ?>   
+        <?php $this->load->view('searchpages/hidden_fields_for_set_context'); ?>
         </form>
     </div>
+
 </div>
 <script type="text/javascript">
+var horizon = 4 * 60 * 60 * 1000;
+var to = new Date();
+var from = new Date(to - horizon);
+
 $( "input:text[name=from]" ).datetimepicker({
       dateFormat: 'yy-mm-dd'
-});
+}).datetimepicker('setDate', from);
 $( "input:text[name=to]" ).datetimepicker({
       dateFormat: 'yy-mm-dd'
-});
+}).datetimepicker('setDate', to);
 </script>
