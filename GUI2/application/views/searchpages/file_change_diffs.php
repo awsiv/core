67ef7b6bfe11231a2dfc901d1ae6af2bda6af046
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2012-05-14 15:50:17 +0200

    Added datetime picker with default time(4 hrs) for file change / diffs. Redmine# 685

--- GUI2/application/views/searchpages/file_change_diffs.php
+++ GUI2/application/views/searchpages/file_change_diffs.php
@@ -1,5 +1,4 @@
-<div id="searchform" class="panel">
-    
+<div id="searchform" class="panel">    
     <div class="panelcontent">
         <form method="post" action="<?php echo site_url('search') ?>">
             <p><label>File name: (.*+[])</label>
@@ -9,12 +8,26 @@
               
                 <p><label>Match content: (.*+[])</label> 
                   <input class="searchfield" type="text" name="diff" size="80" value="<?php echo (is_array($paramArray) && isset($paramArray['diff'])) ? trim($paramArray['diff']) : ''; ?>"></p>
-                <p>
-                    <span class="green_btn floatright" style="margin-top: 2px"><input class="green_btn" type="submit" value="Generate Report"></input></span>
-                    <span class="clear"></span>
-                </p>
+                  <p style="padding-top:15px">
+                      <label>From </label> <input type="text" name="from" size="17" value="<?php echo (is_array($paramArray) && isset($paramArray['from'])) ? trim($paramArray['from']) : ''; ?>">
+                      <label style="margin-left:10px">To </label> <input type="text" name="to" size="17" value="<?php echo (is_array($paramArray) && isset($paramArray['to'])) ? trim($paramArray['to']) : ''; ?>">
+                      <span class="green_btn floatright" style="margin-top: 5px"><input class="green_btn" type="submit" value="Generate Report"></input></span>
+                      <span class="clear"></span>
+                  </p>
           
             <?php $this->load->view('searchpages/hidden_fields_for_set_context'); ?>        
         </form>
     </div>
 </div>
+
+<script type="text/javascript">
+    var horizon = 4 * 60 * 60 * 1000;
+    var to = new Date();
+    var from = new Date(to - horizon);
+    $( "input:text[name=from]" ).datetimepicker({
+dateFormat: 'yy-mm-dd'
+}).datetimepicker('setDate', from);
+$( "input:text[name=to]" ).datetimepicker({
+dateFormat: 'yy-mm-dd'
+}).datetimepicker('setDate', to);
+</script>
