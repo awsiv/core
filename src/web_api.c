awsiv/core

commit 64b755768e676c989f86a314d9a6d45408a80223

Avoid returning static local variable pointer (from a function that can be called repeatedly throughout program execution). Pass the buffer to write-to and it size instead (Redmine #690)

src/web_api.c
@@ -2299,8 +2299,12 @@ int Nova2PHP_filediffs_report(char *hostkey, char *file, char *diffs, bool regex
 
         EscapeJson(hd->path, jsonEscapedStr, sizeof(jsonEscapedStr));
 
+        char diff[CF_BUFSIZE] = {0};
+
+        Nova_FormatDiff(hd->diff, diff, sizeof(diff) - 1);
+
         snprintf(buffer, sizeof(buffer), "[\"%s\",\"%s\",%ld,%s],",
-                 hd->hh->hostname, jsonEscapedStr, hd->t, Nova_FormatDiff(hd->diff));
+                 hd->hh->hostname, jsonEscapedStr, hd->t, diff);
 
         margin = headerLen + noticeLen + strlen(buffer);
         if (!JoinMargin(returnval, buffer, NULL, bufsize, margin))
@@ -3971,35 +3975,37 @@ void Nova2PHP_ComplianceSummaryGraph(char *policy, char *buffer, int bufsize)
 
 /*****************************************************************************/
 
-char *Nova_FormatDiff(char *s)
+void Nova_FormatDiff(const char *diffStr, char *returnval, int bufsize)
 {
-    char *sp, work[CF_BUFSIZE], diff[CF_BUFSIZE], tline[CF_BUFSIZE];
-    static char returnval[CF_BUFSIZE];
-    char pm;
-    int line = 0;
-    char jsonEscapedStr[CF_BUFSIZE] = { 0 };
+    snprintf(returnval, bufsize, "[");
 
-    strcpy(returnval, "[");
+    const char *sp;
+    char tline[CF_BUFSIZE];
 
-    for (sp = s; *sp != '\0'; sp += strlen(tline) + 1)
+    for (sp = diffStr; *sp != '\0'; sp += strlen(tline) + 1)
     {
+        int line = 0;
+        char pm;
+
+        char work[CF_BUFSIZE],
+             diff[CF_BUFSIZE],
+             jsonEscapedStr[CF_BUFSIZE] = {0};
+
         sscanf(sp, "%c,%d,%2047[^\n]", &pm, &line, diff);
         sscanf(sp, "%2047[^\n]", tline);
 
         EscapeJson(diff, jsonEscapedStr, sizeof(jsonEscapedStr));
         snprintf(work, sizeof(work), "[\"%c\",%d,\"%s\"],", pm, line, jsonEscapedStr);
-        if (!Join(returnval, work, sizeof(returnval)))
+
+        if (!Join(returnval, work, bufsize))
         {
             break;
         }
     }
-    if (returnval[strlen(returnval) - 1] == ',')
-    {
-        returnval[strlen(returnval) - 1] = '\0';
-    }
+
+    ReplaceTrailingChar(returnval, ',', '\0');
 
     EndJoin(returnval, "]\n", sizeof(returnval));
-    return returnval;
 }
 
 /*****************************************************************************/

src/cf.nova.cmd_api.h
@@ -11,7 +11,7 @@
 
 void Nova2Txt_getlicense(char *buffer, int bufsize);
 
-char *Nova_FormatDiff(char *s);
+void Nova_FormatDiff(const char *diffStr, char *returnval, int bufsize);
 char *Nova_LongState(char s);
 char *Nova_LongStateWarn(char s);
 int Nova_TimeWarn(time_t now, time_t then, time_t threshold, char *outStr, int outStrSz);

src/cf.nova.web_api.h
@@ -120,7 +120,7 @@ int Nova2PHP_get_value_graph(char *hostkey, char *day, char *month, char *year, 
 int Nova2PHP_list_all_goals(char *buffer, int bufsize);
 
 JsonElement *Nova2PHP_get_knowledge_view(int pid, char *view);
-char *Nova_FormatDiff(char *s);
+void Nova_FormatDiff(const char *diffStr, char *returnval, int bufsize);
 JsonElement *Nova2PHP_network_speed(char *hostkey);
 void Nova2PHP_GetPromiseBody(char *name, char *type, char *returnval, int bufsize);
 int Nova2PHP_body_list(char *name, char *type, char *returnval, int bufsize);

src/cmd_api.c
@@ -676,6 +676,9 @@ int Nova2Txt_filediffs_report(char *hostkey, char *file, char *diffs, bool regex
     {
         hd = (HubFileDiff *) rp->item;
 
+        char diff[CF_BUFSIZE] = {0};
+        Nova_FormatDiff(hd->diff, diff, sizeof(diff) - 1);
+
         if (CSV)
         {
             char *sp;
@@ -690,11 +693,11 @@ int Nova2Txt_filediffs_report(char *hostkey, char *file, char *diffs, bool regex
                 }
             }
 
-            printf("%s,%s,%ld,%s\n", hd->hh->hostname, hd->path, hd->t, Nova_FormatDiff(hd->diff));
+            printf("%s,%s,%ld,%s\n", hd->hh->hostname, hd->path, hd->t, diff);
         }
         else
         {
-            printf("%s file %s changed on %ld\n%s\n", hd->hh->hostname, hd->path, hd->t, Nova_FormatDiff(hd->diff));
+            printf("%s file %s changed on %ld\n%s\n", hd->hh->hostname, hd->path, hd->t, diff);
         }
     }
 

commit 6b8c5cc6b4811ab96856fb5c98049fd6af446c00

Fix: typo introduced in 64b755768e676c989f86a314d9a6d45408a80223

src/web_api.c
@@ -4033,7 +4033,7 @@ void Nova_FormatDiff(const char *diffStr, char *returnval, int bufsize)
 
     ReplaceTrailingChar(returnval, ',', '\0');
 
-    EndJoin(returnval, "]\n", sizeof(returnval));
+    EndJoin(returnval, "]\n", bufsize);
 }
 
 /*****************************************************************************/