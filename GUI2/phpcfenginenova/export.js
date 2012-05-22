10414214998fc7cb8445b1f5749fc1116d55dd01
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2012-05-22 14:53:07 +0200

    Avoid resetting phpcfengine database during upgrade. This DB contains information about users, roles, mission portal settings...

--- GUI2/phpcfenginenova/export.js
+++ GUI2/phpcfenginenova/export.js
@@ -1,9 +1,3 @@
-
-db["onlineusers"].drop();
-db["roles"].drop();
-db["users"].drop();
-db["appsettings"].drop();
-
 /** appsettings indexes **/
 db["appsettings"].ensureIndex({
   "_id": 1
@@ -33,52 +27,85 @@ db["users"].ensureIndex({
 ]);
 
 /** appsettings records **/
-db["appsettings"].insert({
-  "_id": ObjectId("4e36aa774a235a3e49000003"),
-  "active_directory_domain": "",
-  "admin_role": "admin",
-  "appemail": "",
-  "base_dn": "",
-  "bluehost_threshold_global": "3000000",
-  "encryption": "plain",
-  "experimental": true,
-  "external_admin_username": "",
-  "fall_back_for": "admin",
-  "host": "",
-  "login_attribute": "",
-  "member_attribute": "",
-  "mode": "database",
-  "rbac": "false",
-  "users_directory": ""
-});
+if(db["appsettings"].findOne())
+{
+    /* New addition in Nova 2.2.0*/
+    db["appsettings"].update({},{$set:{
+	"bluehost_threshold_global": "3000000",
+	"external_admin_username": "",
+	"rbac" : false
+    }}, false, true);
+}
+else
+{
+    db["appsettings"].insert({
+	"_id": ObjectId("4e36aa774a235a3e49000003"),
+	"active_directory_domain": "",
+	"admin_role": "admin",
+	"appemail": "",
+	"base_dn": "",
+	"bluehost_threshold_global": "3000000",
+	"encryption": "plain",
+	"experimental": true,
+	"external_admin_username": "",
+	"fall_back_for": "admin",
+	"host": "",
+	"login_attribute": "",
+	"member_attribute": "",
+	"mode": "database",
+	"rbac": false,
+	"users_directory": ""
+    });
+}
 
 /** onlineusers records **/
-db["onlineusers"].insert({
-  "_id": ObjectId("4f7c87d54a235a594a000000"),
-  "ip": "10.0.0.42",
-  "user_id": "admin",
-  "time": 1333561301,
-  "uri": "\/index.php\/engineering",
-  "bot": false
-});
+if(!db["onlineusers"].findOne())
+{
+    db["onlineusers"].insert({
+	"_id": ObjectId("4f7c87d54a235a594a000000"),
+	"ip": "10.0.0.42",
+	"user_id": "admin",
+	"time": 1333561301,
+	"uri": "\/index.php\/engineering",
+	"bot": false
+    });
+}
 
 /** roles records **/
-db["roles"].insert({
-  "_id": ObjectId("4df78a754a235ac761000000"),
-  "name": "admin"
-});
+if(db["groups"].findOne())
+{
+    /* Groups collection is called roles from Nova 2.2.0*/
+    db["groups"].renameCollection("roles");
+}
+else if(!db["roles"].findOne())
+{
+    db["roles"].insert({
+	"_id": ObjectId("4df78a754a235ac761000000"),
+	"name": "admin"
+    });
+}
 
 /** users records **/
-db["users"].insert({
-  "_id": ObjectId("4df786844a235a7d61000002"),
-  "active": 1,
-  "created_on": 1308067460,
-  "email": " ",
-  "ip_address": "10.0.0.9",
-  "last_login": 1333561299,
-  "password": "fbca3dac873569d2152483c148e14eb5d84a1e87",
-  "roles": [
-    "admin"
-  ],
-  "username": "admin"
-});
+if(db["users"].findOne())
+{
+    /* groups renames to roles from Nova 2.2.0*/
+    db["users"].update({},{$rename:{
+	"groups": "roles"
+    }}, false, true);
+}
+else
+{
+    db["users"].insert({
+	"_id": ObjectId("4df786844a235a7d61000002"),
+	"active": 1,
+	"created_on": 1308067460,
+	"email": " ",
+	"ip_address": "10.0.0.9",
+	"last_login": 1333561299,
+	"password": "fbca3dac873569d2152483c148e14eb5d84a1e87",
+	"roles": [
+	    "admin"
+	],
+	"username": "admin"
+    });
+}
