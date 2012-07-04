6c2d1f241fd500298ff71ec333d881c5f25e5daa
Sudhir Pandey <sudhir.pandey@cfengine.com>
2012-07-04 11:31:10 +0200

    changes incorporated for showing the start and stop button together

--- GUI2/application/views/eventviewer/addTracker.php
+++ GUI2/application/views/eventviewer/addTracker.php
@@ -2,7 +2,7 @@
 <?php
 echo form_open('eventTracker/create');
 ?>
-   <div id='step1'>
+
 <p>
    <label for="reportType">Report Type<span class="required"></span></label> 
    <select id="reportType" name="reportType">
@@ -18,36 +18,27 @@ echo form_open('eventTracker/create');
    <label for="time">Start Time<span class="required"></span></label> 
     <input id="time" type="text" name="time" value=""  />
 </p>
-
-<p>
-    <label></label>
-     <a class="green_btn" id="testBtn">
-           <span class="green_btn">Start</span>
-     </a> 
-    <span class="loadinggif" style="display:none">&nbsp;</span>
-</p>
-    </div>
-    
-<div id ="step2" style="display:none">
-    <p>
+   
+   <!-- <p>
         <a href="#" id="startViewhandle">Redefine tracker</a>
-    </p>
+    </p>-->
     <p>
         <label for="trackerName">Save <span class="required"></span></label> 
         <input id="trackerName" type="text" name="trackerName" value=""  />
     </p>
+    
     <p id="btnholder">
         <label></label>
-         <span class="green_btn">
+         <a class="green_btn" id="testBtn">
+           <span class="green_btn">Start</span>
+         </a> 
+        
+         <span class="green_btn" id="submitForm">
                <input class="green_btn" type="submit" value="Save" />
-         </span>
-        
-        <span class="green_btn">
-            <input class="green_btn" type="reset" value="Create New"/>
-        </span>
-        
+         </span> 
+         
     </p>
-</div>
+   
 <?
 echo form_close();
 ?>
