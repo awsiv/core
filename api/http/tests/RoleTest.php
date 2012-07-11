7087c36a53dd180628feefc96e40c4ec33314d95
Sudhir Pandey <sudhir.pandey@cfengine.com>
2012-07-11 13:52:53 +0200

    checking for additional fields while creating a role

--- api/http/tests/RoleTest.php
+++ api/http/tests/RoleTest.php
@@ -21,7 +21,11 @@ class RoleTest extends APIBaseTest
     public function testAddRole(){
         try{
          $this->pest->put('/role/jersey', '{
-                    "description": "Jersey Shore Role"
+                    "description": "Jersey Shore Role",
+                     "classrxinclude":"linux",
+                     "classrxexclude":"windows",
+                     "bundlerxinlcude":"bundle_1",
+                     "bundlerxexclude":"bundle_2"
                 }');
             $this->assertEquals(201, $this->pest->lastStatus());
 
@@ -29,6 +33,11 @@ class RoleTest extends APIBaseTest
             $roles = $this->getResults('/role');
             $this->assertValidJson($roles);
             $this->assertEquals('jersey', $roles[1]['name']);
+            $this->assertEquals('linux', $roles[1]['classrxinclude']);
+            $this->assertEquals('windows', $roles[1]['classrxexclude']);
+            $this->assertEquals('bundle_1', $roles[1]['bundlerxinlcude']);
+            $this->assertEquals('bundle_2', $roles[1]['bundlerxexclude']);
+             
         }catch(Pest_Exception $e)
         {
             $this->fail($e); 
