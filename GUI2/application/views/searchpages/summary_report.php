ff6c85fe61b545d92362c2e5a725fce00de28bfa
Nishes Joshi <nishes.joshi@cfengine.com>
2012-07-17 15:37:11 +0200

    Added feature for exporting reports when return host names only is checked. Fixes #831

--- GUI2/application/views/searchpages/summary_report.php
+++ GUI2/application/views/searchpages/summary_report.php
@@ -2,7 +2,7 @@
     <div class="panelcontent">
         <form method="post" action="<?php echo site_url('search') ?>">
            <p>Version: (.*+[])
-           <span class="floatright"><label> Return host names only:</label> <input type="checkbox" name="hosts_only" value="true"></span>
+           <span class="floatright"><label> Return host names only:</label> <input type="checkbox" name="hosts_only" value="true" <?php echo (is_array($paramArray) && isset($paramArray['hosts_only'])) ? 'checked' : ''; ?>></span>
            <span class="clear"></span> 
            <input class="searchfield" type="text" name="version" value="<?php echo (is_array($paramArray) && isset($paramArray['version'])) ? trim($paramArray['version']) : ''; ?>"></p>
         
