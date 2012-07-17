2dbdc9fd8fc08c15bfcf5d82904c037d1c8b15e4
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2012-07-17 15:09:56 +0200

    Add error message about non-existent bundle Refs redmine# 877

--- src/ontology.c
+++ src/ontology.c
@@ -336,6 +336,12 @@ void Nova_MapPromiseToTopic(FILE *fp, Promise *pp, const char *version)
                 const Policy *policy = PolicyFromPromise(pp);
                 bp = GetBundle(policy, bundlename, "agent");
 
+                if(bp == NULL)
+                {
+                    CfOut(cf_error, "", " Non-existent bundle \"%s\" referenced in \"%s\"\n", bundlename, (char *)cp->rval.item);
+                    break;
+                }
+
                 for (rp = bp->args; rp != NULL; rp = rp->next)
                 {
                     fprintf(fp, "bundles::\n\n");
