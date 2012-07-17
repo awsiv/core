ffdef6f477728c2fc0fe14a47a3cf1b397d26297
Sudhir Pandey <sudhir.pandey@cfengine.com>
2012-07-17 16:46:53 +0200

    two more unit test for settings to check, the rest api to handle ldap  related settings

--- api/http/tests/SettingsTest.php
+++ api/http/tests/SettingsTest.php
@@ -76,4 +76,60 @@ class SettingsTest extends APIBaseTest
             $this->fail($e);
         }
     }
+    
+     public function testUpdateLdapSetting()
+    {
+        try
+        {
+            $this->pest->post('/settings', '{
+                "ldapHsost": "yahoo.com",
+                "baseDn":"dc=cfengine;dc=com",
+                "loginAttr":"uid",
+                "userDirectories":["ou=jersey","cn=shore"],
+                "ldapEncryption":"ssl",
+                "externalAdmin":"ronnie"
+                }');
+            $this->assertEquals(204, $this->pest->lastStatus());
+
+            $settings = $this->getResults('/settings');
+            $this->assertValidJson($settings);
+            $this->assertEquals('yahoo.com', $settings[0]['ldapHost']);
+            $this->assertEquals('dc=cfengine;dc=com', $settings[0]['baseDn']);
+            $this->assertEquals('uid', $settings[0]['loginAttr']);
+            $this->assertEquals('jersey', $settings[0]['userDirectories'][0]);
+            $this->assertEquals('shore', $settings[0]['userDirectories'][1]);
+            $this->assertEquals('ssl', $settings[0]['ldapEncryption']);
+            $this->assertEquals('ronnie', $settings[0]['externalAdmin']);
+        }
+        catch (Pest_Exception $e)
+        {
+            $this->fail($e);
+        }
+    }
+    
+     public function testLdapLogin()
+    {
+        try
+        {
+            $this->pest->post('/settings', '{
+                "ldapHsost": "yahoo.com",
+                "baseDn":"dc=cfengine;dc=com",
+                "loginAttr":"uid",
+                "userDirectories":["ou=jersey","cn=shore"],
+                "ldapEncryption":"ssl",
+                "externalAdmin":"ronnie"
+                }');
+            $this->assertEquals(204, $this->pest->lastStatus());
+
+            $this->pest->setupAuth("ronnie", "password");
+            $settings = $this->getResults('/settings');
+            $this->assertValidJson($settings);
+              
+        }
+        catch (Pest_Exception $e)
+        {
+            $this->fail($e);
+        }
+    }
+    
 }
