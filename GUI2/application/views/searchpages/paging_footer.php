f8c9d6e5b260b50dc1445f4ce14ea53cfb2e0da5
Nishes Joshi <nishes.joshi@cfengine.com>
2012-07-19 15:17:34 +0200

    Hard coded the limit of number of rows selection on report generation. Fixes #419

--- GUI2/application/views/searchpages/paging_footer.php
+++ GUI2/application/views/searchpages/paging_footer.php
@@ -8,11 +8,11 @@ $path = isset($url) ? $url : 'search/index/';
             <?php if($pg['page']> $pg['first'] && $pg['start'] >  $pg['first']){?>
             <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . '/page/' . $pg['first']) ?>" title="Go to First Page" class="first"><span>First</span></a>
             <?php }?>
-            
+
             <?php if($pg['page']> $pg['first'] && $pg['start'] >  $pg['first']){?>
             <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . '/page/' .$pg['prevSet'] ) ?>" title="Go to Previous Page <?php echo $pg['prevSet'] ?>" class="prev"><span><</span></a>
             <?php }?>
-            
+
             <?php
             for ($i = $pg['start']; $i <= $pg['end']; $i++) {
                 if ($i == $pg['page']) {
@@ -25,24 +25,32 @@ $path = isset($url) ? $url : 'search/index/';
                 <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . "/page/$i") ?>" title="Go to Page <?php echo $i ?>" class="page <?php echo $current_class; ?>"><span><?php echo $i; ?></span></a>
 
             <?php } ?>
-            
+
              <?php if($pg['page'] != $pg['last'] && $pg['end']< $pg['last']){?>
             <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . '/page/' . $pg['nextSet']) ?>" title="Go to page <?php echo $pg['nextSet'] ?>" class="next"><span>></span></a>
              <?php } ?>
-            
+
             <?php if($pg['page'] != $pg['last'] && $pg['end']< $pg['last']){?>
             <a href="<?php echo site_url($path . $params . 'rows/' . $number_of_rows . '/page/' . $pg['last']) ?>" title="Go to Last Page" class="last"><span>Last</span></a>
             <?php } ?>
-            
+
            <div class="page_counter">
         <?php
         echo form_open($path . $params);
-        echo form_input('rows', $number_of_rows);
+        $arrayListRows = array("25"=>"25","50"=>"50","100"=>"100","250"=>"250","500"=>"500","1000"=>"1000");
+        echo form_dropdown('rows', $arrayListRows,array(0=>$number_of_rows));
         echo "<span>  Rows/Page</span>";
         echo form_close();
         ?>
     </div>
         </div>
-         
+
     </div>
 </div>
+<script type="text/javascript">
+$(function(){
+  $('select[name=rows]').change(function(){
+     $(this).closest("form").submit();
+  });
+});
+</script>
