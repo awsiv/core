1eac89649189992af1a0e0e855f0b7d30ce14d23
Sudhir Pandey <sudhir.pandey@cfengine.com>
2012-07-27 13:31:46 +0200

    fix for adjusting the width of text box in from and to date fields for chrome

--- GUI2/application/views/searchpages/file_change_log.php
+++ GUI2/application/views/searchpages/file_change_log.php
@@ -7,10 +7,10 @@
                <input class="searchfield" type="text" name="name" title="Regular expression supported" value="<?php echo (is_array($paramArray) && isset($paramArray['name'])) ? trim($paramArray['name']) : ''; ?>"></p>
           
             <p style="padding-top:15px">
-                <label>From </label> <input type="text" name="fromText" size="17" value="" />
+                <label>From </label> <input type="text" name="fromText" value="" class="dateField"/>
                 <input type="hidden" name="from" value="<?php echo (is_array($paramArray) && isset($paramArray['from'])) ? trim($paramArray['from']) : ''; ?>"/>
                 <input type="hidden" name="to" value="<?php echo (is_array($paramArray) && isset($paramArray['to'])) ? trim($paramArray['to']) : ''; ?>"/>
-                <label style="margin-left:10px">To </label> <input type="text" name="toText" size="17" value="" />
+                <label style="margin-left:10px">To </label> <input type="text" name="toText" value="" class="dateField"/>
                 <span class="green_btn floatright" style="margin-top: 5px"><input class="green_btn" type="submit" value="Generate Report"></input></span>
                 <span class="clear"></span>
             </p>
