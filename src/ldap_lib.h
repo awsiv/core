76ede9b55d18da10b9e90213ad7320b43384eb75
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-08-01 14:33:25 +0200

    Remove obsolute JSONish-string producing functions from ldap_lib

--- src/ldap_lib.h
+++ src/ldap_lib.h
@@ -11,12 +11,6 @@ Rlist *CfLDAP_GetSingleAttributeList(const char *username, const char *password,
                                      const char *attribute_name, const char *scopes, const char *security, bool start_tls,
                                      size_t page, size_t results_per_page, const char **const errstr);
 
-int CfLDAP_JSON_GetSeveralAttributes(char *uri, char *user, char *basedn, char *filter, Rlist *names, char *scopes,
-                                     char *sec, char *passwd, bool starttls, int page, int linesperpage, char *buffer,
-                                     int bufsize, const char **const errstr);
-int CfLDAP_JSON_GetSingleAttributeList(char *uri, char *user, char *basedn, char *filter, char *name, char *scopes,
-                                       char *sec, char *passwd, bool starttls, int page, int linesperpage, char *buffer,
-                                       int bufsize, const char **const errstr);
 void *CfLDAPValue(char *uri, char *dn, char *filter, char *name, char *scope, char *sec);
 
 #endif
