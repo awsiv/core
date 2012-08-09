06b5cd9c1ead96122ac9ca816b81a1904394c6d4
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-08-09 16:06:37 +0200

    Add LDAP test config + data

--- tests/serve-slapd.sh
+++ tests/serve-slapd.sh
@@ -5,8 +5,11 @@ SERVICE_NAME="OpenLDAP (slapd)"
 ROOT=$(dirname $(pwd))/
 LOGDIR=${ROOT}tests/log
 CONFDIR=${ROOT}tests/conf
+VARDIR=${ROOT}tests/var
+PORT=1025
 
 mkdir -p $LOGDIR
+mkdir -p $VARDIR/ldap
 
 echo "Starting ${SERVICE_NAME}"
-exec slapd -d 1 -h ldap://localhost:1025 -f $CONFDIR/ldap.conf -F $CONFDIR/slapd.d 1> $LOGDIR/slapd-stdout.log 2> $LOGDIR/slapd-stderr.log
+exec slapd -d 1 -h ldap://localhost:${PORT} -f $CONFDIR/slapd.conf 1> $LOGDIR/slapd-stdout.log 2> $LOGDIR/slapd-stderr.log
