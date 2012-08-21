aaf6c7e2c602113a77d3434c797dfaba0a403079
Mark Burgess <mark@cfengine.com>
2012-08-21 12:44:22 +0200

    Fix typos and confict in nova module

--- cfmod/private-api.c
+++ cfmod/private-api.c
@@ -4378,11 +4378,6 @@ PHP_FUNCTION(cfpr_list_services)
 
 PHP_FUNCTION(cfpr_service_histogram)
 {
-    const int bufsize = 1000000;
-    char buffer[bufsize];
-
-    buffer[0] = '\0';
-
     JsonElement *out = Nova2PHP_get_service_histogram();
     RETURN_JSON(out);
 }
