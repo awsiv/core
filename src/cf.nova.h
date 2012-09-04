5fe0b31e50a673bf6d1cd84dcbbe90f4d27ad0e6
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2012-09-04 12:01:45 +0200

    Move db keys definitions to cf.nova.h

--- src/cf.nova.h
+++ src/cf.nova.h
@@ -1488,6 +1488,23 @@ typedef struct
 #define cfk_qualifier "q"
 #define cfk_inference "i"
 
+// Scheduled reports
+
+/* top level: Level 0 */
+#define cfr_user_id "user" /* Must be unique */
+#define cfr_user_fullname "fullname"
+#define cfr_user_email "email"
+
+#define cfr_query_id "query_id" /* must be unique */
+#define cfr_query "query"
+#define cfr_created "created"
+#define cfr_last_modified "last_modified"
+#define cfr_last_run "last_executed"
+#define cfr_run_history "run_history" /* Array */
+#define cfr_run_classes "run_classes"
+#define cfr_enabled "enabled"
+#define cfr_already_run "already_run"
+
 // Report types
 
 #define CFREPORT_HOSTS 1
