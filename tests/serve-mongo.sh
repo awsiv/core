0185fe39d07ca69729d96a4634c6e9e43c270ddd
Eystein Måløy Stenberg <eystein@cfengine.com>
2012-09-05 13:56:41 -0700

    allow db and log file override for mongo testing

--- tests/serve-mongo.sh
+++ tests/serve-mongo.sh
@@ -4,8 +4,8 @@ SERVICE_NAME="MONGODB"
 ROOT=$(pwd)/..
 
 SERVICE=mongod
-LOGPATH=${ROOT}/tests/log/mongod.log
-DBPATH=${ROOT}/tests/test-db
+LOGPATH=${CFENGINE_TEST_OVERRIDE_MONGO_LOGPATH:-${ROOT}/tests/log/mongod.log}
+DBPATH=${CFENGINE_TEST_OVERRIDE_MONGO_DBPATH:-${ROOT}/tests/test-db}
 PORT=${CFENGINE_TEST_OVERRIDE_MONGO_PORT:-27777}
 
 mkdir -p log
