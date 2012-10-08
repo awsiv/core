3afe0c10cadd57cd5016b463b02028f757c6e36d
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2012-10-08 15:51:27 +0200

    Add BsonDoubleGet

--- src/bson_lib.c
+++ src/bson_lib.c
@@ -99,6 +99,19 @@ bool BsonTimeGet(const bson *b, const char *key, time_t *out)
     }
 }
 
+bool BsonDoubleGet(const bson *b, const char *key, double *out)
+{
+    bson_iterator it;
+
+    if (bson_find(&it, b, key) == BSON_DOUBLE)
+    {
+        *out = bson_iterator_double(&it);
+        return true;
+    }
+
+    return false;
+}
+
 bool BsonArrayGet(const bson *b, const char *key, bson *out)
 {
     bson_iterator it;
