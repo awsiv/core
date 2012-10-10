1fb040f8fa4cdabbe8e33f32580c07cdfaaac7d6
Sigurd Teigen <sigurd.teigen@cfengine.com>
2012-10-10 11:31:07 +0200

    Add memtest for POST /api/user/:id

--- api/tests/cfapi_user_put.php
+++ api/tests/cfapi_user_put.php
@@ -4,4 +4,8 @@ $password = getenv("CFENGINE_TEST_PASSWORD");
 
 $res = cfapi_user_put($username, 'user123', 'pass', 'User', 'a@b.com', array('admin')); 
 fwrite(STDERR, $res);
+
+$res = cfapi_user_post($username, 'user123', 'pass', 'User2', null, null);
+fwrite(STDERR, $res);
+
 ?>
