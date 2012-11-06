awsiv/core

commit ceb958ec64513e4729bf3c8972880a03796f01ac

Remove unused variables

src/magnify.c
@@ -23,7 +23,7 @@
 
 bool Nova_ReadMagTimeSeries2(EnterpriseDB *conn, DataView *cfv, char *hostkey, char *vitalId)
 {
- double ry, rq, rs, rg;
+ double rq, rs;
     double q[CF_MAGDATA], e[CF_MAGDATA], d[CF_MAGDATA], g[CF_MAGDATA];
     int i;
     bool hasData = false;
@@ -42,10 +42,8 @@ bool Nova_ReadMagTimeSeries2(EnterpriseDB *conn, DataView *cfv, char *hostkey, c
         }
 
         // Num() resets negative numbers to zero
-        ry = cfv->data_E[i] = Num(e[i]);
         rq = cfv->data_q[i] = Num(q[i]);
         rs = cfv->bars[i] = Num(d[i]);
-        rg = cfv->bars[i] = Num(g[i]);
 
         if (rq > cfv->max)
         {

cfmod/private-api.c
@@ -4378,11 +4378,6 @@ PHP_FUNCTION(cfpr_list_services)
 
 PHP_FUNCTION(cfpr_service_histogram)
 {
-    const int bufsize = 1000000;
-    char buffer[bufsize];
-
-    buffer[0] = '\0';
-
     JsonElement *out = Nova2PHP_get_histogram_for_service();
     RETURN_JSON(out);
 }
@@ -4391,11 +4386,6 @@ PHP_FUNCTION(cfpr_service_histogram)
 
 PHP_FUNCTION(cfpr_port_histogram)
 {
-    const int bufsize = 1000000;
-    char buffer[bufsize];
-
-    buffer[0] = '\0';
-
     JsonElement *out = Nova2PHP_get_open_port_histograms();
     RETURN_JSON(out);
 }

src/topicmap.c
@@ -1825,7 +1825,6 @@ Item *Nova_NearestNeighbours(int search_id, char *assoc_mask)
     bson_iterator it1, it2, it3;
     EnterpriseDB conn;
     Item *list = NULL;
-    int topic_id;
     char assoc_name[CF_BUFSIZE];
     char afwd[CF_BUFSIZE], abwd[CF_BUFSIZE];
     char assoc_context[CF_BUFSIZE];
@@ -1864,8 +1863,6 @@ Item *Nova_NearestNeighbours(int search_id, char *assoc_mask)
     while (mongo_cursor_next(cursor) == MONGO_OK)   // loops over documents
     {
         bson_iterator_init(&it1, mongo_cursor_bson(cursor));
-
-        topic_id = 0;
 
         while (BsonIsTypeValid(bson_iterator_next(&it1)) > 0)
         {

src/web_api.c
@@ -4881,7 +4881,7 @@ JsonElement *Nova2PHP_get_open_port_histograms(void)
     Item *udp6 = NULL;
     Item **this_ptr, *ip;
     bson_iterator it1, it2, it3, it4, it5;
-    char rscope[CF_MAXVARSIZE], lval[CF_MAXVARSIZE], rtype;
+    char rscope[CF_MAXVARSIZE], lval[CF_MAXVARSIZE];
 
     if (!CFDB_Open(&dbconn))
     {        
@@ -4935,7 +4935,6 @@ JsonElement *Nova2PHP_get_open_port_histograms(void)
                     while (bson_iterator_next(&it3))
                     {
                         lval[0] = '\0';
-                        rtype = CF_SCALAR;
 
                         bson_iterator_subiterator(&it3, &it4);
 
@@ -4973,7 +4972,6 @@ JsonElement *Nova2PHP_get_open_port_histograms(void)
                                 case BSON_ARRAY:
                                 case BSON_OBJECT:
                                     bson_iterator_subiterator(&it4, &it5);
-                                    rtype = CF_LIST;
 
                                     while (bson_iterator_next(&it5))
                                        {

commit 6a00ea4d162257b490d6475fcd3fdee6fa56259b

Fix: Average value for cfpr_vitals_view_magnified always zero. Redmine #1625. Bug re-introduced in: ceb958ec64513e4729bf3c8972880a03796f01ac

src/magnify.c
@@ -23,7 +23,7 @@
 
 bool Nova_ReadMagTimeSeries2(EnterpriseDB *conn, DataView *cfv, const char *hostkey, const char *vitalId)
 {
- double rq, rs;
+    double rq, rs;
     double q[CF_MAGDATA], e[CF_MAGDATA], d[CF_MAGDATA], g[CF_MAGDATA];
     int i;
     bool hasData = false;
@@ -42,6 +42,8 @@ bool Nova_ReadMagTimeSeries2(EnterpriseDB *conn, DataView *cfv, const char *host
         }
 
         // Num() resets negative numbers to zero
+        cfv->data_E[i] = Num(e[i]);
+        cfv->data_dq[i] = Num(g[i]);
         rq = cfv->data_q[i] = Num(q[i]);
         rs = cfv->bars[i] = Num(d[i]);
 