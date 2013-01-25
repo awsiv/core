f43016f353a4a09492b3203072283e5a3e747caf
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2013-01-25 12:54:11 +0100

    Add regular expression support to sqlite3
        Example query:
        SELECT * FROM Software Where SoftwareName REGEXP \"zlib1.*\";

---
+++ sqlite3/sqlite3_functions.c
@@ -0,0 +1,31 @@
+#include "conf.h"
+#include "sqlite3_functions.h"
+#include "bool.h"
+#include "string_lib.h"
+
+void RegExp(sqlite3_context *ctx, int argc, sqlite3_value **argv)
+{
+    if (argc != 2)
+    {
+        sqlite3_result_error(ctx, "Incorrect argument count for SQL regexp()", -1);
+        return;
+    }
+
+    const unsigned char *regex = sqlite3_value_text(argv[0]);
+
+    if (regex == NULL)
+    {
+        sqlite3_result_error(ctx, "Please provide a regular expression for SQL regexp()", -1);
+        return;
+    }
+
+    const unsigned char *text = sqlite3_value_text(argv[1]);
+    if (text == NULL)
+    {
+        sqlite3_result_error(ctx, "Text unavailable for regular expression comparision in SQL regexp()", -1);
+        return;
+    }
+
+    sqlite3_result_int(ctx, StringMatchFull(regex, text));
+    return;
+}
---
+++ sqlite3/sqlite3_functions.h
@@ -0,0 +1,8 @@
+#ifndef SQLITE3_FUNCTIONS_H
+#define SQLITE3_FUNCTIONS_H
+
+#include "sqlite3.h"
+
+void RegExp(sqlite3_context *ctx, int argc, sqlite3_value **argv);
+
+#endif // SQLITE3_FUNCTIONS_H
