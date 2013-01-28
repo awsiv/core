awsiv/core

commit 3a128e352195a629adfe9087819f9d92e42aa1bc

Fix: segfault in promiselogs summary report.
Adding the returned hash index for handle and cause separately produced out-of-bounds error.

Concatenate the two strings and then hash instead.

libcfnova/install.c
@@ -17,6 +17,7 @@ This file is (C) Cfengine AS. See COSL LICENSE for details.
 #include "matching.h"
 #include "hashes.h"
 #include "cfstream.h"
+#include "buffer.h"
 
 #include <assert.h>
 
@@ -479,16 +480,22 @@ unsigned int HubPromiseLogHash(const void *record, unsigned int max)
 {
     const HubPromiseLog *record_a = (const HubPromiseLog *)record;
 
-    unsigned int hash = 0;
+    Buffer *buf = NULL;
+    if (BufferNew(&buf))
+    {
+        CfOut(cf_error, "", "!! Memory allocation error in HubPromiseLogHash");
+    }
+
     if (record_a->handle)
     {
-        hash += OatHash(record_a->handle, max);
+        BufferPrintf(buf, "%s", record_a->handle);
     }
     if (record_a->cause)
     {
-        hash += OatHash(record_a->cause, max);
+        BufferPrintf(buf, "%s", record_a->cause);
     }
-    return hash;
+
+    return OatHash(buf->buffer, max);
 }
 
 bool HubPromiseLogEqual(const void *a, const void *b)

commit ff9200ffd3be7dceb3502cd7a9ee1cd3e86ba239

Buffer needs to be destroyed. Fix memory leak introduced in 3a128e352195a629adfe9087819f9d92e42aa1bc

libcfnova/install.c
@@ -495,7 +495,11 @@ unsigned int HubPromiseLogHash(const void *record, unsigned int max)
         BufferPrintf(buf, "%s", record_a->cause);
     }
 
-    return OatHash(buf->buffer, max);
+    unsigned int hash = OatHash(buf->buffer, max);
+
+    BufferDestroy(&buffer);
+
+    return hash;
 }
 
 bool HubPromiseLogEqual(const void *a, const void *b)