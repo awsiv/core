d19a4754176ad8b4656f6f7fdfa75967433e5337
Bishwa Shrestha <bishwa.shrestha@cfengine.com>
2013-02-12 12:58:45 +0100

    Fix failing test: test_bundle_qualified_name_split

--- tests/unit/conversion_test.c
+++ tests/unit/conversion_test.c
@@ -16,7 +16,7 @@ static void test_bundle_qualified_name_split(void **state)
     assert_string_equal("", ns);
     assert_string_equal("foo", name);
 
-    assert_true(BundleQualifiedNameSplit("bar.foo", ns, name));
+    assert_true(BundleQualifiedNameSplit("bar:foo", ns, name));
     assert_string_equal("bar", ns);
     assert_string_equal("foo", name);
 }
